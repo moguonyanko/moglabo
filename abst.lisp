8808ab1534859ef5d77a6bd93de16a8316c1af64
KouichiYamada <moguo_50000_nyanko@yahoo.co.jp>
2012-06-25 03:45:47 +0900

    reference add

--- abst.lisp
+++ abst.lisp
@@ -1,6 +1,16 @@
 ;;;2012/06/22 moguonyanko
 ;;;Practice coding CommonLisp to 
 ;;;train abstraction and study math.
+;;;Reference:
+;;;		ANSI Common Lisp
+;;;			Paul Graham
+;;;		On Lisp
+;;;			Paul Graham
+;;;		Paradigms of Artifical Intelligence Programing:Case Studies in Common Lisp
+;;;			Peter Norvig
+;;;		Introduction to FORTRAN 90, Second Edition
+;;;			Larry Nyhoff, Sanford Leestma
+;;;
 
 (defun sum (lst)
 	(labels ((_sum (_lst acc)
@@ -15,6 +25,12 @@
 (defun heron (a b c)
 	(let ((s (/ (+ a b c) 2)))
 		(sqrt (+ s (- s a)(- s b)(- s c)))))
+
+(defun quadeq (a b c)
+	)
+
+(defun integral (a b n)
+	)
 
 (defclass vector ()
 	;Vector class
