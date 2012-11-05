a429721c803fa7713b580b43dc8aacda712e8519
KouichiYamada <moguo_50000_nyanko@yahoo.co.jp>
2012-11-06 03:06:15 +0900

    setsumon1 add

---
+++ lang/r/setsumon1.R
@@ -0,0 +1,16 @@
+zokusei <- read.csv("oubo_zokusei.csv", header=F)
+
+summary(zokusei)
+length(zokusei$V1)
+nenrei <- na.omit(zokusei$V3) 
+summary(nenrei)
+boxplot(nenrei)
+
+kiroku <- read.csv("oubo_kiroku.csv", header=F)
+summary(kiroku)
+length(kiroku$V1)
+oubono <- kiroku$V1
+hist(oubono)
+
+
+
