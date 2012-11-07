3bf517ab20e7e7a004bd23576d0de8dded1899db
KouichiYamada <moguo_50000_nyanko@yahoo.co.jp>
2012-11-08 02:42:41 +0900

    setsumon solution modify

--- lang/r/setsumon1.R
+++ lang/r/setsumon1.R
@@ -1,19 +1,29 @@
-zokusei <- read.csv("oubo_zokusei.csv", header=F)
+#zokusei <- read.csv("oubo_zokusei.csv", header=F)
 
-summary(zokusei)
-length(zokusei$V1)
-nenrei <- na.omit(zokusei$V3) 
-summary(nenrei)
-boxplot(nenrei)
+#summary(zokusei)
+#length(zokusei$V1)
+#nenrei <- na.omit(zokusei$V3) 
+#summary(nenrei)
+#boxplot(nenrei)
 
-kiroku <- read.csv("oubo_kiroku.csv", header=F)
+#kiroku <- read.csv("oubo_kiroku.csv", header=F)
 
-summary(kiroku)
-length(kiroku$V1)
-jikantai <- kiroku$V2
-plot(jikantai)
+#summary(kiroku)
+#length(kiroku$V1)
+#jikantai <- kiroku$V2
+#plot(jikantai)
+
 library(chron)
-day.of.week()
+day2week <- function(day){
+	strday <- strsplit(day, " ")[[1]][1]
+	ymd <- strsplit(strday, "/")
+	week <- day.of.week(ymd[[1]][2], ymd[[1]][3], ymd[[1]][1])
+	
+	week
+}
+
+#jikantai_week <- jikantai[sapply(jikantai, day2week)]
+#plot(jikantai_week)
 
 
 
