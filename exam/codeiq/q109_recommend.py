e51e36774dda8bbebca57f3f534e386ec50a7de9
KouichiYamada <moguo_50000_nyanko@yahoo.co.jp>
2012-11-22 03:26:42 +0900

    recommend system planning

--- exam/codeiq/q109_recommend.py
+++ exam/codeiq/q109_recommend.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import unittest
-import sys
-
 class Customer():
 	'''
-	お客さんクラスです。
+	お客さんのクラスです。
 	'''
 	def __init__(self, customerid, name):
 		self.id = customerid
@@ -38,37 +35,80 @@ class CustomerList():
 		self.customers = customers
 
 class Book():
-	def __init__(self, name, category):
+	'''
+	書籍のクラスです。
+	'''
+	def __init__(self, name, rank):
+		self.name = name
+		self.rank = rank
+		
+	def __lt__(self, other):
 		pass
+
+	def __gt__(self, other):
+		pass
+
+	def __eq__(self, other):
+		pass
+
+	def __ne__(self, other):
+		pass
+	
+	def __str__(self):
+		return str(rank)+":"+name
 
 class SalesRanking():
 	'''
-	書籍の売り上げランキングクラスです。
+	書籍の売り上げランキングのクラスです。
 	'''
 	def __init__(self, path):
 		ranking = {}
+		categories = {}
 		
 		with open(path, encoding='utf-8') as sales:
 			for idx, line in enumerate(sales):
 				if idx == 0: continue #ヘッダを飛ばす
 				
 				rank, name, category = line.split("\t")
-				ranking[rank] = Book(name, category)
+				book = Book(name, rank)
+				ranking[rank] = book
+				if categories[category] == None:
+					categories[category] = []
+				categories[category].append(book)
 						
 		self.ranking = ranking
 				
-def recommend(customer, sales):
+def recommend(customers, sales, recommendnum=3):
+	'''
+	*推薦方針* 
+	基本的に購入実績が多いジャンルの書籍を推薦する。
+	推薦する書籍数はデフォルトで3冊とする。
+	購入実績が並ぶ場合は売上ランクの高い書籍を推薦する。
+	購入実績の割合から推薦内容を決定する。
+	例えば「技術;文学;技術」であれば，技術系の書籍を2冊,
+	文学系の書籍を1冊推薦することを試みる。
+	3冊未満など購入実績が乏しい場合,推薦内容は
+	売上ランクの結果に大きく依存することになる。
+	全体で最も多く推薦されているジャンルの書籍を
+	別枠で表示できるようにするのもいい。
+	'''
 	pass
-
-class RecommendBooks(unittest.TestCase):
-	pass
+	
+	def __str__(self):
+		pass
 
 if __name__ == '__main__':
+	import sys
+	
 	args = sys.argv
 	if len(args) > 1: 
 		quit()
 		
 	customer_list_file = args[0]
 	sales_ranking_file = args[1]
+	customerlist = CustomerList(customer_list_file)
+	salesranking = SalesRanking(sales_ranking_file)
 	
-
+	result = recommend(customerlist, salesranking)
+	print(result)
+	
