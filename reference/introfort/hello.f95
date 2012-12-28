8f0d687afbdaf13af2964e568678c9a5000bb996
KouichiYamada <moguo_50000_nyanko@yahoo.co.jp>
2012-12-29 04:20:09 +0900

    reference remove

--- reference/introfort/hello.f95
+++
@@ -1,9 +0,0 @@
-!----------------------------------------------------------------------------------------------
-!20120622 MoguoNyanko fortran studying
-!Reference:
-!	Introduction to FORTRAN 90, Second Edition:Larry Nyhoff, Sanford Leestma
-!----------------------------------------------------------------------------------------------
-
-program hello
-  print *, 'Hello World!'
-end program hello
