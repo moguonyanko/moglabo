65ef2f3c29742527af37a71e378738f8eb49bb1f
moguonyanko <moguonyanko@mognyanhost>
2012-12-30 23:07:55 +0900

    BMTCalc heap use

--- lang/objective-c/BMICalc/main.m
+++ lang/objective-c/BMICalc/main.m
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 typedef struct {
@@ -6,20 +7,25 @@ typedef struct {
 	int weightInKilos;
 } Person; 
 
-float bodyMathIndex(Person p)
+float bodyMathIndex(Person *p)
 {
-	return p.weightInKilos / pow(p.heightInMeters, 2);
+	return p->weightInKilos / pow(p->heightInMeters, 2);
 }
 
 int main(int argc, const char * argv[])
 {
-	Person person;
-	person.weightInKilos = 96;
-	person.heightInMeters = 1.8;
-	printf("person weights %d kilograms\n", person.weightInKilos);
-	printf("person is %.2f meters tall\n", person.heightInMeters);
+	Person *person = (Person *)malloc(sizeof(Person));
+	
+	person->weightInKilos = 81;
+	person->heightInMeters = 2.0;
+	printf("person weights %d kilograms\n", person->weightInKilos);
+	printf("person is %.2f meters tall\n", person->heightInMeters);
 	float bmi = bodyMathIndex(person);
 	printf("person has a BMI of %f\n", bmi);
+
+	free(person);
+
+	person = NULL;
 
 	return 0;
 }
