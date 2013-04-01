75cca96bba4a6a7e2089bf019c9e6e95794118cc
KouichiYamada <moguo_50000_nyanko@yahoo.co.jp>
2013-04-02 01:57:03 +0900

    ci module add

---
+++ lang/csharp/ci.cs
@@ -0,0 +1,22 @@
+/**
+ * This is Collective Intelligence library for studying oneself.
+ * Reference:
+ * 「集合知プログラミング(O'REILLY)」
+ **/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CI
+{
+	public class DocumentFiltering
+	{
+		public static Dictionary<string, int> GetWords(string doc)
+		{
+			return null;
+		}
+	}
+}
+
---
+++ lang/csharp/test_ci.cs
@@ -0,0 +1,15 @@
+using System;
+using NUnit.Framework;
+using CI;
+
+namespace TestCI
+{
+	[TestFixture]
+	public class TestDocumentFiltering
+	{
+		[Test]
+		public void TestGetWords()
+		{
+		}
+	}
+}	
