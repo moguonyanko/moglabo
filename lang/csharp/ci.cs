5328adfd88953ea82efdb06052d1e593bb483df5
moguonyanko <moguo_50000_nyanko@yahoo.co.jp>
2013-04-02 03:13:22 +0900

    GetWords implenment

--- lang/csharp/ci.cs
+++ lang/csharp/ci.cs
@@ -8,14 +8,37 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace CI
 {
 	public class DocumentFiltering
 	{
+		private static bool IsAcceptWord(string word)
+		{
+			return !string.IsNullOrEmpty(word) && 
+					2 < word.Length && word.Length < 20;
+		}
+	
 		public static Dictionary<string, int> GetWords(string doc)
 		{
-			return null;
+			Dictionary<string, int> result = new Dictionary<string, int>();
+			Regex splitter = new Regex(@"[\n\r,.\(\)\s\t]+", RegexOptions.Multiline);
+			string[] words = splitter.Split(doc);
+			
+			var wordRecords = 
+				from word in words 
+				where IsAcceptWord(word) 
+				group word by word.ToLower() into grp 
+				orderby grp.Count() 
+				select new { Count = grp.Count(), Word = grp.Key };
+			
+			foreach (var record in wordRecords)
+			{
+				result.Add(record.Word, record.Count);
+			}
+			
+			return result;
 		}
 	}
 }
--- lang/csharp/test_ci.cs
+++ lang/csharp/test_ci.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using NUnit.Framework;
+
 using CI;
 
 namespace TestCI
@@ -10,6 +12,23 @@ namespace TestCI
 		[Test]
 		public void TestGetWords()
 		{
+			string sample = @"Hello world, 
+			I am happy.
+			(This is test.)	The hello test.";
+			
+			Dictionary<string, int> result = DocumentFiltering.GetWords(sample);
+			
+			Dictionary<string, int> expect = new Dictionary<string, int>()
+			{
+				{"world", 1},
+				{"happy", 1},
+				{"this", 1},
+				{"the", 1},
+				{"hello", 2},
+				{"test", 2}
+			};
+			
+			Assert.AreEqual(expect, result);
 		}
 	}
 }	
