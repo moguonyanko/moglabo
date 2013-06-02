7eb9b4dfe8852e39384c9a140cc0c1abb78b2ffa
KouichiYamada <k-yamada@tg-inet.co.jp>
2013-06-03 02:39:52 +0900

    feature count sql add
    Signed-off-by: moguonyanko

---
+++ lang/sql/create_table_categorycount.sql
@@ -0,0 +1,6 @@
+﻿DROP TABLE IF EXISTS `geolib`.`categorycount`;
+CREATE TABLE  `geolib`.`categorycount` (
+  `category` varchar(64) NOT NULL DEFAULT 'unknown',
+  `count` BIGINT unsigned DEFAULT '0',
+  PRIMARY KEY (`category`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
---
+++ lang/sql/create_table_featurecount.sql
@@ -0,0 +1,7 @@
+﻿DROP TABLE IF EXISTS `geolib`.`featurecount`;
+CREATE TABLE  `geolib`.`featurecount` (
+  `feature` varchar(255) NOT NULL,
+  `category` varchar(64) NOT NULL DEFAULT 'unknown',
+  `count` BIGINT unsigned DEFAULT '0',
+  PRIMARY KEY (`feature`,`category`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
