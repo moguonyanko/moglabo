3f4744759e07e4f5da5cbccdfffe3148acacef9a
moguonyanko <moguo_50000_nyanko@yahoo.co.jp>
2013-08-10 05:07:27 +0900

    parallelism parameter add

--- lang/java/exercise/src/exercise/concurrent/ForkJoinPractice.java
+++ lang/java/exercise/src/exercise/concurrent/ForkJoinPractice.java
@@ -11,21 +11,26 @@ public class ForkJoinPractice {
 
 	public static void main(String[] args) {
 
+		System.out.println("available CPU:" + Runtime.getRuntime().availableProcessors());
+
 		int n = 40;
+		FibonacciTask task = new FibonacciTask(n);
+
 		FibFunc func;
-		
-		func = new NormalFibFunc(n);
-		//func = new SubmitFibFunc(n);
-		//func = new InvokeFibFunc(n);
-		
+		int parallelism = 4;
+
+		//func = new NormalFibFunc(task, parallelism);
+		//func = new SubmitFibFunc(task, parallelism);
+		func = new InvokeFibFunc(task, parallelism);
+
 		long startTime = System.currentTimeMillis();
-		
+
 		int resultInvoke = func.calc();
-		
+
 		long endTime = System.currentTimeMillis();
-		
+
 		long executeTime = endTime - startTime;
-		
+
 		System.out.println("n = " + n);
 		System.out.println("Fibonacci solution by invoke:" + resultInvoke);
 		//System.out.println("Fibonacci solution by submit:" + resultSubmit);
@@ -56,85 +61,88 @@ class FibonacciTask extends RecursiveTask<Integer> {
 		return task2.invoke() + task1.join();
 
 	}
-	
-	final int getSource(){
+
+	final int getStart() {
 		return n;
 	}
 }
 
-interface FibFunc{
+interface FibFunc {
+
 	int calc();
 }
 
-abstract class CalcFibFunc implements FibFunc{
-	
-	final FibonacciTask task;
+abstract class CalcFibFunc implements FibFunc {
 
-	public CalcFibFunc(int n) {
-		this.task = new FibonacciTask(n);
+	final FibonacciTask task;
+	final int parallelism;
+
+	public CalcFibFunc(FibonacciTask task, int parallelism) {
+		this.task = task;
+
+		if (parallelism <= 0) {
+			parallelism = Runtime.getRuntime().availableProcessors();
+		}
+
+		this.parallelism = parallelism;
 	}
-	
 }
 
-class SubmitFibFunc extends CalcFibFunc{
+class SubmitFibFunc extends CalcFibFunc {
 
-	public SubmitFibFunc(int n) {
-		super(n);
+	public SubmitFibFunc(FibonacciTask task, int parallelism) {
+		super(task, parallelism);
 	}
 
 	@Override
 	public int calc() {
-		final ForkJoinPool joinPool = new ForkJoinPool();
-		
+		final ForkJoinPool joinPool = new ForkJoinPool(parallelism);
+
 		ForkJoinTask<Integer> resultTask = joinPool.submit(task);
 
 		int result = 0;
-		
+
 		try {
 			result = resultTask.get();
 		} catch (InterruptedException | ExecutionException ex) {
 			Logger.getLogger(ForkJoinPractice.class.getName()).log(Level.SEVERE, null, ex);
 		}
-		
+
 		return result;
 	}
-	
 }
 
-class InvokeFibFunc extends CalcFibFunc{
+class InvokeFibFunc extends CalcFibFunc {
 
-	public InvokeFibFunc(int n) {
-		super(n);
+	public InvokeFibFunc(FibonacciTask task, int parallelism) {
+		super(task, parallelism);
 	}
 
 	@Override
 	public int calc() {
 		/* thrown ClassCastException */
 		//int result = task.compute();
-		final ForkJoinPool joinPool = new ForkJoinPool();
+		final ForkJoinPool joinPool = new ForkJoinPool(parallelism);
 		return joinPool.invoke(task);
 	}
-	
 }
 
-class NormalFibFunc extends CalcFibFunc{
+class NormalFibFunc extends CalcFibFunc {
 
-	public NormalFibFunc(int n) {
-		super(n);
+	public NormalFibFunc(FibonacciTask task, int parallelism) {
+		super(task, parallelism);
 	}
-	
-	private int getResult(int n){
-		if(n <= 1){
+
+	private int getResult(int n) {
+		if (n <= 1) {
 			return n;
 		}
-		
+
 		return getResult(n - 1) + getResult(n - 2);
 	}
 
 	@Override
 	public int calc() {
-		return getResult(task.getSource());
+		return getResult(task.getStart());
 	}
-
 }
-
