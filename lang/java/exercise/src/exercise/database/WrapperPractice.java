72e11c150b768ab196489c8af0775ab1c71fbf51
moguonyanko <moguo_50000_nyanko@yahoo.co.jp>
2013-08-13 03:26:46 +0900

    MySQL Wrapper practice add

---
+++ lang/java/exercise/src/exercise/database/WrapperPractice.java
@@ -0,0 +1,42 @@
+package exercise.database;
+
+import com.mysql.jdbc.MySQLConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class WrapperPractice {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		String url = "jdbc:mysql://localhost:3306/geolib";
+		String user = "geofw", pass = "geofw";
+		
+		/* usao is allowed 'SELECT' only */
+		String newUser = "usao", newPassword = "usao";
+
+		try (Connection con = DriverManager.getConnection(url, user, pass)) {
+			
+			if(con.isWrapperFor(MySQLConnection.class)){
+				MySQLConnection myCon = con.unwrap(MySQLConnection.class);
+				
+				myCon.changeUser(newUser, newPassword);
+				System.out.println("User changed " + newUser + " from " + user);
+				
+				Statement statement = myCon.createStatement();
+				/* Next query is denied */
+				statement.execute("INSERT INTO batchtest VALUES (0, 'sample0');");
+				System.out.println("INSERT finished.");
+			}
+			
+		} catch (SQLException ex) {
+			Logger.getLogger(WrapperPractice.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+}
