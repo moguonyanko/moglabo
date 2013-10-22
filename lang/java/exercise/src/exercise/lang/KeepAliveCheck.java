d1ca3c5d4ebf7e62d711171cb034938a3900621e
moguonyanko <moguo_50000_nyanko@yahoo.co.jp>
2013-10-23 03:25:40 +0900

    KeepAlive check sample add

---
+++ lang/java/exercise/src/exercise/lang/KeepAliveCheck.java
@@ -0,0 +1,23 @@
+package exercise.lang;
+
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class KeepAliveCheck {
+
+	public static void main(String[] args) {
+
+		Socket socket = new Socket();
+		/* socket default keep-alive is false because RFC1122. */
+		try {
+			socket.setKeepAlive(true);
+			System.out.println(socket.getInetAddress());
+			System.out.println(socket.getKeepAlive());
+		} catch (SocketException ex) {
+			Logger.getLogger(KeepAliveCheck.class.getName()).log(Level.SEVERE, null, ex);
+		}
+
+	}
+}
