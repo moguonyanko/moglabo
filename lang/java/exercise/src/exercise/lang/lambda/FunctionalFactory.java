e10fee618b26a4df2563a04951fb748ff0fbf999
KouichiYamada <moguo_50000_nyanko@yahoo.co.jp>
2014-03-31 01:16:07 +0900

    Supplier sample implement

--- lang/java/exercise/src/exercise/lang/lambda/FunctionalFactory.java
+++ lang/java/exercise/src/exercise/lang/lambda/FunctionalFactory.java
@@ -7,17 +7,18 @@ import java.util.function.Supplier;
 
 public class FunctionalFactory {
 
-	public static <T, DEST extends Collection<T>> DEST
+	public static <DEST extends Collection> DEST
 		packItems(Map<String, Integer> stuffs,
 			Supplier<DEST> packFactory) {
 
 		Set<String> keys = stuffs.keySet();
 
 		DEST result = packFactory.get();
-		for (String key : keys) {
-			ShopItem item = new ShopItem(key, stuffs.get(key));
-			//result.add(item); /* @todo compile error... */
-		}
+		
+		keys.stream().map((key) -> 
+		new ShopItem(key, stuffs.get(key))).forEach((item) -> {
+			result.add(item);
+		});
 
 		return result;
 	}
