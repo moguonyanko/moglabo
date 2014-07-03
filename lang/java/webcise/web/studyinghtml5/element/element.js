03860d861c864391869b3bf00c7d447dd6ed7f71
KouichiYamada <moguo_50000_nyanko@yahoo.co.jp>
2014-07-04 02:54:16 +0900

    add dataset sample

--- lang/java/webcise/web/studyinghtml5/element/element.js
+++ lang/java/webcise/web/studyinghtml5/element/element.js
@@ -14,7 +14,7 @@
 		function updateProgress() {
 			var currentValue = parseInt(progress.getAttribute("value")),
 				maxValue = parseInt(progress.getAttribute("max"));
-			
+
 			var value = currentValue + 1;
 
 			if (value > maxValue) {
@@ -26,7 +26,7 @@
 			}
 		}
 
-		function startProgress(){
+		function startProgress() {
 			intervalId = setInterval(updateProgress, interval);
 			progressSwitch.removeEventListener("click", startProgress, false);
 			progressSwitch.addEventListener("click", stopProgress, false);
@@ -43,8 +43,54 @@
 		};
 	}
 
+	function Dataset() {
+		var customDataList = ref("customdatalist"),
+			customDatas = customDataList.getElementsByTagName("li"),
+			outputArea = ref("outputarea"),
+			newAge = ref("newage"),
+			dispDatasetBtn = ref("displaydataset"),
+			setDataBtn = ref("setdataset");
+
+		function displayDataset(element, index, array) {
+			var name = element.textContent;
+			outputArea.value += name + ":" + element.dataset.age +
+				":" + element.dataset.from + "\n";
+		}
+
+		function setDatasset(element) {
+			var newDatas = {};
+			
+			newDatas.age = newAge.value;
+			
+			for(var name in newDatas){
+				element.dataset[name] = newDatas[name];
+			}
+		}
+		
+		function outputDataset() {
+			outputArea.value = "";
+			
+			//customDatas.forEach(displayData);
+			for(var i = 0, max = customDatas.length; i < max; i++){
+				displayDataset(customDatas[i], i, customDatas);
+			}
+		}
+		
+		function updateDataset(){
+			for(var i = 0, max = customDatas.length; i < max; i++){
+				setDatasset(customDatas[i]);
+			}
+		}
+		
+		this.init = function() {
+			dispDatasetBtn.addEventListener("click", outputDataset, false);
+			setDataBtn.addEventListener("click", updateDataset, false);
+		};
+	}
+
 	var myModules = {
-		progress: Progress
+		progress: Progress,
+		dataset : Dataset
 	};
 
 	function init() {
