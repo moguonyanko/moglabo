e4e02d1bfdade246d69a639e3fdda8429ec6f9c3
KouichiYamada <moguo_50000_nyanko@yahoo.co.jp>
2014-09-16 01:50:34 +0900

    add swift first sample

---
+++ lang/swift/claybox.playground/section-1.swift
@@ -0,0 +1,19 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+func greet(){
+    var str = "Hello, playground"
+    println(str)
+}
+
+
+
+// Entry point
+
+func main(){
+    greet()
+}
+
+main()
+
