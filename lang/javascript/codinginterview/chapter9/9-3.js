e37d037519bc7c61c415c99377a42cdf7ef46d1a
KouichiYamada <moguo_50000_nyanko@yahoo.co.jp>
2014-11-14 02:57:26 +0900

    modify search magic number function

--- lang/javascript/codinginterview/chapter9/9-3.js
+++ lang/javascript/codinginterview/chapter9/9-3.js
@@ -6,15 +6,25 @@
 			return -1;
 		}
 
-		var mid = Math.ceil((start + end) / 2);
+		var midIndex = Math.ceil((start + end) / 2),
+			midValue = array[midIndex];
 
-		if (array[mid] === mid) {
-			return mid;
-		} else if (array[mid] > mid) {
-			return magicFast(array, start, mid - 1);
-		} else {
-			return magicFast(array, mid + 1, end);
+		if (midValue === midIndex) {
+			return midIndex;
 		}
+
+		var leftIndex = Math.min(midIndex - 1, midValue),
+			left = magicFast(array, start, leftIndex);
+		if (left >= 0) {
+			return left;
+		}
+
+		/* 左側を探し切ってしまった。 */
+
+		var rightIndex = Math.max(midIndex + 1, midValue),
+			right = magicFast(array, rightIndex, end);
+
+		return right;
 	}
 
 	function magic(array) {
@@ -24,8 +34,11 @@
 	/**
 	 * test
 	 */
-	var sample = [-40, -20, -1, 1, 2, 3, 5, 7, 9, 12, 13];
-	var expected = 7;
+	//var sample = [-40, -20, -1, 1, 2, 3, 5, 7, 9, 12, 13];
+	//var expected = 7;
+	var sample = [-10, -5, 2, 2, 2, 3, 4, 7, 9, 12, 13];
+	var expected = 2; /* 左側から探しているので7ではなく2が返る。 */
+	
 	var actual = magic(sample);
 
 	g.assertEquals(expected, actual);
