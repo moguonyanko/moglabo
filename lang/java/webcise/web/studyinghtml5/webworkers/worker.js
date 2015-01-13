c1387bbb5d6cca1888d6d49b8fc4ffa5b9c20552
KouichiYamada <moguo_50000_nyanko@yahoo.co.jp>
2015-01-14 02:39:47 +0900

    postMessage modified

--- lang/java/webcise/web/studyinghtml5/webworkers/worker.js
+++ lang/java/webcise/web/studyinghtml5/webworkers/worker.js
@@ -4,12 +4,11 @@
  * エラーになります。
  */
 
-self.onmessage = function (evt) {
-	var n = parseInt(evt.data);
-	var result = fib(n);
-	self.postMessage(result);
-};
-
+/**
+ * Windowオブジェクトの直下に保存されるように
+ * このスクリプトが読み込まれれば，この関数は
+ * 外部スクリプトから参照可能になる。
+ */
 function fib(n) {
 	if (n <= 1) {
 		return n;
@@ -17,3 +16,41 @@ function fib(n) {
 
 	return fib(n - 1) + fib(n - 2);
 }
+
+function download() {
+	var xhr = new XMLHttpRequest(),
+		async = false;
+
+	/* Worker内部では同期リクエストしてもWebページの動作をブロックしない。 */
+	xhr.open("GET", "sample.json", async);
+	xhr.send();
+	if (xhr.status === 200) {
+		/**
+		 * JSONオブジェクトのメソッドでシリアライズできるオブジェクトであれば
+		 * postMessageの引数に渡すことができる。
+		 */
+		self.postMessage({
+			result: xhr.responseText
+		});
+	}
+}
+
+/**
+ * 以下の関数群は本来は各々のスクリプトに分けるべきである。
+ */
+var messageHandler = {
+	fib: function (arg) {
+		var n = parseInt(arg);
+		var result = fib(n);
+		self.postMessage({
+			result: result
+		});
+	},
+	download: download
+};
+
+self.onmessage = function (evt) {
+	var handlerName = evt.data.handler,
+		handler = messageHandler[handlerName];
+	handler(evt.data.arg);
+};
