f30da3550a129608f89b7df1111b2cb140f15bce
KouichiYamada <k-yamada@tg-inet.co.jp>
2015-03-25 12:26:57 +0900

    added JSON filter

--- lang/java/webcise/web/studyinghtml5/javascript/json.js
+++ lang/java/webcise/web/studyinghtml5/javascript/json.js
@@ -3,7 +3,7 @@
 	
 	function getWeightFilter(border){
 		
-		return function weightFilter(name, value){
+		return function(name, value){
 			if(name === "weight"){
 				if(value >= border){
 					return value;
@@ -17,31 +17,60 @@
 		};
 	}
 	
-	var filters = {
-		parse : {
-			weight : getWeightFilter
-		},
-		stringify : {
-			
-		}
+	function defaultFilter(name, value){
+		return value;
+	}
+	
+	var filterFactories = {
+		weight : getWeightFilter
 	};
+	
+	function getFilter(){
+		var eles = m.refs("JSONFilters"),
+			borderEle = m.ref("JSONBorderValue");
+		
+		var ele;
+		for(var i = 0, len = eles.length; i < len; i++){
+			if(eles[i].checked){
+				ele = eles[i];
+				break;
+			}
+		}
+		
+		if(ele && typeof filterFactories[ele.value] === "function"){
+			return filterFactories[ele.value](borderEle.value);
+		}else{
+			return defaultFilter;
+		}
+	}
 
 	function getSampleJSON() {
-		var container = m.ref("SampleJSONObject");
-		m.log(container);
-		return JSON.parse(container.text);
+		var container = m.ref("SampleJSONObject"),
+			/**
+			 * pre要素内の文字列はTextNodeのnodeValueとして
+			 * 保存されている。
+			 */
+			jsonText = container.firstChild.nodeValue;
+		
+		var filter = getFilter();
+		
+		return JSON.parse(jsonText, filter);
 	}
 
 	function output(o) {
 		var area = m.ref("JSONResultArea");
-		m.println(area, o);
+		m.println(area, o, true);
 	}
 
 	(function init() {
 		m.addListener(m.ref("DisplayParsedJSON"), "click", function() {
 			var json = getSampleJSON();
+			
 			m.log(json);
-			output(json);
+			
+			var indent = 4;
+			var strJson = JSON.stringify(json, null, indent);
+			output(strJson);
 		});
 
 		m.export("jsonNS", {});
