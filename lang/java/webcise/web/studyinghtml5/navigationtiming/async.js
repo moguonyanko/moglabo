22fd398da7ba1e67fbad3bd035f964af8aa170f2
KouichiYamada <k-yamada@tg-inet.co.jp>
2015-04-06 13:11:57 +0900

    modified async script message

--- lang/java/webcise/web/studyinghtml5/navigationtiming/async.js
+++ lang/java/webcise/web/studyinghtml5/navigationtiming/async.js
@@ -1,5 +1,5 @@
 /**
- * 非同時スクリプト読み込み時のパフォーマンス調査用スクリプトです。
+ * 非同時スクリプト読み込み時の動作確認用スクリプトです。
  * 
  */
 
@@ -8,10 +8,22 @@
 
 	(function() {
 
+		m.log("asyncスクリプト");
+
 		var message = "ここでコストの大きい非同期処理を行うと，" + 
 			"DOMContentLoadedイベント完了時間と" +
 			"loadイベント完了時間の差を大きくなる。";
 		m.log(message);
+		
+		var readyState = document.readyState,
+			stateLog = "document.readyState:" + readyState;
+		m.log(stateLog);
+		
+		m.log("document.readyState:interactiveでdocument.write()を呼び出しても無視される。");
+		document.write("<p>async test page</p>");
+		document.write("<p>" + stateLog + "</p>");
+		
+		m.log("document.readyState:complete以降にdocument.write()を呼び出すとページが上書きされる。");
 
 	}());
 
