cae107f40f7829fa98b1abc6d4ce1a1b557e1eb8
KouichiYamada <k-yamada@tg-inet.co.jp>
2015-04-09 14:14:49 +0900

    added drag image

--- lang/java/webcise/web/studyinghtml5/draganddrop/draganddrop.js
+++ lang/java/webcise/web/studyinghtml5/draganddrop/draganddrop.js
@@ -3,35 +3,78 @@
 
 	var dragContainer = m.ref("draggable-container"),
 		dropContainer = m.ref("drop-target-container");
+		
+	/**
+	 * reference:
+	 * https://developer.mozilla.org/ja/docs/DragDrop/Drag_Operations
+	 */
+	function getDragImage(){
+		var dragImage = doc.createElementNS("http://www.w3.org/1999/xhtml", "html:canvas");
+		dragImage.width = 50;
+		dragImage.height = 50;
 
-	function dragStart(evt){
+		var imgContext = dragImage.getContext("2d");
+		imgContext.lineWidth = 4;
+		imgContext.moveTo(0, 0);
+		imgContext.lineTo(50, 50);
+		imgContext.moveTo(0, 50);
+		imgContext.lineTo(50, 0);
+		imgContext.stroke();
+		
+		return dragImage;
+	}
+
+	function setDragWithCustomImage(dataTransfer){
+		var img = getDragImage(),
+			xOffset = 25,
+			yOffset = 25;
+
+		/**
+		 * DataTransfer.setDragImageの第1引数にはImage以外に
+		 * Canvasも指定することができる。
+		 */
+		dataTransfer.setDragImage(img, xOffset, yOffset);
+	}
+
+	function dragStart(evt) {
 		var dt = evt.dataTransfer;
 		dt.setData("text/plain", evt.target.id);
+		setDragWithCustomImage(dt);
+		
+		m.log("DataTransfer.dropEffect=" + dt.dropEffect);
+		/**
+		 * effectAllowedを指定しなくてもドラッグアンドドロップできる。
+		 * noneを設定するとドラッグアンドドロップできなくなる。
+		 */
 		dt.effectAllowed = "copyMove";
 	}
-	
-	function dragOver(evt){
+
+	function dragOver(evt) {
 		evt.preventDefault();
 	}
-	
-	function dragEnd(evt){
+
+	function dragEnd(evt) {
 		var dt = evt.dataTransfer;
-		if(dt.dropEffect === "move"){
+		if (dt.dropEffect === "move") {
 			var target = evt.target,
 				txt = "<p>ドラッグされました</p>";
 			m.print(target, txt, true);
 		}
 	}
-	
-	function drop(evt){
+
+	function drop(evt) {
 		var dt = evt.dataTransfer;
 		var targetId = dt.getData("text/plain");
-		var draggedEle = m.ref(targetId);
-		/**
-		 * ドラッグされた要素はDataTransfer.effectAllowedが
-		 * copyであっても元の要素から自動的に削除される。
-		 */
-		evt.target.appendChild(draggedEle);
+
+		if (targetId) {
+			var draggedEle = m.ref(targetId);
+			/**
+			 * ドラッグされた要素はDataTransfer.effectAllowedが
+			 * copyであっても元の要素から自動的に削除される。
+			 */
+			evt.target.appendChild(draggedEle);
+		}
+		
 		/**
 		 * preventDefaultしないとブラウザがドラッグされた要素の
 		 * テキストをURLと解釈してページ遷移しようとする。
