4c65db5ee95dcb47f5db1ccfed5a416f5bd5d62b
KouichiYamada <k-yamada@tg-inet.co.jp>
2015-04-30 12:21:00 +0900

    shorten Consumer sample

--- lang/java/exercise/src/exercise/function/util/Functions.java
+++ lang/java/exercise/src/exercise/function/util/Functions.java
@@ -6,6 +6,6 @@ import java.util.function.Consumer;
 public class Functions {
 
 	public static <T> void dump(Collection<T> targets, Consumer<? super T> dumpAction) {
-		targets.forEach(target -> dumpAction.accept(target));
+		targets.forEach(dumpAction::accept);
 	}
 }
