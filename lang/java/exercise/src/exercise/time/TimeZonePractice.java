4a497c146a0ed5d5e8704c3fddfea6a7c335b30f
KouichiYamada <k-yamada@tg-inet.co.jp>
2015-06-30 16:05:28 +0900

    added zoned time sample

---
+++ lang/java/exercise/src/exercise/time/TimeZonePractice.java
@@ -0,0 +1,29 @@
+package exercise.time;
+
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class TimeZonePractice {
+
+	public static ZonedDateTime now(ZoneId toZoneId){
+		LocalDateTime localDateTime = LocalDateTime.now();
+		return ZonedDateTime.of(localDateTime, toZoneId);
+	} 
+	
+	public static OffsetDateTime getOffsetDateTime(ZoneId fromZoneId, 
+		ZoneId toZoneId){
+		LocalDateTime fromLocalDateTime = LocalDateTime.now(fromZoneId);
+		LocalDateTime toLocalDateTime = LocalDateTime.now(toZoneId);
+		/**
+		 * ZonedDateTime.atZone(ZoneId)はLocalDateTimeにZoneIdを
+		 * 付け足すだけである。
+		 * Periodは時間を考慮しない。時間を考慮した期間を得たければDurationを使う。
+		 */
+		ZonedDateTime zonedDateTime = ZonedDateTime.of(fromLocalDateTime, fromZoneId)
+			.withZoneSameInstant(toZoneId);
+		return OffsetDateTime.of(toLocalDateTime, zonedDateTime.getOffset());
+	}
+	
+}
--- lang/java/exercise/test/test/exercise/time/TestTimePractice.java
+++ lang/java/exercise/test/test/exercise/time/TestTimePractice.java
@@ -2,19 +2,25 @@ package test.exercise.time;
 
 import java.time.DayOfWeek;
 import java.time.format.TextStyle;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
-import static org.hamcrest.CoreMatchers.is;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
 
 import exercise.time.DayOfWeekPractice;
 import exercise.time.LocalPractice;
-import java.time.LocalDate;
-import java.time.Month;
+import exercise.time.TimeZonePractice;
 
 public class TestTimePractice {
 	
@@ -66,6 +72,46 @@ public class TestTimePractice {
 		LocalDate actual = LocalPractice.lastDayOfMonth(date);
 		
 		assertThat(actual, is(expected));
+	}
+	
+	private static DateTimeFormatter getPatternForTest(){
+		return DateTimeFormatter.ofPattern("yyyy MM dd hh mm");
+	}
+	
+	@Test
+	public void 現在時刻をタイムゾーン付きで得る(){
+		ZonedDateTime expected = ZonedDateTime.now();
+		ZoneId zoneId = ZoneId.of("Asia/Tokyo");
+		ZonedDateTime actual = TimeZonePractice.now(zoneId);
+		
+		/**
+		 * 秒及びミリ秒はテストする要素に含めたくないが分までは含めたいので，
+		 * ZonedDateTime.toLocalDate()の戻り値で比較するのは避け，
+		 * ZonedDateTime.format(DateTimeFormatter)の戻り値で比較を行っている。
+		 */
+		DateTimeFormatter formatter = getPatternForTest();
+		assertThat(actual.format(formatter), is(expected.format(formatter)));
+	}
+	
+	@Test
+	public void 時差を考慮した時刻を得る(){
+		ZoneId fromZoneId = ZoneId.of("Asia/Tokyo");
+		ZoneId toZoneId = ZoneId.of("Europe/Berlin");
+		
+		/* 時差が +07:00 ある。 */
+		ZonedDateTime berlinZonedDateTime = LocalDateTime.now()
+			.minusHours(7)
+			.atZone(toZoneId);
+		OffsetDateTime expected = berlinZonedDateTime.toOffsetDateTime();
+		
+		OffsetDateTime actual = TimeZonePractice.getOffsetDateTime(fromZoneId, toZoneId);
+		
+		/**
+		 * ミリ秒を比較されるとテストに失敗する。
+		 * 秒を含まずにテストするように書式化を行う。
+		 */
+		DateTimeFormatter formatter = getPatternForTest();
+		assertThat(actual.format(formatter), is(expected.format(formatter)));
 	}
 	
 }
