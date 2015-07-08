b4309c5ddbf67e7cc1c3d4a8bab5ab5c4c344884
KouichiYamada <k-yamada@tg-inet.co.jp>
2015-07-08 12:35:01 +0900

    modified route search method

--- lang/java/exercise/src/exercise/function/util/Grapher.java
+++ lang/java/exercise/src/exercise/function/util/Grapher.java
@@ -1,61 +1,88 @@
 package exercise.function.util;
 
+import java.util.function.Predicate;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * 参考：
- * 「アルゴリズムとデータ構造」(SoftbankCreative) 
+ * 「アルゴリズムとデータ構造」(SoftbankCreative)
  */
 public class Grapher {
+
+	private static class MinElement {
+
+		private final int index;
+		private int cost;
+
+		public MinElement(int index, int cost) {
+			this.index = index;
+			this.cost = cost;
+		}
+
+		public void setCost(int cost) {
+			this.cost = cost;
+		}
+
+	}
 
 	/**
 	 * ダイクストラ法による経路探索を行う。
 	 * グラフは隣接行列として表現している。
+	 *
+	 * @todo
+	 * 経路を返していない。
+	 * 
+	 * IntStreamを使い過ぎでは。型がIntegerに固定されるので
+	 * mapを適用しにくくなってしまう。
+	 *
 	 */
-	public static int[] routeSearch(final int[][] graph, final int startIndex){
+	public static int[] routeSearch(final int[][] graph, final int startIndex) {
 		int elementSize = graph.length;
-		int[] currentCost = new int[elementSize];
+		final int[] currentCost = new int[elementSize];
 		boolean[] fix = new boolean[elementSize];
-		
+		int defaultMinCost = -1;
+
 		IntStream.range(0, elementSize)
 			.forEach(idx -> {
-				currentCost[idx] = -1;
+				currentCost[idx] = defaultMinCost;
 				fix[idx] = false;
 			});
-		
+
 		currentCost[startIndex] = 0;
-		
-		while(true){
-			int minElement = -1;
-			int minCost = -1;
-			
-			for(int i = 0; i < elementSize; i++){
-				if(!fix[i] && currentCost[i] != -1){
-					if(minCost == -1 || minCost > currentCost[i]){
-						minCost = currentCost[i];
-						minElement = i;
-					}
-				}
-			}
-			
-			if(minCost == -1){
+
+		while (true) {
+			int minElement = IntStream.range(0, elementSize)
+				.filter(i -> !fix[i] && currentCost[i] != -1)
+				.reduce((element, nextElement)
+					-> currentCost[element] > currentCost[nextElement]
+					? nextElement : element)
+				.orElse(Integer.MIN_VALUE);
+			int minCost = minElement > Integer.MIN_VALUE
+				? currentCost[minElement] : defaultMinCost;
+
+			if (minCost == defaultMinCost) {
 				break;
 			}
 			
-			for(int i = 0; i < elementSize; i++){
-				if(!fix[i] && graph[minElement][i] > 0){
-					int newCost = minCost + graph[minElement][i];
-					
-					if(currentCost[i] == -1 || currentCost[i] > newCost){
-						currentCost[i] = newCost;
+			IntStream.range(0, elementSize)
+				.filter(i -> !fix[i] && graph[minElement][i] > 0)
+				.forEach(element -> {
+					int newCost = minCost + graph[minElement][element];
+
+					if (currentCost[element] == -1 || currentCost[element] > newCost) {
+						currentCost[element] = newCost;
 					}
-				}
-			}
-			
+				});
+
 			fix[minElement] = true;
 		}
-		
+
 		return currentCost;
 	}
-	
+
+	public static int[] routeSearch(final int[][] graph) {
+		return routeSearch(graph, 0);
+	}
+
 }
