fa2812bd40afc02b8f961ff83dd8fe9ce93e008b
KouichiYamada <k-yamada@tg-inet.co.jp>
2015-08-10 16:00:16 +0900

    modified promise error handling

--- lang/java/webcise/web/ecmascript6/promise/promise.js
+++ lang/java/webcise/web/ecmascript6/promise/promise.js
@@ -3,15 +3,24 @@
 
 	var resultArea = m.ref("promise-result-area"),
 		timeoutEle = m.ref("promise-timeout-seconds");
-		
+
+	var separator = ",";
+
+	/**
+	 * Promiseの処理中にエラーが発生した時はthenに渡した関数は
+	 * 自分で直接呼び出していない限り呼び出されない。
+	 */
 	function resolve(res) {
 		m.println(resultArea, "計算結果=" + res.result);
-		m.log(new Date() + ":Promiseによる計算が成功しました。");
 	}
 
+	/**
+	 * Promiseの処理中にエラーが発生した時はどこで発生した場合でも
+	 * rejectが呼び出される。この時の引数はErrorあるいはそのサブタイプの
+	 * オブジェクトになっている。
+	 */
 	function reject(err) {
-		m.println(resultArea, err);
-		m.log(new Date() + ":Promiseによる計算が失敗しました。\n" + err.message);
+		m.println(resultArea, err.message);
 		/**
 		 * Firefox39ではthrowされたErrorがブラウザのデバッガに通知されるまでに
 		 * 数秒から数十秒かかる。
@@ -19,8 +28,73 @@
 		throw err;
 	}
 
+	/**
+	 * thenに渡す関数はPromiseのコンストラクタ関数に渡した関数内で
+	 * 直接呼び出していなくも，Promiseの処理が終われば呼び出される。
+	 * この時の呼び出される関数の引数はundefinedになっている。
+	 */
+	function logging(res) {
+		var date = new Date(),
+			status = res ? res.status : "",
+			msg = res ? res.message : "";
+
+		var logs = [
+			date,
+			"Promiseによる計算が終了しました。",
+			msg
+		];
+
+		if (status) {
+			logs.push(status);
+		}
+
+		m.log(logs.join(separator));
+	}
+
+	function PromiseError(baseMsg, result) {
+		this.result = result || {};
+		//this.message = PromiseError.formatMessage(baseMsg, this.result);
+		this.rawMessage = baseMsg;
+	}
+
+	/**
+	 * definePropertiesを使わず以下の静的メソッドを使うことでも
+	 * エラーメッセージの整形は行える。ただし静的メソッドだと公開されてしまう。
+	 * 静的メソッドが完全にオブジェクト内部で使うためだけのヘルパーメソッドで
+	 * あった場合はdefinePropertiesを使う方が良いかもしれない。
+	 */
+//	PromiseError.formatMessage = function(message, result) {
+//		var msg = [
+//			message,
+//			result.status
+//		];
+//
+//		return msg.join(" ");
+//	};
+
+	PromiseError.prototype = Object.create(Error.prototype);
+
+	Object.defineProperties(PromiseError.prototype, {
+		message : {
+			get : function() {
+				var msg = [
+					this.rawMessage,
+					this.getStatus()
+				];
+
+				return msg.join(" ");
+			},
+			enumerable : true, 
+			configurable : false
+		}
+	});
+
+	PromiseError.prototype.getStatus = function() {
+		return this.result.status;
+	};
+
 	function promiseFunc(resolve, reject) {
-		m.log(new Date() + ":Promiseの準備を開始します。");
+		m.log(new Date() + separator + "Promiseの準備を開始します。");
 
 		var urlParts = [
 			"/webcise/Calculator?"
@@ -43,25 +117,22 @@
 					if (res.status === 200) {
 						resolve(res);
 					} else {
-						var err = new Error("calculation failed, status=" + res.status);
-						reject(err);
+						reject(new PromiseError("calculation failed", res));
 					}
 				} catch (parseErr) {
 					/**
 					 * @todo
 					 * こちらのrejectが呼び出されるとontimeout等のハンドラが呼び出されない。
 					 */
-					reject(parseErr);
+					reject(new PromiseError(parseErr.message));
 				}
 			}
 		};
 		xhr.onerror = function() {
-			var err = new Error("http error!");
-			reject(err);
+			reject(new PromiseError("http error!"));
 		};
 		xhr.ontimeout = function() {
-			var err = new Error("timeout!");
-			reject(err);
+			reject(new PromiseError("timeout!"));
 		};
 		xhr.send(null);
 	}
@@ -69,12 +140,18 @@
 	function run() {
 		var promise = new Promise(promiseFunc);
 
-		m.log(new Date() + ":Promiseが作成されました。");
+		m.log(new Date() + separator + "Promiseが作成されました。");
 
+		/**
+		 * Promiseの成否に関わらず呼び出したい関数はthenとcatchの両方に
+		 * 渡しておかなければならない。finally的な関数は無いのか？
+		 */
 		promise.then(resolve)
-			.catch(reject);
+			.then(logging)
+			.catch(reject)
+			.catch(logging);
 
-		m.log(new Date() + ":Promiseの関数を設定しました。");
+		m.log(new Date() + separator + "Promiseの関数を設定しました。");
 	}
 
 	(function() {
