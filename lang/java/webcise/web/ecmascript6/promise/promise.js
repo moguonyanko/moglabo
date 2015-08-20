826dd21e58fbe13954b7d30c86294108a36257fd
KouichiYamada <k-yamada@tg-inet.co.jp>
2015-08-20 18:23:56 +0900

    added asynchronous promise sample

--- lang/java/webcise/web/ecmascript6/promise/promise.js
+++ lang/java/webcise/web/ecmascript6/promise/promise.js
@@ -5,9 +5,9 @@
 		timeoutEle = m.ref("promise-timeout-seconds");
 
 	var separator = ",";
-	
+
 	var resultCanvases = m.selectAll(".promise-result-canvas");
-	
+
 	/**
 	 * Promiseの処理中にエラーが発生した時はthenに渡した関数は
 	 * 自分で直接呼び出していない限り呼び出されない。
@@ -190,7 +190,7 @@
 
 		m.log(new Date() + separator + "Promiseの関数を設定しました。");
 	}
-	
+
 	/**
 	 * Promiseの拡張テスト
 	 */
@@ -203,43 +203,43 @@
 //		this.then(fn);
 //		this.catch(fn);
 //	};
-	
-	function preparePromise(chainEle){
+
+	function preparePromise(chainEle) {
 		/**
 		 * Promise.resolveやPromise.rejectだと
 		 * 成功・失敗毎のPromiseを生成することになる。
 		 */
-		if(chainEle.checked){
+		if (chainEle.checked) {
 			return Promise.resolve({
-				then: function(resolve, reject){
+				then : function(resolve, reject) {
 					resolve(chainEle.value + " Promise 成功");
 				}
 			});
-		}else{
+		} else {
 			return Promise.reject({
-				then: function(resolve, reject){
+				then : function(resolve, reject) {
 					reject(chainEle.value + " Promise 失敗");
 				}
 			});
 		}
 	}
-	
-	function getChainPromise(chainEle){
-		return new Promise(function(resolve, reject){
-			if(chainEle.checked){
+
+	function getChainPromise(chainEle) {
+		return new Promise(function(resolve, reject) {
+			if (chainEle.checked) {
 				resolve(chainEle.value + " Promise 成功");
-			}else{
+			} else {
 				reject(chainEle.value + " Promise 失敗");
 			}
 		});
 	}
-	
+
 	function chainPromises() {
 		var chains = m.refs("promise-state-check"),
 			ps = Array.prototype.map.call(chains, getChainPromise);
-		
+
 		m.log(ps);
-		
+
 		/* Firefox39で試したところletがシンタックスエラーになった。 */
 //		let [p1, p2, p3] = ps;
 //		
@@ -253,9 +253,9 @@
 			m.println(resultArea, value);
 			return value;
 		};
-		
-		var returnPromiseFunc = function(fn, promise){
-			return function(value){
+
+		var returnPromiseFunc = function(fn, promise) {
+			return function(value) {
 				fn(value);
 				return promise;
 			};
@@ -280,100 +280,250 @@
 			}
 		}
 	}
-	
-	function drawImage(img, onScreenCanvas){
+
+	function drawImage(img, onScreenCanvas) {
 		var offScreenCanvas = doc.createElement("canvas");
 		offScreenCanvas.width = onScreenCanvas.width;
 		offScreenCanvas.height = onScreenCanvas.height;
 		var offCtx = offScreenCanvas.getContext("2d");
 		offCtx.drawImage(img, 0, 0);
-		
+
 		var onCtx = onScreenCanvas.getContext("2d");
 		onCtx.clearRect(0, 0, onScreenCanvas.width, onScreenCanvas.height);
 
 		var offImg = offCtx.getImageData(0, 0, offScreenCanvas.width, offScreenCanvas.height);
 		onCtx.putImageData(offImg, 0, 0);
 	}
-	
-	function createImage(url, onloadCallback, onerrorCallback){
+
+	function createImage(url, onloadCallback, onerrorCallback) {
 		var img = new Image();
-		img.onload = function(evt){
+		img.onload = function(evt) {
 			onloadCallback(this);
 		};
 		img.onerror = onerrorCallback;
-		
+
 		var noCache = new Date().getTime();
 		img.src = url + "?" + noCache;
 	}
-	
-	function clearPromiseImage(){
-		Array.prototype.forEach.call(resultCanvases, function(cvs){
+
+	function clearPromiseImage() {
+		Array.prototype.forEach.call(resultCanvases, function(cvs) {
 			cvs.getContext("2d").clearRect(0, 0, cvs.width, cvs.height);
 		});
 	}
-	
-	function drawPromiseImage(){
-		var p1 = new Promise(function(resolve, reject){
+
+	function drawPromiseImage() {
+		var p1 = new Promise(function(resolve, reject) {
 			createImage("star1.jpg", resolve, reject);
 		});
-		var p2 = new Promise(function(resolve, reject){
+		var p2 = new Promise(function(resolve, reject) {
 			createImage("star2.jpg", resolve, reject);
 		});
-		var p3 = new Promise(function(resolve, reject){
+		var p3 = new Promise(function(resolve, reject) {
 			createImage("star3.jpg", resolve, reject);
 		});
-		
+
 		/**
 		 * Promise.allの引数の配列要素の順序はthenが受け取る配列要素の順序と
 		 * 一致する。各Promiseの処理は並列で行われているのか，コールバックを
 		 * 入れ子にした時のように順次行われているのかは不明である。
 		 */
 		var pAll = Promise.all([p1, p2, p3]);
-		
-		pAll.then(function(values){
-			values.forEach(function(img, idx, imgs){
+
+		pAll.then(function(values) {
+			values.forEach(function(img, idx, imgs) {
 				drawImage(img, resultCanvases[idx]);
 			});
 		});
 	}
-	
-	function downloadPromiseRace(){
+
+	function downloadPromiseRace() {
 		var imgContainer = m.ref("promise-race-result");
 		imgContainer.innerHTML = "";
 		var checks = m.refs("promise-download-image-check");
-		
-		var ps = Array.prototype.map.call(checks, function(el){
-			var imgName = el.value; 
-			if(!el.checked){
+
+		var ps = Array.prototype.map.call(checks, function(el) {
+			var imgName = el.value;
+			if (!el.checked) {
 				imgName += "_reject";
 			}
 			imgName += ".jpg";
-			
-			var p = new Promise(function(resolve, reject){
+
+			var p = new Promise(function(resolve, reject) {
 				var img = new Image();
-				img.onload = function(evt){
+				img.onload = function(evt) {
 					resolve(this);
 				};
-				img.onerror = function(iamge){
+				img.onerror = function(image) {
 					reject(new Error(this.src + "のダウンロードに失敗しました。"));
 				};
 				img.src = imgName;
 			});
-			
+
 			return p;
 		});
-		
+
 		var allPs = Promise.race(ps);
-		
+
 		/**
 		 * 先に成功するPromiseがあれば，その後で失敗するPromiseがあったとしても
 		 * thenだけが処理されcatchは無視される。
 		 */
-		allPs.then(function(value){
+		allPs.then(function(value) {
 			imgContainer.appendChild(value);
-		}).catch(function(err){
+		}).catch(function(err) {
 			m.println(resultArea, err.message);
 		});
+	}
+
+	function sendRequest(ws, startNumber, limit) {
+		ws.send(startNumber + "," + limit);
+	}
+
+	function createWebSockect() {
+		var host = location.host,
+			port = 8080;
+
+		return new WebSocket("ws://" + host + ":" + port + "/webcise/prime");
+	}
+
+	function WSResult(ws, pid, pm) {
+		this.ws = ws;
+		this.promiseId = pid;
+		this.param = parseInt(pm);
+
+//				Object.defineProperties(this, {
+//					id : {
+//						value : "Resend:" + this.promiseId
+//					},
+//					webSocket : {
+//						get : function() {
+//							if (this.isOpen()) {
+//								return this.ws;
+//							} else {
+//								return createWebSockect();
+//							}
+//						}
+//					}
+//				});
+	}
+
+	WSResult.prototype = {
+		close : function() {
+			this.ws.close();
+		},
+		isOpen : function() {
+			return this.ws.readyState === WebSocket.OPEN;
+		},
+		inc : function() {
+			/* paramが文字だと1が文字列連結されることに注意せよ。 */
+			//this.param += 1;
+			return ++this.param;
+		},
+		getPromiseId : function() {
+			return this.promiseId;
+		},
+		getWebSocket : function() {
+			if (this.isOpen()) {
+				return this.ws;
+			} else {
+				return createWebSockect();
+			}
+		}
+	};
+
+//			Object.defineProperty(WSResult.prototype, "id", {
+//				value : "Resend:" + this.promiseId
+//			});
+
+	function WSError(ws) {
+		this.ws = ws;
+	}
+
+	WSError.prototype = Object.create(Error.prototype);
+
+	WSError.prototype.close = function() {
+		this.ws.close();
+	};
+
+	function getPrimePromise(promiseId, param, lim, existingWs) {
+		var p = new Promise(function(resolve, reject) {
+			/**
+			 * Promiseで包んでやることでWebSocketを大きなスコープで管理する必要が
+			 * 無くなる。
+			 */
+			var ws = existingWs || createWebSockect();
+
+			ws.onopen = function() {
+				m.println(resultArea, "Promise" + promiseId + " 接続開始");
+				sendRequest(this, param, lim);
+			};
+
+			ws.onclose = function(evt) {
+				var reason = "code " + evt.code + " reason " + evt.reason;
+				var txt = "Promise" + promiseId + "を[" + reason + "]で接続を閉じました。\n";
+				txt += "意図的な切断" + (evt.wasClean ? "です。" : "ではありません。") + "\n";
+				m.println(resultArea, txt);
+			};
+
+			ws.onmessage = function(evt) {
+				m.println(resultArea, "Promise" + promiseId + " 計算結果▼");
+				var res = JSON.parse(evt.data);
+				m.println(resultArea, res.result);
+				resolve(new WSResult(this, promiseId, param));
+				sendRequest(this, param, lim);
+			};
+
+			ws.onerror = function(evt) {
+				m.println(resultArea, evt);
+				reject(new WSError(this));
+			};
+		});
+
+		return p;
+	}
+
+	function getPromiseLimitSize() {
+		return m.ref("promise-prime-limit-size").value;
+	}
+
+	function isPromisePrimeClosed() {
+		return m.ref("promise-prime-check").checked;
+	}
+
+	function getPrimes() {
+		var inputs = m.selectAll(".promise-prime-number");
+
+		var ps = Array.prototype.map.call(inputs, function(el) {
+			return getPrimePromise(el.id, el.value, getPromiseLimitSize());
+		});
+
+		var promiseAll = function(ps) {
+			var allPs = Promise.all(ps);
+
+			allPs.then(function(wss) {
+				if (isPromisePrimeClosed()) {
+					wss.forEach(function(wsRes) {
+						wsRes.close();
+					});
+				} else {
+					var newPs = wss.map(function(wsRes) {
+						return getPrimePromise(wsRes.getPromiseId(),
+							wsRes.inc(),
+							getPromiseLimitSize(),
+							wsRes.getWebSocket());
+					});
+
+					setTimeout(function() {
+						promiseAll(newPs);
+					}, 5000);
+				}
+			}).catch(function(wsErr) {
+				wsErr.close();
+			});
+		};
+
+		promiseAll(ps);
 	}
 
 	(function() {
@@ -382,13 +532,15 @@
 		m.addListener(m.ref("clear-promise-element"), "click", function() {
 			m.print(resultArea, "", true);
 		}, false);
-		
+
 		m.addListener(m.ref("promise-chain-runner"), "click", chainPromises, false);
-		
+
 		m.addListener(m.ref("promise-image-downloader"), "click", drawPromiseImage, false);
 		m.addListener(m.ref("promise-image-clearer"), "click", clearPromiseImage, false);
-		
+
 		m.addListener(m.ref("promise-race-downloader"), "click", downloadPromiseRace, false);
+
+		m.addListener(m.ref("promise-prime-calc-executer"), "click", getPrimes, false);
 	}());
 
 }(window, document, my));
