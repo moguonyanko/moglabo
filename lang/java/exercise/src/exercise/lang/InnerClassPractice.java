8d13fd55e86471d70442bf3542d855a624d7a98a
KouichiYamada <k-yamada@tg-inet.co.jp>
2015-10-09 15:42:46 +0900

    Added inner class and inner interface sample

--- lang/java/exercise/src/exercise/lang/InnerClassPractice.java
+++ lang/java/exercise/src/exercise/lang/InnerClassPractice.java
@@ -7,6 +7,16 @@ public class InnerClassPractice {
 	
 	public class Inner{
 		
+		private final int id;
+
+		public Inner() {
+			this.id = 0;
+		}
+		
+		public Inner(int id) {
+			this.id = id;
+		}
+		
 		public String getSampleString(){
 			return sampleString + " from Inner";
 		}
@@ -17,6 +27,78 @@ public class InnerClassPractice {
 		
 		public String getSampleString(){
 			return SAMPLE_STRING + " from InnerStatic";
+		}
+		
+	}
+	
+	/**
+	 * 同じインターフェースを実装したい内部クラスが複数存在する時，
+	 * 内部インターフェースは有用かもしれない。
+	 * 内部インターフェースはprivateにすることができる。
+	 * 当然privateにするとクラス外からそのインターフェースの型を
+	 * 参照することはできなくなる。
+	 */
+	private interface InnerComparable extends Comparable<InnerComparable>{
+		
+		/**
+		 * privateな内部インターフェースのメソッドであっても
+		 * privateを指定することはできない。
+		 */
+		int getInnerId();
+		
+		/**
+		 * Objectクラスのメソッドでなければdefault修飾子を指定して
+		 * オーバーライドすることは可能。
+		 */
+		@Override
+		default int compareTo(InnerComparable other){
+			return this.getInnerId() - other.getInnerId();
+		}
+		
+	};
+	
+	public class InnerComparableClass implements InnerComparable {
+		
+		private final Inner inner;
+
+		public InnerComparableClass(Inner inner) {
+			this.inner = inner;
+		}
+		
+		@Override
+		public int getInnerId(){
+			InnerComparable dummy = new InnerComparable() {
+
+				@Override
+				public int getInnerId() {
+					return -1000;
+				}
+
+				@Override
+				public int compareTo(InnerComparable o) {
+					return 0;
+				}
+
+				@Override
+				public String toString() {
+					return "Access private interface : " + InnerComparable.class.getCanonicalName();
+				}
+				
+			};
+			
+			System.out.println(dummy);
+			
+			return inner.id;
+		}
+		
+//		@Override
+//		public int compareTo(InnerComparable other){
+//			return this.getInnerId() - other.getInnerId();
+//		}
+
+		@Override
+		public String toString() {
+			return "My Inner id is " + getInnerId();
 		}
 		
 	}
--- lang/java/exercise/test/test/exercise/lang/TestInnerClass.java
+++ lang/java/exercise/test/test/exercise/lang/TestInnerClass.java
@@ -1,11 +1,18 @@
 package test.exercise.lang;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.function.Function;
+
 import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 import exercise.lang.InnerClassPractice;
 import exercise.lang.InnerClassPractice.Inner;
+import exercise.lang.InnerClassPractice.InnerComparableClass;
 import exercise.lang.InnerClassPractice.InnerStatic;
 
 public class TestInnerClass {
@@ -58,6 +65,73 @@ public class TestInnerClass {
 		
 		String expected = "sample string from anonymous" + suffix;
 		String actual = anonymous.getMyString();
+		
+		assertThat(actual, is(expected));
+	}
+	
+	private static class InnerDivSorter{
+		
+		private static final int DIV = 15;
+		
+		private static int getSortKey(InnerComparableClass ic){
+			/**
+			 * DIVで割った余りが小さい順にソートさせる。
+			 */
+			return ic.getInnerId() % DIV;
+		}
+		
+	}
+	
+	@Test
+	public void 内部インターフェースを実装したクラスのオブジェクト群をソートする(){
+		InnerClassPractice icp = new InnerClassPractice();
+		Inner inner150 = icp.new Inner(150);
+		Inner inner200 = icp.new Inner(200);
+		Inner inner100 = icp.new Inner(100);
+		
+		InnerComparableClass ic150 = icp.new InnerComparableClass(inner150);
+		InnerComparableClass ic200 = icp.new InnerComparableClass(inner200);
+		InnerComparableClass ic100 = icp.new InnerComparableClass(inner100);
+		
+		List<InnerComparableClass> ics = Arrays.asList(ic150, ic200, ic100);
+		
+		List<InnerComparableClass> expected = Arrays.asList(ic150, ic200, ic100);
+		
+		List<InnerComparableClass> actual = ics.stream()
+			.sorted()
+			/**
+			 * compareToを実装している内部インターフェースが
+			 * private指定されていたりすると，
+			 * <pre>
+			 * sorted(InnerComparableClass::compareTo)
+			 * </pre>
+			 * は<em>実行時に</em>」IllegalAccessErrorを発生させる。
+			 * アクセス不可能なコードをコンパイラが検出できないようだ。
+			 * 
+			 * privateな内部インターフェースにdefaultメソッドでcompareToを
+			 * 定義していなくてもIllegalAccessErrorになる。
+			 * privateな内部インターフェースを実装したpublicなクラスに
+			 * compareToがオーバーライドされていてもIllegalAccessErrorになる。
+			 * 
+			 * InnerComparableClass.getInnerIdは呼び出してもエラーにならない。
+			 * getInnerIdもprivateな内部インターフェースで宣言されているメソッドである。
+			 * getInnerId内で内部インターフェースの型を参照してもエラーにならない。
+			 */
+			//.sorted(InnerComparableClass::compareTo)
+			.peek(ic -> {
+				try{
+					/* 以下の行もIllegalAccessErrorを発生させる。 */
+					Function<InnerComparableClass, Integer> fn = ic::compareTo;
+					System.out.println(fn.getClass().getCanonicalName());
+				}catch(Throwable t){
+					System.err.println(t.getMessage());
+				}
+				/* comapreTo以外のメソッドは呼び出しても問題無し。 */
+				System.out.println(ic.getInnerId());
+			})
+			.sorted(Comparator.comparing(InnerDivSorter::getSortKey))
+			.peek(System.out::println)
+			.collect(Collectors.toList());
 		
 		assertThat(actual, is(expected));
 	}
