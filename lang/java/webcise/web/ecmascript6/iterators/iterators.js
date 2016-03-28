bdcda6441aea29d1a9548870db6d477b04a221cc
KouichiYamada <k-yamada@tg-inet.co.jp>
2016-03-28 16:58:13 +0900

    Added destructuring assignment sample to iterator sample

--- lang/java/webcise/web/ecmascript6/iterators/iterators.js
+++ lang/java/webcise/web/ecmascript6/iterators/iterators.js
@@ -38,6 +38,10 @@
             return this.fruits.map(fruit => fruit.displayName).toString();
         }
         
+        get length(){
+            return this.fruits.length;
+        }
+        
         /**
          * Iteration protocols
          * 実装方法自体は従来の自前のイテレータと変わらない。
@@ -110,6 +114,36 @@
             for(let fruit of [...fruitIterator]){
                 g.println(resultArea, fruit);
             }
+        },
+        destructuring: (fruitIterator, resultArea) => {
+            /**
+             * Spread Operatorを使用して代入を行う場合であっても
+             * 代入先配列がconstで宣言されていたらエラーになる。
+             * const sampleVars = [];
+             * sampleVars[0] = "1"; //ok
+             * [...sampleVars] = [1, 2, 3]; //error(sampleVarsがletで宣言されていればOK。)
+             */
+            
+            /**
+             * Iteration protocolsを実装している場合，保持している要素群を
+             * 構造化代入(Destructuring assignment)することができる。
+             * 
+             * FruitIteratorの保持するFruitを左辺の配列の各要素に構造化代入する。
+             * 左辺の配列は全ての要素を代入するのに必要な分だけ自動的に拡張される。
+             * 以下のコードでは配列の宣言も同時に行っている。宣言を同時に行う場合は
+             * constで宣言できる。宣言を分離した場合は上のコメントで述べた理由により
+             * letにしなければならない。
+             * 
+             * Chrome49では「(var)[Symbol.iterator] is not a function」と出力され
+             * エラーになる。配列の宣言を分離しても結果は変わらない。
+             */
+            const [...fruitVars] = fruitIterator;
+            
+            g.log(fruitVars);
+            
+            g.forEach(fruitVars, fruit => {
+                g.println(resultArea, fruit);
+            });
         }
     };
     
