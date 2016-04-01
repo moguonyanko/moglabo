86e06e511e4c52bcbfd30b868ad3aaa9a4847339
KouichiYamada <k-yamada@tg-inet.co.jp>
2016-04-01 18:51:58 +0900

    Added Map sample

--- lang/java/webcise/web/ecmascript6/mapandset/mapandset.js
+++ lang/java/webcise/web/ecmascript6/mapandset/mapandset.js
@@ -57,6 +57,80 @@
         }
     };
     
+    class SampleKey{
+        constructor(key) {
+            this.key = key;
+        }
+        
+        toString() {
+            return this.key;
+        }
+        
+        /**
+         * equalsを実装していてもMapの値取得には全く影響を与えない。
+         * 演算子 === で等しいと見なされるキーをMapに与えた時のみ値取得に成功する。
+         */
+        equals(other) {
+            if(other instanceof SampleKey){
+                return this.key === other.key;
+            }else{
+                return false;
+            }
+        }
+    }
+    
+    class MapAction{
+        constructor(key){
+            this.key = key;
+        }
+        
+        setValue(map, value) {
+            map.set(this.key, value);
+        }
+        
+        /**
+         * this.keyと等価なキーを外部から与えて値が取得できるかを
+         * テストするためのメソッドである。
+         */
+        getValue(map, otherKey) {
+            return map.get(otherKey);
+        }
+        
+        toString() {
+            return "キー " + this.key + " で値を取得します。";
+        }
+    }
+    
+    /**
+     * 今のところどのタイプのキーを用いる時でも常に同じ値を保存する。
+     */
+    const SAMPLE_MAP_VALUE = "値が取得できました！";
+    
+    const createMapKey = {
+        "string": () => "sample string",
+        "number": () => 1,
+        "boolean": () => true,
+        "function": () => () => 1,
+        "nan": () => NaN,
+        /**
+         * () => {} と記述するとブロックを持つArrow functionと区別が付かなくなる。
+         * そのためオブジェクトを返すArrow functionを記述したい時は
+         *  () => { return {}; } のように明示的にオブジェクトをreturnしなければ
+         * ならない。
+         */
+        "object": () => { return { toString: () => "object literal" }; },
+        "class": () => new SampleKey("sample key")
+    };
+    
+    let mapActions = {};
+    
+    const initMapActions = types => {
+        for(let type of types){
+            const mapKey = createMapKey[type]();
+            mapActions[type] = new MapAction(mapKey);
+        }
+    };
+            
     const initializers = [
         g => {
             const resultArea = g.select(".set-container .result-area");
@@ -110,6 +184,46 @@
                     array = Array.from(set);
                 }
                 g.println(resultArea, array);
+            });
+        },
+        g => {
+            const resultArea = g.select(".map-container .result-area");
+            const baseClass = ".control-map ";
+            const typeEles = g.selectAll(baseClass + ".map-key-conatner input");
+            
+            const map = new Map();
+            
+            const types = g.values(typeEles);
+            initMapActions(types);
+            
+            g.log(mapActions);
+            
+            const getType = () => {
+                const typeEle = g.findFirst(typeEles, t => t.checked);
+                const type = typeEle.value;
+                return type;
+            };
+            
+            const getMapAction = () => {
+                const action = mapActions[getType()];
+                return action;
+            };
+            
+            g.clickListener(g.select(baseClass + ".set-map-value"), e => {
+                const action = getMapAction();
+                action.setValue(map, SAMPLE_MAP_VALUE);
+                g.println(resultArea, action);
+            });
+            
+            g.clickListener(g.select(baseClass + ".get-map-value"), e => {
+                const action = getMapAction();
+                const mapKey = createMapKey[getType()]();
+                const value = action.getValue(map, mapKey);
+                g.println(resultArea, value);
+            });
+            
+            g.clickListener(g.select(".map-container .clear-result-area"), e => {
+                g.clear(resultArea);
             });
         }
     ];
