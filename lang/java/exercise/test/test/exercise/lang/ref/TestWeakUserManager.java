612362bd2f5a7b5aeb3544181f580a8e7d621531
KouichiYamada <k-yamada@tg-inet.co.jp>
2016-04-13 21:51:12 +0900

    Added WeakHashMap sample template

---
+++ lang/java/exercise/test/test/exercise/lang/ref/TestWeakUserManager.java
@@ -0,0 +1,27 @@
+package test.exercise.lang.ref;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import static org.junit.Assert.*;
+
+public class TestWeakUserManager {
+    
+	@BeforeClass
+	public static void setUpClass() {
+	}
+	
+	@AfterClass
+	public static void tearDownClass() {
+	}
+	
+	@Before
+	public void setUp() {
+	}
+	
+	@After
+	public void tearDown() {
+	}
+    
+}
