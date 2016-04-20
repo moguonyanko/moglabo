cf0005fd3dbb9657ee87e7ca573e866846f89620
k-yamada <k-yamada@tg-inet.co.jp>
2016-04-20 16:19:19 +0900

    Added simple unit test

--- lang/swift/twist/Sources/main.swift
+++ lang/swift/twist/Sources/main.swift
@@ -1,25 +1,52 @@
-func testGreet(){
+/**
+ * Hello, Swift world!
+ */
+func helloSwift(){
     if Process.arguments.count < 2 {
         print("Require argument as name")
     } else {
         let name = Process.arguments[1]
-        greet(name: name, count: 10)
+        greet(name: name, count: 3)
     }
 }
 
-func testFractorial(){
-    if Process.arguments.count < 2 {
-        print("Require fractorial argument")
-    } else {
-        let arg = Process.arguments[1]
-        let n: Int! = Int(arg)
-        if n != nil {
-            let result = fractorial(n: n)
-            print("Fractorial(\(n)) = \(result)")
-        } else {
-            print("Illegal argument \"\(arg)\" as Int")
+//Common test functions and errors
+ 
+enum AssertError: ErrorProtocol {
+    case Fail
+}
+
+func assertEquals<T: Equatable>(expected: T, actual: T) throws {
+    guard expected == actual else {
+        throw AssertError.Fail
+    } 
+}
+
+//Testcase definition
+
+func testFractorial() throws {
+    let expected = 3628800
+    let actual = fractorial(n: 10)
+    
+    try assertEquals(expected: expected, actual: actual)
+}
+
+let allTests = [
+    "Math": [testFractorial]
+]
+
+/**
+ * All tests runner
+ */
+func runAllTests() throws {
+    for (moduleName, testCases) in allTests {
+        for testCase in testCases {
+            try testCase()
         }
+        print("\(moduleName) test is OK")
     }
 }
 
-testFractorial()
+//Test entry point
+try runAllTests()
+print("\(allTests.count) tests finished")
