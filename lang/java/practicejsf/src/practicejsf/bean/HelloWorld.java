c4417371b68713d6bf3aa26fe6297206a9b52df6
KouichiYamada <k-yamada@tg-inet.co.jp>
2016-05-26 17:37:49 +0900

    Added message bean

--- lang/java/practicejsf/src/practicejsf/bean/HelloWorld.java
+++ lang/java/practicejsf/src/practicejsf/bean/HelloWorld.java
@@ -1,18 +1,32 @@
 package practicejsf.bean;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 
 @ManagedBean(name = "helloWorld", eager = true)
 @RequestScoped
 public class HelloWorld {
+	
+	@ManagedProperty(value = "#{message}")
+	private Message messageBean;
+	
+	private String message;
 
 	public HelloWorld() {
 		System.out.println("HelloWorld はじまります！");
 	}
 	
 	public String getMessage(){
-		return "Hello World!";
+		if(messageBean != null){
+			message = messageBean.getMessage();
+		}
+		
+		return message;
+	}
+
+	public void setMessageBean(Message messageBean) {
+		this.messageBean = messageBean;
 	}
 	
 }
---
+++ lang/java/practicejsf/src/practicejsf/bean/Message.java
@@ -0,0 +1,20 @@
+package practicejsf.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = "message", eager = true)
+@RequestScoped
+public class Message {
+	
+	private String message = "Hello World!";
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
