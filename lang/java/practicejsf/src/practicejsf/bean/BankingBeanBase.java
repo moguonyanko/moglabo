354b1811be98813406704435ea29cd74734b5f50
KouichiYamada <k-yamada@tg-inet.co.jp>
2016-06-22 17:21:56 +0900

    Added Ajax sample

---
+++ lang/java/practicejsf/src/practicejsf/bean/BankingBeanBase.java
@@ -0,0 +1,33 @@
+package practicejsf.bean;
+
+public abstract class BankingBeanBase {
+	
+	private String customerId;
+	private String password;
+	private Customer customer;
+
+	public String getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(String customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	
+}
