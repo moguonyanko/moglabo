491e0ea2410d9758a7b25330206e3d012b4b6ac1
KouichiYamada <k-yamada@tg-inet.co.jp>
2016-07-29 16:45:45 +0900

    Added combined image canvas class

--- lang/javascript/logbook/public_html/contents/imagecombiner/imagecombiner.js
+++ lang/javascript/logbook/public_html/contents/imagecombiner/imagecombiner.js
@@ -11,22 +11,52 @@
 		return parseInt(rowEle.value);
 	};
 	
-	const createCanvas = (baseW = 0, baseH = 0) => {
-		const w = baseW * getColumnSize();
-		const h = baseH * getRowSize();
+	class CombiledImageCanvas {
+		constructor (baseWidth = 0, baseHeight = 0, smooth = true) {
+			this.baseWidth = baseWidth;
+			this.baseHeight = baseHeight;
+			
+			const canvas = doc.createElement("canvas");
+			canvas.setAttribute("width", baseWidth * getColumnSize());
+			canvas.setAttribute("height", baseHeight * getRowSize());
+			this.canvas = canvas;
+			this.context = canvas.getContext("2d");
+			this.setImageSmoothingEnable(smooth);
+		}
 		
-		const canvas = doc.createElement("canvas");
-		canvas.setAttribute("width", w);
-		canvas.setAttribute("height", h);
+		get width () {
+			return this.canvas.width;
+		}
 		
-		canvas.baseWidth = baseW;
-		canvas.baseHeight = baseH;
+		get height () {
+			return this.canvas.height;
+		}
 		
-		return canvas;
-	};
+		get element () {
+			return this.canvas;
+		}
+		
+		setImageSmoothingEnable (enable) {
+			this.context.imageSmoothingEnabled = enable;
+			this.context.mozImageSmoothingEnabled = enable;
+			this.context.webkitImageSmoothingEnabled = enable;
+			this.context.msImageSmoothingEnabled = enable;
+		}
+		
+		/**
+		 * 全ての画像はcanvas生成時の基準となった画像のサイズに合わせるように
+		 * 拡大・縮小されて描画される。
+		 */
+		drawImage (img, xIndex, yIndex) {
+			const dx = xIndex * this.baseWidth,
+				dy = yIndex * this.baseHeight;
+			this.context.drawImage(img, dx, dy, this.baseWidth, this.baseHeight);
+		}
+	}
 	
 	/**
 	 * 画像を一度全て読み込みcanvasのサイズを決定してcanvas要素を生成する。
+	 * canvasのサイズは最も大きい画像のサイズを基準にして決定する。
 	 */
 	const withCanvas = (imgFile, index, imgFiles, drawer) => {
 		const img = doc.createElement("img");
@@ -42,17 +72,10 @@
 			}
 			
 			if(index >= imgFiles.length - 1){
-				drawer(createCanvas(drawer.maxW, drawer.maxH));
+				drawer(new CombiledImageCanvas(drawer.maxW, drawer.maxH));
 			}
 		}
 		img.src = url;
-	};
-	
-	const setImageSmoothingEnable = (ctx, enable) => {
-		ctx.imageSmoothingEnabled = true;
-		ctx.mozImageSmoothingEnabled = true;
-		ctx.webkitImageSmoothingEnabled = true;
-		ctx.msImageSmoothingEnabled = true;
 	};
 	
 	/**
@@ -60,19 +83,14 @@
 	 * canvasの描画結果は画像として右クリックから保存することができる。
 	 * プログラム内でcanvasの描画結果をBlobに変換して保存する必要は無い。
 	 */
-	const draw = (canvas, imgFiles, onDrawn, 
-		imgFile = imgFiles[0], ctx = canvas.getContext("2d"), col = 0, row = 0) => {
+	const draw = (combinedImageCanvas, imgFiles, onDrawn, 
+		imgFile = imgFiles[0], col = 0, row = 0) => {
 		const img = doc.createElement("img");
 		const url = lB.createBlobURL(imgFile);
 		img.onload = () => {
 			lB.revokeBlobURL(url);
 			
-			setImageSmoothingEnable(ctx, true);
-			const dWidth = canvas.baseWidth,
-				dHeight = canvas.baseHeight,
-				dx = col * dWidth,
-				dy = row * dHeight;
-			ctx.drawImage(img, dx, dy, dWidth, dHeight);
+			combinedImageCanvas.drawImage(img, col, row);
 			
 			const colSize = getColumnSize();
 			const nextImgFile = imgFiles[(col + 1) + colSize * row];
@@ -84,9 +102,9 @@
 				} else {
 					col += 1;
 				}
-				draw(canvas, imgFiles, onDrawn, nextImgFile, ctx, col, row);
+				draw(combinedImageCanvas, imgFiles, onDrawn, nextImgFile, col, row);
 			} else {
-				onDrawn(canvas);
+				onDrawn(combinedImageCanvas);
 			}
 		};
 		img.src = url;
@@ -102,13 +120,16 @@
 		 */
 		const clonedImages = [...targetImages];
 		
+		const imageContainerClassName = ".image-container";
+		
 		/**
 		 * 以下のアロー関数を変数等に保存せずwithCanvasの引数として直接記述すると
 		 * withCanvasの呼び出しのたびにアロー関数が生成される。
 		 */
-		const drawer = canvas => draw(canvas, clonedImages, 
-				canvas => lB.select(".image-container").replaceChild(canvas, 
-				lB.select(".image-container canvas")));
+		const drawer = combinedImageCanvas => 
+			draw(combinedImageCanvas, clonedImages, combinedImageCanvas => 
+				lB.select(imageContainerClassName).replaceChild(combinedImageCanvas.element, 
+				lB.select(imageContainerClassName + " canvas")));
 				
 		lB.forEach(clonedImages, (imgFile, index, imgFiles) => 
 			withCanvas(imgFile, index, imgFiles, drawer));
