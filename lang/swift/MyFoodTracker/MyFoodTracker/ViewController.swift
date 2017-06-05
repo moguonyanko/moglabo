44147071be337736e44e99b0b37bb9ec733b7afc
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2017-06-05 16:47:45 +0900

    Added iOS sample application

---
+++ lang/swift/MyFoodTracker/MyFoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFoodTracker
+//
+//  Reference:
+//  Start Developing iOS Apps(Swift) by Apple
+//  https://developer.apple.com/library/content/referencelibrary/GettingStarted/DevelopiOSAppsSwift/index.html
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
