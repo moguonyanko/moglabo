b843195ccea726ffd77dc618e3977ce5b3af15b8
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2017-06-06 17:17:27 +0900

    Added outlets

--- lang/swift/MyFoodTracker/MyFoodTracker/ViewController.swift
+++ lang/swift/MyFoodTracker/MyFoodTracker/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
