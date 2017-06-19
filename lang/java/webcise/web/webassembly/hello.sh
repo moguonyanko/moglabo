8328454b1d30303905775deff45bd387a4eefa96
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2017-06-20 03:50:39 +0900

    Added WebAssembly initial sample

---
+++ lang/java/webcise/web/webassembly/hello.sh
@@ -0,0 +1,7 @@
+mkdir hello
+cd hello
+echo '#include <stdio.h>' > hello.c
+echo 'int main(int argc, char ** argv) {' >> hello.c
+echo 'printf("Hello, world!\n");' >> hello.c
+echo '}' >> hello.c
+emcc hello.c -s WASM=1 -o hello.html
