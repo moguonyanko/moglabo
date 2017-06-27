b423f28d8d744116457f441a01bcf502fbcf59fa
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2017-06-28 02:02:27 +0900

    Modified Server Sent Events sample

--- lang/java/webcise/src/webcise/ServerSender.java
+++ lang/java/webcise/src/webcise/ServerSender.java
@@ -7,6 +7,9 @@ import javax.json.Json;
 import javax.json.JsonArrayBuilder;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
+import javax.servlet.AsyncContext;
+import javax.servlet.AsyncEvent;
+import javax.servlet.AsyncListener;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,8 +19,20 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * サーブレット呼び出しではServerSentEventsのopenイベントしか発生させることができない。
  */
-@WebServlet(name = "ServerSender", urlPatterns = {"/ServerSender"})
+@WebServlet(name = "ServerSender", urlPatterns = {"/ServerSender"}, asyncSupported = true)
 public class ServerSender extends HttpServlet {
+
+    private void writeJSON(PrintWriter out) {
+        JsonObjectBuilder builder = Json.createObjectBuilder();
+        JsonObjectBuilder jb = Json.createObjectBuilder();
+        JsonArrayBuilder ab = Json.createArrayBuilder();
+        ab.add(200);
+        jb.add("code", ab);
+        builder.add("status", jb);
+        JsonObject json = builder.build();
+        out.write(json.toString());
+        out.flush();
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -26,15 +41,37 @@ public class ServerSender extends HttpServlet {
          * text/event-stream以外のContent-Typeを返すとクライアント側でエラーになる。
          */
         response.setContentType("text/event-stream");
+        response.setHeader("Cache-Control", "no-cache");
+        response.setHeader("Connection", "keep-alive");
+        response.setCharacterEncoding("UTF-8");
+
+        final AsyncContext asyncContext = request.startAsync();
+
         try (PrintWriter out = response.getWriter()) {
-            JsonObjectBuilder builder = Json.createObjectBuilder();
-            JsonObjectBuilder jb = Json.createObjectBuilder();
-            JsonArrayBuilder ab = Json.createArrayBuilder();
-            ab.add(200);
-            jb.add("code", ab);
-            builder.add("status", jb);
-            JsonObject json = builder.build();
-            out.print(json.toString());
+            asyncContext.addListener(new AsyncListener() {
+                @Override
+                public void onComplete(AsyncEvent ae) throws IOException {
+                    writeJSON(out);
+                }
+
+                @Override
+                public void onTimeout(AsyncEvent ae) throws IOException {
+                    out.write("timeout!");
+                    out.flush();
+                }
+
+                @Override
+                public void onError(AsyncEvent ae) throws IOException {
+                    out.write("error!");
+                    out.flush();
+                }
+
+                @Override
+                public void onStartAsync(AsyncEvent ae) throws IOException {
+                    out.write("start sync!");
+                    out.flush();
+                }
+            });
         }
     }
 
