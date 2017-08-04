88e425ee56aad1297b4b673fed946d84cf534290
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2017-08-04 15:30:09 +0900

    Modified random number generator function

--- lang/swift/DesignPattern/DesignPattern/iterator.swift
+++ lang/swift/DesignPattern/DesignPattern/iterator.swift
@@ -50,6 +50,9 @@ private class MyCollection<E>: ICollection {
         //typealias Element = E?
         // エンクロージングクラスの型パラメータはネストクラス内部からでも参照可能。
         // propertyは例外をスローできない。
+        // genericsのoptional typeを返してしまうと呼び出した側でunwrapできない。
+        // optional typeを返せないのでイテレーション終了時にnilを返すことができない。
+        // そこで終了時は例外をスローしている。
         func next() throws -> E {
             guard self.hasNext() else {
                 throw IterationError.stopIteration
@@ -62,14 +65,14 @@ private class MyCollection<E>: ICollection {
 }
 
 private func dumpIteratorElements<T: Iteratable>(_ iterator: T) {
-    repeat {
+    while iterator.hasNext() {
         let element = try? iterator.next()
         print("\(element!)")
-    } while iterator.hasNext()
+    }
 }
 
-// TODO: 負の数を含む乱数を生成できない。
-private func makeRandomNumbers(max: UInt32, count: Int) -> [Int] {
+// この関数では負の数を含む乱数を生成できない。
+private func makeRandomUInts(max: UInt32, count: Int) -> [Int] {
     let mapper = {(element: UInt32) -> Int in
         let n = arc4random_uniform(element)
         return Int(n)
@@ -79,9 +82,23 @@ private func makeRandomNumbers(max: UInt32, count: Int) -> [Int] {
     return Array(repeating: max, count: count).map(mapper)
 }
 
+// 負の数を含む乱数を生成できるバージョン
+// TODO: 乱数の偏りが激しいように思える。
+private func makeRandomInts(range: CountableClosedRange<Int>, count: Int) -> [Int] {
+    let mapper = {(element: (absMinimum: Int, maximum: Int)) -> Int in
+        let upperBound = UInt32(element.maximum + element.absMinimum)
+        return Int(arc4random_uniform(upperBound)) - element.absMinimum
+    }
+    let absMin = abs(range.lowerBound)
+    // maxの値も生成されるようにするため1を加えている。
+    return Array(repeating: (absMin, range.upperBound + 1), count: count).map(mapper)
+}
+
 struct Iterator {
     static func main() {
-        let numbers = makeRandomNumbers(max: 100, count: 5)
+        // スペースを除いて(-10...-1)とするとコンパイルエラーになる。
+        // minをmaxより大きくしてもコンパイルエラーにはならず実行時エラーになる。
+        let numbers = makeRandomInts(range: (-20 ... 0), count: 5)
         let numberCollection = MyCollection(numbers)
         let iterator = numberCollection.makeIterator()
         dumpIteratorElements(iterator)
@@ -89,5 +106,9 @@ struct Iterator {
         let collection2 = MyCollection(["H", "e", "l", "l", "o"])
         let iterator2 = collection2.makeIterator()
         dumpIteratorElements(iterator2)
+        
+        let collection3 = MyCollection([Double]())
+        let iterator3 = collection3.makeIterator()
+        dumpIteratorElements(iterator3)
     }
 }
