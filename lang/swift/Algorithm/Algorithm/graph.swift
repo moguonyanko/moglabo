fec8439035f22f4a8512e8826c47a6d96546439a
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2017-08-24 17:27:56 +0900

    Added route search sample

--- lang/swift/Algorithm/Algorithm/graph.swift
+++ lang/swift/Algorithm/Algorithm/graph.swift
@@ -124,11 +124,93 @@ private func printWeightStationMatrix() {
     print(wsm.descriotion)
 }
 
+// ダイクストラ法による最短経路探索
+private let stationMatrix = [
+    (stationName: "横浜", linkedStations: [0, 12, 28, 0, 0, 0]),
+    (stationName: "武蔵小杉", linkedStations: [12, 0, 10, 13, 0, 0]),
+    (stationName: "品川", linkedStations: [28, 10, 0, 11, 7, 0]),
+    (stationName: "渋谷", linkedStations: [0, 13, 11, 0, 0, 9]),
+    (stationName: "新橋", linkedStations: [0, 0, 7, 0, 0, 4]),
+    (stationName: "溜池山王", linkedStations: [0, 0, 0, 9, 4, 0])
+]
+
+private struct ShortestRoute {
+    private var currentCost: [Int]
+    private var fix: [Bool]
+    private var viaStations: [[String]]
+    init() {
+        currentCost = Array(repeating: -1, count: stationMatrix.count)
+        fix = Array(repeating: false, count: stationMatrix.count)
+        viaStations = Array(repeating: [String](), count: stationMatrix.count)
+    }
+    mutating func search(startIndex: Int) -> [(stationName: String, cost: Int,
+        viaStations: [String])] {
+        // 出発地点のコストはゼロ
+        currentCost[startIndex] = 0
+        var results = [(stationName: String, cost: Int, viaStations: [String])]()
+        while true {
+            var minStation = -1
+            var minTime = -1
+            for i in 0..<stationMatrix.count {
+                // 未確定かつ到達不可能でない
+                if !fix[i] && currentCost[i] != -1 {
+                    // 最短時間が未登録またはより所要時間が短い駅が見つかった
+                    if minTime == -1 || currentCost[i] < minTime {
+                        minTime = currentCost[i]
+                        minStation = i
+                    }
+                }
+            }
+            // 到達可能な駅が無かった，あるいは全ての駅を探索した
+            if minTime == -1 {
+                break
+            }
+            // 探索中の駅から繋がっている駅を走査して最も短い所要時間を保存する。
+            for j in 0..<stationMatrix.count {
+                if !fix[j] && stationMatrix[minStation].linkedStations[j] > 0 {
+                    let newTime = minTime + stationMatrix[minStation].linkedStations[j]
+                    // コスト未登録またはより短時間で到達可能
+                    if currentCost[j] == -1 || newTime < currentCost[j] {
+                        currentCost[j] = newTime
+                        // 出発地点と最短駅地点が同じ場合は経由無しと見なして駅名を保存しない。
+                        if startIndex != minStation {
+                            viaStations[j].append(stationMatrix[minStation].stationName)
+                        }
+                    }
+                }
+            }
+            fix[minStation] = true
+        }
+        for k in 0..<stationMatrix.count {
+            results.append((stationName: stationMatrix[k].stationName,
+                            cost: currentCost[k],
+                            viaStations: viaStations[k]))
+        }
+        return results
+    }
+}
+
+private func searchShortestRoute() {
+    // mutatingなメソッドを利用するのでvarで宣言しなければならない。
+    var sh = ShortestRoute()
+    let startStationIndex = 0
+    let results = sh.search(startIndex: startStationIndex)
+    for i in 0..<results.count {
+        let msg = [
+            "\(stationMatrix[startStationIndex].stationName)->\(results[i].stationName)",
+            "必要時間:\(results[i].cost)分",
+            "経由駅:\(results[i].viaStations.description)"
+        ]
+        print(msg.joined(separator: " "))
+    }
+}
+
 struct Graph {
     static func main() {
         //printSampleStations()
         //printStationMatrix()
-        printWeightStationMatrix()
+        //printWeightStationMatrix()
+        searchShortestRoute()
     }
 }
 
