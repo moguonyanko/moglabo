ec49453f16858870ea67d242a239d2d7ed623af9
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2017-08-24 11:52:58 +0900

    Added graph matrix

--- lang/swift/Algorithm/Algorithm/graph.swift
+++ lang/swift/Algorithm/Algorithm/graph.swift
@@ -65,9 +65,70 @@ private func printSampleStations() {
     stations.forEach { print($0.getStationInfo()) }
 }
 
+private struct StationMatrix {
+    private let matrix = [
+        (stationName: "鎌倉", linkedStations: [0, 1, 1, 1, 0, 0]),
+        (stationName: "藤沢", linkedStations: [1, 0, 1, 0, 1, 0]),
+        (stationName: "横浜", linkedStations: [1, 1, 0, 0, 0, 1]),
+        (stationName: "横須賀", linkedStations: [1, 0, 0, 0, 0, 0]),
+        (stationName: "茅ヶ崎", linkedStations: [0, 1, 0, 0, 0, 0]),
+        (stationName: "東京", linkedStations: [0, 0, 1, 0, 0, 0])
+    ]
+    var descriotion: String {
+        var msg = [String]()
+        for i in 0..<matrix.count {
+            msg.append("\(matrix[i].stationName):")
+            for j in 0..<matrix[i].linkedStations.count {
+                if matrix[i].linkedStations[j] == 1 {
+                    msg.append("->\(matrix[j].stationName) ")
+                }
+            }
+            msg.append("\n")
+        }
+        return msg.joined()
+    }
+}
+
+private func printStationMatrix() {
+    let sm = StationMatrix()
+    print(sm.descriotion)
+}
+
+private struct WeightStationMatrix {
+    private let matrix = [
+        (stationName: "横浜", linkedStations: [0, 12, 28, 0, 0, 0]),
+        (stationName: "武蔵小杉", linkedStations: [12, 0, 10, 13, 0, 0]),
+        (stationName: "品川", linkedStations: [28, 10, 0, 11, 7, 0]),
+        (stationName: "渋谷", linkedStations: [0, 13, 11, 0, 0, 9]),
+        (stationName: "新橋", linkedStations: [0, 0, 7, 0, 0, 4]),
+        (stationName: "溜池山王", linkedStations: [0, 0, 0, 9, 4, 0])
+    ]
+    var descriotion: String {
+        var msg = [String]()
+        for i in 0..<matrix.count {
+            msg.append("\(matrix[i].stationName):")
+            for j in 0..<matrix[i].linkedStations.count {
+                if matrix[i].linkedStations[j] > 0 {
+                    let info = "->\(matrix[j].stationName)(\(matrix[i].linkedStations[j])分) "
+                    msg.append(info)
+                }
+            }
+            msg.append("\n")
+        }
+        return msg.joined()
+    }
+}
+
+private func printWeightStationMatrix() {
+    let wsm = WeightStationMatrix()
+    print(wsm.descriotion)
+}
+
 struct Graph {
     static func main() {
-        printSampleStations()
+        //printSampleStations()
+        //printStationMatrix()
+        printWeightStationMatrix()
     }
 }
 
