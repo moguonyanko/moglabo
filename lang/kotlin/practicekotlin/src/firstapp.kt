126fad3b8d08c0ba3df53580fa3bd07c98249fbd
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2017-09-13 15:54:29 +0900

    Added first kotlin application

---
+++ lang/kotlin/practicekotlin/src/firstapp.kt
@@ -0,0 +1,11 @@
+/**
+ * My first kotlin application
+ *
+ * Reference:
+ * https://kotlinlang.org/docs/tutorials/getting-started.html
+ * https://kotlinlang.org/docs/tutorials/koans.html
+ */
+
+fun main(args: Array<String>) {
+    println("Hello Kotlin world!")
+}
