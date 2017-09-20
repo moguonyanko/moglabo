3283d062b9ae54a6dfd936225c24e8c608e4cfa3
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2017-09-20 17:45:40 +0900

    Added aircraft fire config

---
+++ lang/javascript/logbook/public_html/scripts/antiaircraftfire/main.js
@@ -0,0 +1,17 @@
+((window, document) => {
+    "use strict";
+    
+    class Downing {
+        // TODO: implement
+    }
+    
+    const runTest = () => {
+        
+    };
+    
+    const init = () => {
+        runTest();
+    };
+    
+    window.addEventListener("DOMContentLoaded", init);
+})(window, document);
