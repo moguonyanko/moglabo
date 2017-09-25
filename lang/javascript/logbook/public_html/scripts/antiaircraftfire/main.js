036a7b83b61db24de5c49bd82f99c62f297a9d77
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2017-09-25 18:52:47 +0900

    Implemented antiaircraft fire calculation system(alpha)

--- lang/javascript/logbook/public_html/scripts/antiaircraftfire/main.js
+++ lang/javascript/logbook/public_html/scripts/antiaircraftfire/main.js
@@ -160,6 +160,13 @@
         }
     }
     
+    class AntiaircraftCutin2 {
+        constructor({fixedBonus, cutinTypeName} = {}) {
+            this.fixedBonus = fixedBonus;
+            this.floatingBonus = floatingBonus;
+        }
+    }
+    
     class MisfireAntiaircraftCutin extends AntiaircraftCutin {
         constructor() {
             // superのコンストラクタ呼び出しが無いと実行時エラーとなる。
@@ -190,7 +197,7 @@
             const downingSize = this.downings.map(downing => downing.execute({
                 fleet, enemyCarrySize
             })).reduce((a, b) => a + b, 0);
-            return downingSize;              
+            return downingSize < enemyCarrySize ? downingSize : enemyCarrySize;              
         }
         get interceptShip() {
             const intercepter = this.ships.filter(ship => ship.intercept)[0];
@@ -243,21 +250,52 @@
         }
     }
     
+    const makeFleet = ({
+        downings = [], 
+        ships = [], 
+        antiaircraftCutin,
+        fleetTypeName,
+        formationName
+    }) => {
+        if (!fleetTypeName) {
+            throw new Error(`Fleet type is falsy: ${fleetTypeName}`);
+        }
+        
+        const type = fleetTypeName.toLowerCase();
+        if (type === "normal") {
+            return new NormalFleet({
+                downings, 
+                ships, 
+                antiaircraftCutin,
+                formationName
+            });
+        } else if(type === "union") {
+            return new UnionFleet({
+                downings, 
+                ships, 
+                antiaircraftCutin,
+                formationName
+            });
+        } else {
+            throw new Error(`Fleet type is invalid: ${fleetTypeName}`);
+        }
+    };
+    
     // for Test
     
     const makeSampleShip = (fleetPosition, intercept) => {
         const eq1 = new Equipment({
-            typeName: "高角砲＋高射装置",
+            typeName: "高角砲(高射装置有)",
             antiaircraft: 10,
             improvement: 10
         }),
         eq2 = new Equipment({
-            typeName: "高角砲＋高射装置",
+            typeName: "高角砲(高射装置有)",
             antiaircraft: 10,
             improvement: 10
         }),
         eq3 = new Equipment({
-            typeName: "対空電探",
+            typeName: "小型電探",
             antiaircraft: 4,
             improvement: 10
         });    
@@ -326,8 +364,241 @@
         console.log(`${fleet.name}は敵艦載機${enemy.targetSlotCarrySize}機のうち${downingSize}機撃墜しました。`);
     };
     
+    // DOM
+    
+    const el = (id, doc) => {
+        return (doc || document).getElementById(id);
+    };
+    
+    const makeAntiaircraftElement = () => {
+        const antiAir = document.createElement("label");
+        antiAir.appendChild(document.createTextNode("対空値"));
+        const antiAirInput = document.createElement("input");
+        antiAirInput.setAttribute("class", "antiaircraft-value");
+        antiAirInput.setAttribute("type", "number");
+        antiAirInput.setAttribute("min", "0");
+        antiAirInput.setAttribute("max", "100");
+        antiAirInput.setAttribute("value", "0");
+        antiAir.appendChild(antiAirInput);
+        
+        return antiAir;
+    };
+    
+    const makeEquipmentElement = () => {
+        const container = document.createElement("div");
+        container.setAttribute("class", "equipment");
+        
+        const wKeys = Object.keys(weightedMagnification),
+            fKeys = Object.keys(fleetMagnification);
+        const eqTypeNames = new Set(wKeys.concat(fKeys));
+        const makeSel = () =>{
+            const sel = document.createElement("select");
+            sel.setAttribute("class", "equipment-type");
+            eqTypeNames.forEach(name => {
+                const op = document.createElement("option");
+                op.setAttribute("value", name);
+                op.appendChild(document.createTextNode(name));
+                sel.appendChild(op);
+            });
+            return sel;
+        };
+        
+        const makeImp = () => {
+            const imp = document.createElement("label"); 
+            imp.appendChild(document.createTextNode("改修度"));
+            const impInput = document.createElement("input");
+            impInput.setAttribute("class", "equipment-improvement");
+            impInput.setAttribute("type", "number");
+            impInput.setAttribute("min", "0");
+            impInput.setAttribute("max", "10");
+            impInput.setAttribute("value", "0");
+            imp.appendChild(impInput);
+            return imp;
+        };
+        
+        const maxSlotSize = 5;
+        for (let i = 0; i < maxSlotSize; i++) {
+            const eqEle = document.createElement("div");
+            eqEle.setAttribute("class", "equipment-info-container");
+            eqEle.appendChild(makeSel());
+            eqEle.appendChild(makeAntiaircraftElement());
+            eqEle.appendChild(makeImp());
+            container.appendChild(eqEle);
+        }
+        
+        return container;
+    };
+    
+    const makeEquipments = shipEle => {
+        const eqs = shipEle.querySelectorAll(".equipment-info-container");
+        const equipments = Array.from(eqs).map(eqEle => {
+            const antiaircraft = parseInt(eqEle.querySelector(".antiaircraft-value").value);
+            const typeName = eqEle.querySelector(".equipment-type").value;
+            const improvement = parseInt(eqEle.querySelector(".equipment-improvement").value);
+            
+            const equipment = new Equipment({
+                typeName, antiaircraft, improvement
+            });            
+            
+            return equipment;
+        });
+        
+        return equipments;
+    };
+    
+    const makeShipElement = fleetPosition => {
+        const container = document.createElement("div");
+        container.setAttribute("class", "ship");
+        
+        const antiAir = makeAntiaircraftElement();
+        const eqEle = makeEquipmentElement();
+        
+        const intercept = document.createElement("label");
+        const interceptInput = document.createElement("input");
+        interceptInput.setAttribute("class", "check-intercept");
+        interceptInput.setAttribute("type", "checkbox");
+        interceptInput.addEventListener("click", evt => {
+            if (evt.target.checked) {
+                interceptInput.setAttribute("checked", "checked");
+            } else {
+                interceptInput.removeAttribute("checked");
+            }
+        });
+        intercept.appendChild(interceptInput);
+        intercept.appendChild(document.createTextNode("迎撃艦選出"));
+        
+        const fp = document.createElement("input");
+        fp.setAttribute("class", "fleet-position");
+        fp.setAttribute("type", "hidden");
+        fp.setAttribute("value", fleetPosition);
+        
+        container.appendChild(antiAir);
+        container.appendChild(eqEle);
+        container.appendChild(intercept);
+        container.appendChild(fp);
+        
+        return container;
+    };
+    
+    const makeAntiaircraftCutinElement = () => {
+        const container = document.createElement("div");
+        
+        const sel = document.createElement("select");
+        sel.setAttribute("id", "antiaircraft-cutin-type");
+        Object.keys(antiaircraftBonus).forEach(parentType => {
+            const optionGroup = document.createElement("optgroup");
+            optionGroup.setAttribute("label", parentType);
+            Object.keys(antiaircraftBonus[parentType]).forEach(type => {
+                const option = document.createElement("option");
+                option.appendChild(document.createTextNode(type));
+                option.setAttribute("value", `${parentType},${type}`);
+                optionGroup.appendChild(option);
+            });
+            sel.appendChild(optionGroup);
+        });
+        container.appendChild(sel);
+        
+        return container;
+    };
+    
+    const makeFormationElement = fleetTypeName => {
+        const container = document.createElement("div")
+        const sel = document.createElement("select");
+        sel.setAttribute("id", `${fleetTypeName}-formation-type`);
+        Object.keys(formationRevisionValues[fleetTypeName]).forEach(name => {
+            const option = document.createElement("option");
+            option.appendChild(document.createTextNode(name));
+            option.setAttribute("value", name);
+            sel.appendChild(option);
+        });
+        container.appendChild(sel);
+        
+        return container;
+    };
+    
+    const appendShips = (fleetPosition, className) => {
+        const container1 = document.querySelector(className);
+        const fleetShipSize = 6;
+        for (let i = 0; i < fleetShipSize; i++) {
+            const shipEle = makeShipElement(fleetPosition);
+            container1.appendChild(shipEle);
+        }
+    };
+    
+    const makePage = () => {
+        const normalFc = document.querySelector(".normal-formation-container");
+        normalFc.appendChild(makeFormationElement("normal"));
+        const unionFc = document.querySelector(".union-formation-container");
+        unionFc.appendChild(makeFormationElement("union"));
+        
+        const cutinContainer = document.querySelector(".antiaircutin-container");
+        cutinContainer.appendChild(makeAntiaircraftCutinElement());
+        
+        appendShips(1, ".first-fleet-info-container");
+        appendShips(2, ".second-fleet-info-container");
+    };
+    
+    const makeShips = containerClassName => {
+        const container = document.querySelector(containerClassName);
+        const ships = Array.from(container.querySelectorAll(".ship")).map(shipEle => {
+            const eqEle = shipEle.querySelector(".equipment-type");
+            const ship = new Ship({
+                antiaircraft: parseInt(shipEle.querySelector(".antiaircraft-value").value),
+                equipments: makeEquipments(shipEle),
+                intercept: Boolean(shipEle.querySelector(".check-intercept").checked),
+                fleetPosition: shipEle.querySelector(".fleet-position").value
+            });
+            return ship;
+        });
+        
+        return ships;
+    };
+    
+    const runCalc = () => {
+        let enemyCarrySize = document.querySelector(".enemy-carry-size").value;
+        enemyCarrySize = parseInt(enemyCarrySize);
+        const enemy = new Enemy({carries: [enemyCarrySize], targetSlotNumber: 0});
+        
+        const rateSuccess = document.querySelector(".success-intercept.rate").checked;
+        const fixedSuccess = document.querySelector(".success-intercept.fixed").checked;
+        
+        const rd = new RateDowning({success: rateSuccess}),
+            fd = new FixedDowning({success: fixedSuccess}),
+            mg = new MinimumGuarantee();
+        const downings = [rd, fd, mg];
+
+        const fleetTypeName = Array.from(document.querySelectorAll(".fleet-type"))
+                .filter(ele => ele.checked)[0].value;
+        
+        const selectedCutinValue = el("antiaircraft-cutin-type").value;
+        const cutinNames = selectedCutinValue.split(",");
+        const antiaircraftCutin = new AntiaircraftCutin({
+            shipTypeName: cutinNames[0],
+            cutinTypeName: cutinNames[1]
+        });
+        
+        const formationName = el(`${fleetTypeName}-formation-type`).value;
+        
+        let ships = makeShips(".first-fleet-info-container");
+        if (fleetTypeName === "union") {
+            ships = ships.concat(makeShips(".second-fleet-info-container"));
+        }
+
+        const fleet = makeFleet({
+            downings, fleetTypeName, formationName, antiaircraftCutin, ships
+        });
+        
+        const downingSize = fleet.antiaircraftFire(enemy);
+        
+        const result = `${fleet.name}は敵艦載機${enemy.targetSlotCarrySize}機のうち${downingSize}機撃墜しました。`;
+        const resultArea = document.querySelector(".result");
+        resultArea.innerHTML = result;
+    };
+    
     window.addEventListener("DOMContentLoaded", async () => {
         await loadConfig();
-        runTest();
+        //runTest();
+        makePage();
+        document.querySelector(".runner").addEventListener("click", runCalc);
     });
 })(window, document);
