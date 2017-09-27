602770b2c1f21551df71e8e86ea753c9d474e148
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2017-09-27 17:53:49 +0900

    Modified Service Workers sample

--- lang/java/webcise/web/esproposal/serviceworker/main.js
+++ lang/java/webcise/web/esproposal/serviceworker/main.js
@@ -1,5 +1,7 @@
 ((win, doc, nav, g) => {
 	"use strict";
+    
+    const sw = nav.serviceWorker;
 	
 	const targets = {
 		registerSample(g) {
@@ -9,25 +11,34 @@
 				clearEle = doc.querySelector(base + ".clear-result"),
 				output = doc.querySelector(base + ".result-area");
 			
+            const checkReady = () => {
+                sw.ready.then(registration => g.println(output, "Ready!"))
+                        .catch(err => g.println(output, `Ready failed: ${err.message}`));
+                        // Promise.prototype.finallyは未対応ブラウザが多い。
+                        //.finally(() => g.println(output, "Ready!"));
+            };
+            
 			const regFunc = async () => {
-				if ("serviceWorker" in nav) {
-					const options = {
-						scope: "./"
-					};
-					/**
-					 * registerメソッドに渡すスクリプトのURLはHTTPS等セキュアな
-					 * スキームを使用したオリジンで構成されていなければChromeでは
-					 * エラーになる。Firefoxでは何も起きない。
-					 * Firefoxはページを読み込んだ際にregisterの引数のスクリプトを
-					 * 読み込んでしまう。
-					 */
-					const registration = await nav.serviceWorker.register("worker.js", options);
-					console.log(registration);
-					const sw = registration.active;
-					g.println(output, `${sw.scriptURL} : ${sw.state}`);
-				} else {
+				if (!("serviceWorker" in nav)) {
 					g.println(output, "Cannot use Service Worker");
-				}
+                    return;
+                }
+                
+                checkReady();
+                
+                try {
+                    // このディレクトリのみをスコープとする。
+                    const scope = "./";
+                    // 自己証明書を用いたHTTPSで通信している場合，
+                    // Chromeではセキュリティエラーになってしまう。
+                    // Chromeの起動オプションを変更することで対応できる。
+                    const registration = await sw.register("worker.js", {scope});
+                    console.log(registration);
+                    const active = registration.active;
+                    g.println(output, `${active.scriptURL} : ${active.state}`);
+                } catch(err) {
+    				g.println(output, err.message);
+                }
 			};
 			
 			regEle.addEventListener("click", regFunc);
