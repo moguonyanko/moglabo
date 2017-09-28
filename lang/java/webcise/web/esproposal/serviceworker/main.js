0f1c53c55d0a775648f270883edc811a4a68dbb3
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2017-09-28 17:21:39 +0900

    Added first Cache API sample

--- lang/java/webcise/web/esproposal/serviceworker/main.js
+++ lang/java/webcise/web/esproposal/serviceworker/main.js
@@ -3,6 +3,25 @@
     
     const sw = nav.serviceWorker;
 	
+    const enableSW = () => {
+        return "serviceWorker" in nav;
+    };
+    
+    /**
+     * デフォルトはこのディレクトリのみをスコープとする。
+     * 自己証明書を用いたHTTPSで通信している場合，Chromeでは
+     * セキュリティエラーになってしまう。
+     * Chromeの起動オプションを変更することで対応できる。
+     */
+    const register = async ({url, scope}) => {
+        const registration = await sw.register(url, {scope});
+        return registration;
+    };
+    
+    const getCacheKeys = async () => {
+        return await caches.keys();
+    };
+    
 	const targets = {
 		registerSample(g) {
 			const base = ".register-sample ";
@@ -12,19 +31,17 @@
 				output = doc.querySelector(base + ".result-area");
 			
             const checkReady = () => {
-                sw.ready.then(registration => g.println(output, "Ready!"))
+                sw.ready.then(registration => g.println(output, `Ready: ${JSON.stringify(registration)}`))
                         .catch(err => g.println(output, `Ready failed: ${err.message}`));
                         // Promise.prototype.finallyは未対応ブラウザが多い。
                         //.finally(() => g.println(output, "Ready!"));
             };
             
 			const regFunc = async () => {
-				if (!("serviceWorker" in nav)) {
+				if (!enableSW()) {
 					g.println(output, "Cannot use Service Worker");
                     return;
                 }
-                
-                checkReady();
                 
                 try {
                     // このディレクトリのみをスコープとする。
@@ -32,23 +49,44 @@
                     // 自己証明書を用いたHTTPSで通信している場合，
                     // Chromeではセキュリティエラーになってしまう。
                     // Chromeの起動オプションを変更することで対応できる。
-                    const registration = await sw.register("worker.js", {scope});
+                    const registration = await sw.register("register-sample.js", {scope});
                     console.log(registration);
                     const active = registration.active;
                     g.println(output, `${active.scriptURL} : ${active.state}`);
                 } catch(err) {
     				g.println(output, err.message);
+                } finally {
+                    checkReady();
                 }
 			};
 			
 			regEle.addEventListener("click", regFunc);
 			clearEle.addEventListener("click", () => g.clear(output));
-		}
+		},
+        cacheSample() {
+            if(!enableSW()){
+                return;
+            }
+            
+            const base = document.querySelector(".cache-sample");
+            const resultArea = base.querySelector(".result-area");
+            
+            base.querySelector(".register").addEventListener("click", async () => {
+                const scope = "/webcise/esproposal/serviceworker/";
+                const reg = await register({
+                    url: "cache-sample.js", scope
+                });
+                resultArea.innerHTML += JSON.stringify(reg) + "<br />";
+                resultArea.innerHTML += (await getCacheKeys()).join(",") + "<br />";
+            });
+            
+            base.querySelector(".clear").addEventListener("click", () => {
+                resultArea.innerHTML = "";
+            });
+        }
 	};
 	
-	const init = () => {
-		Object.values(targets).forEach(t => t(g));
-	};
+	const init = () => Object.values(targets).forEach(t => t(g));
 	
 	win.addEventListener("DOMContentLoaded", init);
 })(window, document, navigator, goma);
