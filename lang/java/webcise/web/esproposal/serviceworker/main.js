9b89955c01a135abfae140a6702bb1d1a995e3d0
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2017-10-11 14:51:13 +0900

    Modified service worker fetch listener

--- lang/java/webcise/web/esproposal/serviceworker/main.js
+++ lang/java/webcise/web/esproposal/serviceworker/main.js
@@ -1,26 +1,26 @@
 ((win, doc, nav, g) => {
-	"use strict";
-    
+    "use strict";
+
     const q = (selector, base) => {
         return (base || doc).querySelector(selector);
     };
-    
+
     const qa = (selector, base) => {
         return (base || doc).querySelectorAll(selector);
     };
-    
+
     const pr = (target, content) => {
         target.innerHTML += content + "<br />";
     };
-    
+
     const cl = target => target.innerHTML = "";
-    
+
     const sw = nav.serviceWorker;
-	
+
     const enableSW = () => {
         return "serviceWorker" in nav;
     };
-    
+
     /**
      * デフォルトはこのディレクトリのみをスコープとする。
      * 自己証明書を用いたHTTPSで通信している場合，Chromeでは
@@ -31,7 +31,7 @@
         const registration = await sw.register(url, {scope});
         return registration;
     };
-    
+
     const getCacheKeys = async pattern => {
         if (pattern) {
             const regex = new RegExp(pattern);
@@ -40,32 +40,32 @@
             return await caches.keys();
         }
     };
-    
+
     const getCacheKeyString = async (pattern, separator = ",") => {
         return (await getCacheKeys(pattern)).join(separator);
     };
-    
-	const targets = {
-		registerSample(g) {
-			const base = ".register-sample ";
-			
-			const regEle = doc.querySelector(base + ".register-service-worker"),
-				clearEle = doc.querySelector(base + ".clear-result"),
-				output = doc.querySelector(base + ".result-area");
-			
+
+    const targets = {
+        registerSample(g) {
+            const base = ".register-sample ";
+
+            const regEle = doc.querySelector(base + ".register-service-worker"),
+                    clearEle = doc.querySelector(base + ".clear-result"),
+                    output = doc.querySelector(base + ".result-area");
+
             const checkReady = () => {
                 sw.ready.then(registration => g.println(output, `Ready: ${JSON.stringify(registration)}`))
                         .catch(err => g.println(output, `Ready failed: ${err.message}`));
-                        // Promise.prototype.finallyは未対応ブラウザが多い。
-                        //.finally(() => g.println(output, "Ready!"));
+                // Promise.prototype.finallyは未対応ブラウザが多い。
+                //.finally(() => g.println(output, "Ready!"));
             };
-            
-			const regFunc = async () => {
-				if (!enableSW()) {
-					g.println(output, "Cannot use Service Worker");
+
+            const regFunc = async () => {
+                if (!enableSW()) {
+                    g.println(output, "Cannot use Service Worker");
                     return;
                 }
-                
+
                 try {
                     // このディレクトリのみをスコープとする。
                     const scope = "./";
@@ -76,24 +76,24 @@
                     console.log(registration);
                     const active = registration.active;
                     g.println(output, `${active.scriptURL} : ${active.state}`);
-                } catch(err) {
-    				g.println(output, err.message);
+                } catch (err) {
+                    g.println(output, err.message);
                 } finally {
                     checkReady();
                 }
-			};
-			
-			regEle.addEventListener("click", regFunc);
-			clearEle.addEventListener("click", () => g.clear(output));
-		},
+            };
+
+            regEle.addEventListener("click", regFunc);
+            clearEle.addEventListener("click", () => g.clear(output));
+        },
         cacheSample() {
-            if(!enableSW()){
+            if (!enableSW()) {
                 return;
             }
-            
+
             const base = document.querySelector(".cache-sample");
             const resultArea = base.querySelector(".result-area");
-            
+
             base.querySelector(".register").addEventListener("click", async () => {
                 const scope = "/webcise/esproposal/serviceworker/";
                 const reg = await register({
@@ -102,35 +102,35 @@
                 resultArea.innerHTML += JSON.stringify(reg) + "<br />";
                 resultArea.innerHTML += (await getCacheKeys()).join(",") + "<br />";
             });
-            
+
             base.querySelector(".clear").addEventListener("click", () => {
                 resultArea.innerHTML = "";
             });
         },
         customResponseSample() {
             const base = q(".custom-response-sample"),
-                resultArea = q(".result-area", base),
-                resultImage = q(".result-image", base),
-                adder = q(".adder", base),
-                getter = q(".getter", base),
-                clearer = q(".clearer", base);
-            
+                    resultArea = q(".result-area", base),
+                    resultImage = q(".result-image", base),
+                    adder = q(".adder", base),
+                    getter = q(".getter", base),
+                    clearer = q(".clearer", base);
+
             getter.addEventListener("click", () => {
                 const url = "./images/blue.png";
                 const img = doc.createElement("img");
                 img.onload = () => resultImage.appendChild(img);
                 img.src = url;
             });
-            
+
             clearer.addEventListener("click", () => {
                 cl(resultArea);
                 cl(resultImage);
             });
-            
-            if(!enableSW()){
+
+            if (!enableSW()) {
                 return;
             }
-            
+
             adder.addEventListener("click", async () => {
                 // ここでコンテキストルートをスコープにするにはServiceWorkerスクリプトを
                 // 移動するかService-Worker-Allowedヘッダーを使用しなければならない。
@@ -148,78 +148,86 @@
             if (!enableSW()) {
                 return;
             }
-            
+
             const base = q(".update-cache-sample"),
-                loader = q(".loader", base),
-                appender = q(".appender", base),
-                clearer = q(".clearer", base),
-                resultArea = q(".result-area", base),
-                resultImage = q(".result-image", base);
-                
+                    loader = q(".loader", base),
+                    appender = q(".appender", base),
+                    clearer = q(".clearer", base),
+                    resultArea = q(".result-area", base),
+                    resultImage = q(".result-image", base);
+
             const p = txt => pr(resultArea, txt);
-            
-            loader.addEventListener("click", async () => {
+
+            const printKeyInfo = async () => {
                 const keyStr = await getCacheKeyString("update-cache-sample");
                 const info = `キー[${keyStr}]のキャッシュが登録済みです。`;
                 p(info);
-                
-                const scope = "/webcise/esproposal/serviceworker/";
+            };
+
+            loader.addEventListener("click", async () => {
                 const url = `update-cache-sample.js`;
                 await sw.register(url);
+                await printKeyInfo();
             });
-            
-            const getRandomVersion = () => {
-                if (Date.now() % 2 === 1) {
-                    return "v1";
-                } else {
-                    return "v2";
-                }
-            };
-            
+
             // ServiceWorkerスクリプトを登録している側のスクリプトはCacheStorageに
             // キャッシュされている可能性があるので，CacheStorageのバージョンを明示的に
             // 参照したりしないようにする。バージョンの齟齬が発生しそうだし二重管理にもなる。
-            
+
             const imageUrls = [
-                "images/red.png", 
-                "images/yellow.png", 
-                "images/green.png", 
+                "images/red.png",
+                "images/yellow.png",
+                "images/green.png",
                 "images/orange.png"
             ];
-            
-            appender.addEventListener("click", () => {
-                const imgBase = doc.createDocumentFragment();
+
+            class ImageError extends Error {
+                constructor( {message, img}) {
+                    super(message);
+                    // superによるコンストラクタ呼び出しが無い状態でmessageプロパティを
+                    // 参照すると，初期化されていないプロパティを参照したとしてエラーになる。
+                    // this.message = message;
+                    this.img = img;
+                }
+            }
+
+            const getImages = () => {
                 const allPromise = Promise.all(imageUrls.map(url => {
                     return new Promise((resolve, reject) => {
                         const img = doc.createElement("img");
                         img.onload = event => {
-                            imgBase.appendChild(img);
-                            resolve(imgBase);
+                            resolve(img);
                         };
                         img.onerror = err => {
-                            reject(new Error(`Failed fetch: ${url}. Reason: ${err.message}`));
+                            const message = `Failed fetch: ${url}. Reason: ${err.message}`;
+                            reject(new ImageError({message, img}));
                         };
                         img.src = url;
                     });
                 }));
-                
-                allPromise.then(documentFragments => {
-                    resultImage.appendChild(imgBase);
+                return allPromise;
+            };
+
+            appender.addEventListener("click", async () => {
+                try {
+                    const imageContainer = doc.createDocumentFragment();
+                    const imgs = await getImages();
+                    imgs.forEach(img => imageContainer.appendChild(img));
+                    resultImage.appendChild(imageContainer);
                     p("All images are appended");
-                }).catch(err => {
-                    resultImage.appendChild(imgBase);
+                } catch (err) {
                     p(`ERROR: ${err.message}`);
-                });
+                }
             });
-            
+
             clearer.addEventListener("click", () => {
                 resultArea.innerHTML = "";
                 resultImage.innerHTML = "";
-            });    
+            });
         }
-	};
-	
-	const init = () => Object.values(targets).forEach(t => t(g));
-	
-	win.addEventListener("DOMContentLoaded", init);
+    };
+
+    const init = () => Object.values(targets).forEach(t => t(g));
+
+    win.addEventListener("DOMContentLoaded", init);
 })(window, document, navigator, goma);
--- lang/java/webcise/web/esproposal/serviceworker/update-cache-sample.js
+++ lang/java/webcise/web/esproposal/serviceworker/update-cache-sample.js
@@ -1,7 +1,6 @@
 /**
  * 参考:
  * https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
- * https://jakearchibald.com/2016/caching-best-practices/
  */
 
 const CACHE_PREFIX = "update-cache-sample-";
@@ -12,7 +11,24 @@ const APP_BASE = `${CONTEXT}esproposal/serviceworker/`;
 
 const CACHE_BASE = `${APP_BASE}images/`;
 
-const VERSION = "v2";
+const VERSION = "v1";
+
+const getErrorPage = url => {
+    const page = `
+<html>
+<head>
+<meta charset="UTF-8" />
+<title>Error Page</title>
+</head>
+<body>
+<h1>Error Page</h1>
+<h2>Error URL</h2>
+<a href="${url}">${url}</a>
+</body>
+</html>
+`;
+    return Promise.resolve(page);
+};
 
 const COMMON_RESOURCES = [
     `${CONTEXT}gomakit.js`,
@@ -23,6 +39,8 @@ const COMMON_RESOURCES = [
     `${APP_BASE}main.js`
 ];
 
+// バージョンごとにキャッシュ対象リソースを変えることで
+// キャッシュされるリソースとされないリソースを混在させ動作確認を行う。
 const cacheTargets = {
     [`${CACHE_PREFIX}v1`]: COMMON_RESOURCES.concat([
         `${CACHE_BASE}green.png`,
@@ -36,12 +54,40 @@ const cacheTargets = {
 
 const getKey = () => `${CACHE_PREFIX}${VERSION}`;
 
+const isCacheTargetResource = url => {
+    return cacheTargets[getKey()].indexOf(url) >= 0;
+};
+
+const checkResponse = async ({request, response}) => {
+    if (response) {
+        console.log(`Fetched (from cache storage): ${request.url}`);
+        return Promise.resolve(response);
+    }
+
+    const promise = fetch(request).then(response => {
+        console.log(`Fetched (from server): ${request.url}`);
+        // 元々キャッシュ対象だったリソースのみFetch時にキャッシュへ追加する。
+        if (isCacheTargetResource(request.url)) {
+            return caches.open(getKey()).then(cache => {
+                console.log(`Recoveroed cache: ${request.url}`);
+                cache.put(request, response.clone());
+                return response;
+            });
+        } else {
+            console.log(`Not add to cache: ${request.url}`);
+            return response;
+        }
+    });
+
+    return promise;
+};
+
 self.addEventListener("activate", event => {
-    console.log("Activated update cache sample");
+    console.log(`Activated: ${getKey()}`);
     event.waitUntil(caches.keys().then(keys => {
         return Promise.all(keys.map(key => {
             if (key.startsWith(CACHE_PREFIX) && key !== getKey()) {
-                console.log(`Try to delete: ${key}`);
+                console.log(`Delete: ${key}`);
                 return caches.delete(key);
             } else {
                 return Promise.resolve(false);
@@ -53,18 +99,16 @@ self.addEventListener("activate", event => {
 self.addEventListener("install", async event => {
     const key = getKey();
     const cache = await caches.open(key);
-    console.log(`${key} is installed`);
+    console.log(`Installed: ${key}`);
     event.waitUntil(cache.addAll(cacheTargets[key]));
 });
 
+// CacheStorage保存対象でないリソースへのリクエスト時もfetchイベントが発生する。
 self.addEventListener("fetch", async event => {
-    console.log(`Fetched!: ${event.request.url}`);
-    event.respondWith(caches.match(event.request)
-            .catch(async event => {
-                // TODO: CacheStorageにリソースが存在しない状態でもcatchブロックが評価されない。
-                console.log("Recovery request");
-                const res = await fetch(event.request);
-                (await caches.open(getKey())).put(event.request, res.clone());
-                return res;
-            }));
+    const request = event.request;
+    // waitUntilやrespondWithの引数はPromiseでなければならないので，
+    // thenやcatchの関数の戻り値がPromiseでなければ意図した動作にならない場合がある。
+    event.respondWith(caches.match(request)
+            .then(response => checkResponse({request, response})))
+            .catch(() => getErrorPage(request.url));
 });
