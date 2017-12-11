bac5c96d7bff01c7a618e477f4696ec8966cf46b
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2017-12-11 11:58:32 +0900

    Added data class sample

--- lang/kotlin/practicekotlin/src/dataclasses.kt
+++ lang/kotlin/practicekotlin/src/dataclasses.kt
@@ -2,6 +2,20 @@
  * Data classes
  */
 
-fun main(args: Array<String>) {
+private data class Student(val name: String, val score: Int)
 
+// privateを指定していないとコンパイルエラーになる。
+// Studentクラスがprivateだからである。
+private fun createStudent(): Student {
+    return Student("Foo", 98)
+}
+
+fun main(args: Array<String>) {
+    val student = createStudent()
+    println("${student.name} has gotten ${student.score} points")
+
+    val (name, score) = createStudent()
+    println("$name has gotten $score points")
+
+    println("${student.component1()} has gotten ${student.component2()} points")
 }
