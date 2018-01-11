0280b56fc6279be9dff2a921e0c0ae88b61cf87f
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2018-01-11 17:29:18 +0900

    Added simple map example

---
+++ lang/kotlin/practicekotlin/src/practicekotlin/kotlininaction/chapter3/JoinToString.kt
@@ -0,0 +1,9 @@
+/**
+ * JoinToString
+ */
+
+package practicekotlin.kotlininaction.chapter3
+
+fun main(args: Array<String>) {
+    // TODO: Implement
+}
---
+++ lang/kotlin/practicekotlin/src/practicekotlin/kotlininaction/chapter3/MakingFunctionsEasierToCall.kt
@@ -0,0 +1,10 @@
+/**
+ * MakingFunctionsEasierToCall
+ */
+
+package practicekotlin.kotlininaction.chapter3
+
+fun main(args: Array<String>) {
+    val map = mapOf(Pair("foo", 10), Pair("bar", 20), Pair("foo", 30))
+    println(map)
+}
