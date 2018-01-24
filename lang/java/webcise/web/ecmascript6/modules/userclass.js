db4daa58a461eec103e3f8691cde82468837d91a
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2018-01-24 16:18:55 +0900

    Added default export examples

---
+++ lang/java/webcise/web/ecmascript6/modules/userclass.js
@@ -0,0 +1,20 @@
+"use strict";
+
+class User {
+    constructor({name, age}) {
+        this.name = name;
+        this.age = age;
+    }
+    
+    get grownUp() {
+        return this.age >= 20;
+    }
+    
+    toString() {
+        const txt =  `${this.name} is ${this.age} years old. `;
+        const tmp = this.grownUp ? "Grown-up." : "Not grown-up.";
+        return txt + tmp;
+    }
+}
+
+export default User;
