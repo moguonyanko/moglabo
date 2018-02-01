393d542241f4f7b716304e0051f7ef8b4e943550
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2018-02-01 12:00:44 +0900

    Added by keyword example

--- lang/kotlin/practicekotlin/src/practicekotlin/kotlininaction/chapter4/ClassDelegationUsingTheByKeyword.kt
+++ lang/kotlin/practicekotlin/src/practicekotlin/kotlininaction/chapter4/ClassDelegationUsingTheByKeyword.kt
@@ -4,6 +4,34 @@
 
 package practicekotlin.kotlininaction.chapter4
 
+// by innerListと記述することで抽象メソッドの実装をinnerListに委譲できる。
+// MutableCollectionインターフェースの他のメソッドを実装していなくても
+// コンパイルエラーにならない。
+private class LimitedList<T>(val innerList: MutableCollection<T> =
+                             ArrayList<T>(), val limit: Int = 10):
+        MutableCollection<T> by innerList {
+
+    override fun add(element: T): Boolean {
+        return if (size < limit) {
+            innerList.add(element)
+        } else {
+            println("Limit over")
+            false
+        }
+    }
+
+    override fun addAll(elements: Collection<T>): Boolean {
+        return if (size + elements.size < limit) {
+            innerList.addAll(elements)
+        } else {
+            println("Limit over: ${size + elements.size - limit} items")
+            false
+        }
+    }
+}
+
 fun main(args: Array<String>) {
-    // TODO: Implement
+    val list = LimitedList<String>(limit = 2)
+    list.add("Apple")
+    list.addAll(arrayListOf("orange", "banana", "lemon"))
 }
