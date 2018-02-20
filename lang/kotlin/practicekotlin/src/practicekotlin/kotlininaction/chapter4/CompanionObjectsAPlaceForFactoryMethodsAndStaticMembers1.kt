83c1f2700030b50b9ea76e5307e8d6600a20786d
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2018-02-20 17:47:26 +0900

    Modified comment

--- lang/kotlin/practicekotlin/src/practicekotlin/kotlininaction/chapter4/CompanionObjectsAPlaceForFactoryMethodsAndStaticMembers1.kt
+++ lang/kotlin/practicekotlin/src/practicekotlin/kotlininaction/chapter4/CompanionObjectsAPlaceForFactoryMethodsAndStaticMembers1.kt
@@ -9,6 +9,8 @@ import java.util.Random
 private fun getRandomItemNumber(bound: Int) = Random().nextInt(bound)
 
 // companion objectはstaticのメンバをまとめて定義するための要素だと理解すれば良いか？
+// constructorがprivateで宣言されているのでこのクラス外でコンストラクタを呼び出して
+// オブジェクト生成することはできない。
 private class MyItem3 private constructor(val name: String, val number: Int = 0) {
     companion object {
         fun newItem(name: String) = MyItem3(name)
@@ -21,6 +23,9 @@ private class MyItem3 private constructor(val name: String, val number: Int = 0)
 }
 
 fun main(args: Array<String>) {
+    // コンストラクタはprivateで宣言されているためコンパイルエラー
+    //val item0 = MyItem3("lemon")
+
     val item1 = MyItem3.newItem("hobby car")
     val item2 = MyItem3.newItem("bike", 10)
     val items = listOf(item1, item2)
