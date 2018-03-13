9e63b4d4346609abdee9699103f9473af3349a6f
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2018-03-13 19:00:25 +0900

    Removed unnecessary methods

--- lang/java/exercise/test/test/exercise/lang/TestSyntax.java
+++ lang/java/exercise/test/test/exercise/lang/TestSyntax.java
@@ -1,36 +1,13 @@
 package test.exercise.lang;
 
-import static org.hamcrest.CoreMatchers.is;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 import exercise.lang.EnumFruits;
 import exercise.lang.StaticFruits;
 
 public class TestSyntax {
-	
-	public TestSyntax() {
-	}
-	
-	@BeforeClass
-	public static void setUpClass() {
-	}
-	
-	@AfterClass
-	public static void tearDownClass() {
-	}
-	
-	@Before
-	public void setUp() {
-	}
-	
-	@After
-	public void tearDown() {
-	}
 
 	@Test
 	public void 三項演算子の優先度を調べる(){
