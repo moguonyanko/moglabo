29268fa34492097d2dd6f5959123af4d24e194d3
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2018-05-17 18:39:04 +0900

    Added custom readable stream example

--- lang/java/webcise/web/studyinghtml5/stream/main.js
+++ lang/java/webcise/web/studyinghtml5/stream/main.js
@@ -2,6 +2,7 @@ import sm from "./stream.js";
 
 const main = () => {
     customElements.define("simple-image-stream", sm.element.SimpleImageStream);
+    customElements.define("custom-number-stream", sm.element.CustomNumberStream);
 };
 
 window.addEventListener("DOMContentLoaded", main);
--- lang/java/webcise/web/studyinghtml5/stream/stream.js
+++ lang/java/webcise/web/studyinghtml5/stream/stream.js
@@ -12,7 +12,7 @@ const createReadableStream = async src => {
                     break;
                 }
                 controller.enqueue(value);
-            };
+            }
             controller.close();
             reader.releaseLock();
         }
@@ -56,9 +56,118 @@ class SimpleImageStream extends HTMLElement {
     }
 }
 
+let readableNumber = 1;
+let readedNumbers = [];
+
+// readableNumberがlimitに達する前に呼び出された場合であってもdoneはtrueになっていない。
+// しかしその時CustomNumberStreamのpullが呼び出されてdoReadが中止される。
+// なおlimitに達した後呼び出された場合でもdoneがtrueになっていることは無い。
+const readStream = async ({stream, limit}) => {
+    const reader = stream.getReader();
+    while (true) {
+        const {done, value} = await reader.read();
+        if (done || value > limit) {
+            console.log(`Done -> ${done}, value > limit -> ${value > limit}`);
+            break;
+        }
+        readedNumbers.push(value);
+    }
+    // 呼び出しても呼び出さなくてもエラーにはならない。
+    reader.releaseLock();
+};
+
+const getAttributes = ({obj, attributeNames = [], parser = v => v}) => {
+    const attribtues = attributeNames.reduce((acc, name) => {
+        const elementAttr = obj.getAttribute(name);
+        const attr = parser(elementAttr);
+        if (isNaN(attr)) {
+            throw new TypeError(`Invalid attribute: ${elementAttr}`);
+        }
+        acc[name] = attr;
+        return acc;
+    }, {});
+    return attribtues;
+};
+
+/**
+ * @description 
+ * ReadableStreamDefaultControllerに連番をenqueueしていって、
+ * 後でreadしその結果を出力するサンプルcustom element。
+ */
+class CustomNumberStream extends HTMLElement {
+    constructor() {
+        super();
+
+        Object.assign(this, getAttributes({
+            obj: this,
+            attributeNames: [
+                "initial",
+                "limit",
+                "interval"
+            ],
+            parser: parseInt
+        }));
+
+        this.initialNumber = parseInt(this.getAttribute("initial"));
+        this.limitNumber = parseInt(this.getAttribute("limit"));
+        this.interval = parseInt(this.getAttribute("interval"));
+
+        const shadow = this.attachShadow({mode: "open"});
+        const template = document.querySelector(".custom-number-stream-template");
+        shadow.appendChild(template.content.cloneNode(true));
+    }
+
+    get output() {
+        const base = this.shadowRoot.querySelector(".base");
+        return base.querySelector(".output");
+    }
+
+    connectedCallback() {
+        const base = this.shadowRoot.querySelector(".base");
+        const output = this.output;
+        const limit = this.limitNumber;
+        const interval = this.interval;
+        let number = this.initialNumber;
+        let intervalId;
+
+        const doRead = async ({stream, controller}) => {
+            clearInterval(intervalId);
+            await readStream({stream, limit});
+            output.innerHTML += `Readed numbers: ${readedNumbers} limit ${limit}<br />`;
+            // releaseLockをclose内部で呼び出しているのかもしれない。
+            controller.close();
+        };
+
+        const stream = new ReadableStream({
+            start(controller) {
+                intervalId = window.setInterval(() => {
+                    output.innerHTML = `${number}<br />`;
+                    controller.enqueue(number++);
+                }, interval);
+
+                base.addEventListener("click", async event => {
+                    if (event.target.classList.contains("read")) {
+                        event.stopPropagation();
+                        await doRead({stream, controller});
+                    }
+                });
+            },
+            pull(controller) {
+                console.log("Pulled");
+                console.log(controller);
+            },
+            cancel(reason) {
+                console.log(`Canceled: ${reason}`);
+                clearInterval(intervalId);
+            }
+        });
+    }
+}
+
 const streamLib = {
     element: {
-        SimpleImageStream
+        SimpleImageStream,
+        CustomNumberStream
     }
 };
 
