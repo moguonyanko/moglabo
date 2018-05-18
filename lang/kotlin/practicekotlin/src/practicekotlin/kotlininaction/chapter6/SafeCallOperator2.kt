f27f21e2c13fcfe3296a9f58c2b84641817e9ff9
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2018-05-18 15:11:46 +0900

    Added more safe operator example

--- lang/kotlin/practicekotlin/src/practicekotlin/kotlininaction/chapter6/SafeCallOperator2.kt
+++ lang/kotlin/practicekotlin/src/practicekotlin/kotlininaction/chapter6/SafeCallOperator2.kt
@@ -4,6 +4,33 @@
 
 package practicekotlin.kotlininaction.chapter6
 
+private class Address(val city: String?, val town: String?, val lot: String?) {
+    override fun toString() = "$city:$town:$lot"
+}
+
+private class Profile(val name: String, val address: Address?)
+
+private class User(val profile: Profile?) {
+    override fun toString() =
+            "My name is ${profile?.name}, I am in ${profile?.address}"
+}
+
+// 戻り値の型を明示しないとUnit型が返されるとみなされて
+// コンパイルエラーになってしまう。
+private fun User.isInFirstLot(): Boolean {
+    // プロパティを参照するコードの途中でnullがあると変数にnullが格納される。
+    val lot = this.profile?.address?.lot
+    println("The lot number is $lot")
+    return lot?.startsWith("1") ?: false
+}
+
 fun main(args: Array<String>) {
-    // TODO: Implement
+    val address = Address(city = "FooCity", town = "BarTown", lot = "1-2-3")
+    val profile = Profile("Mike", address)
+    val user1 = User(profile)
+    println(user1)
+    println(user1.isInFirstLot())
+    val user2 = User(Profile(name = "Tom", address = null))
+    println(user2)
+    println(user2.isInFirstLot())
 }
