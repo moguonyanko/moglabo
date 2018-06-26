a9c78167d491f6a789695fc9fb5280f26d6ecebe
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2018-06-26 11:24:19 +0900

    Added local variable and collection examples

--- lang/java/exercise/test/test/exercise/lang/TestLocalVariable.java
+++ lang/java/exercise/test/test/exercise/lang/TestLocalVariable.java
@@ -1,5 +1,9 @@
 package test.exercise.lang;
 
+import java.util.Arrays;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
@@ -9,10 +13,26 @@ public class TestLocalVariable {
     @Test
     public void defineLocalVariable() {
         float x = 5.5F;
-        double y = 5.5;
-        // IDE未対応
-        //var y = 5.5;
+        var y = 5.5;
         assertThat(x, is((float)y));
+    }
+
+    @Test
+    public void defineLambdaWithTypeInference() {
+        BiFunction<Integer, Integer, Integer> add =
+            (x, y) -> x + y;
+
+        int expected = 10;
+        int actual = add.apply(3, 7);
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void defineListByLocalVariable() {
+        var list = Arrays.asList(1, 2, 3);
+        var actual = list.stream().map(i -> i * i).collect(Collectors.toList());
+        var expected = Arrays.asList(1, 4, 9);
+        assertThat(actual, is(expected));
     }
 
 }
