aeb419d4c696d78e6c20a6204df38e47ef99faaf
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2019-03-01 14:37:22 +0900

    Added dynamic greeting button example

--- lang/java/webcise/web/ecmascript6/classes/field.js
+++ lang/java/webcise/web/ecmascript6/classes/field.js
@@ -1,7 +1,10 @@
 /**
- * ネイティブHTML要素の拡張であればサードパーティ製ライブラリのスタイルシートも
- * 適用されやすくなる。
+ * @fileOverview class field調査用スクリプト
+ * @description 2019/03/01時点ではChromeでしか動作しない。
  */
+ 
+// ネイティブHTML要素の拡張であればサードパーティ製ライブラリのスタイルシートも
+// 適用されやすくなる。
 class Greeting extends HTMLButtonElement {
 
   static DEFIEND_VALUES = [
@@ -37,8 +40,36 @@ class Greeting extends HTMLButtonElement {
   }
 }
 
+const listener = {
+  appendGreetingButton() {
+    // createElementの引数にisを指定したオブジェクトを渡せばcustom elementの
+    // コンストラクタが呼び出される。
+    const btn = document.createElement('button', { is: 'greeting-button' });
+    // 同じ処理を以下の記述でも行える。
+    //const btn = new Greeting();
+    
+    // 「.public-field .container」の間のスペースが無いと両方のclassが指定された
+    // 1つの要素を探してしまう。
+    // 例: <div class="public-field container"></div>
+    const container = document.querySelector('.public-field .container');
+    container.appendChild(btn);
+  }
+};
+
 const init = () => {
   customElements.define('greeting-button', Greeting, { extends: 'button' });
+  
+  document.querySelectorAll('.example').forEach(el => {
+    el.addEventListener('click', event => {
+      const t = event.target;
+      if (t.classList.contains('eventtarget')) {
+        event.stopPropagation();
+      }
+      if (typeof listener[t.value] === 'function') {
+        listener[t.value]();
+      }
+    });
+  });
 };
 
 window.addEventListener('DOMContentLoaded', init);
