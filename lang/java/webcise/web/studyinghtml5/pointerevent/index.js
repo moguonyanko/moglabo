aa38d74c76bee6a9871953569badd34f6e22581b
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2019-03-22 17:20:23 +0900

    Added comment for private field

--- lang/java/webcise/web/studyinghtml5/pointerevent/index.js
+++ lang/java/webcise/web/studyinghtml5/pointerevent/index.js
@@ -2,4 +2,7 @@ import PointerInfo from './pointerevent.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   customElements.define('pointer-info', PointerInfo, {extends: 'div'});
+  
+  // constructor経由でもprivateフィールドにはアクセスできない。
+  //console.log(PointerInfo.constructor.#CAPTURE_EVENTS);
 });
