9eb03f544ef9cef057b91387e5920f520fccc830
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2019-04-26 19:17:44 +0900

    Modified ImageBitMap drawing

--- lang/javascript/webxam/public/webapi/canvas/drawworker.js
+++ lang/javascript/webxam/public/webapi/canvas/drawworker.js
@@ -1,8 +1,20 @@
-const drawCircle = ({context, width, height}) => {
-  context.beginPath();
-  const r = 10 + parseInt(Math.random() * 50);
-  context.arc(Math.random() * width, Math.random() * height, r, 0, 2 * Math.PI);
-  context.fill();
+let drawnInfo;
+
+const drawSquare = ({context, width, height}) => {
+  const size = 10 + parseInt(Math.random() * 50);
+  const x = parseInt(Math.random() * width),
+      y = parseInt(Math.random() * height),
+      w = size,
+      h = size;
+
+  if (!drawnInfo) {
+    drawnInfo = {};
+  } else {
+    context.clearRect(drawnInfo.x, drawnInfo.y, drawnInfo.w, drawnInfo.h);
+  }
+
+  Object.assign(drawnInfo, {x, y, w, h});
+  context.fillRect(x, y, w, h);
 };
 
 globalThis.onmessage = event => {
@@ -11,13 +23,17 @@ globalThis.onmessage = event => {
 
   const context = canvas.getContext('2d');
   context.fillStyle = fillStyle;
-  const render = time => {
-    context.clearRect(0, 0, width, height);
-    drawCircle({context, width, height});
+  const render = async time => {
+    drawSquare({context, width, height});
     // WorkerからのpostMessageにOffscreenCanvasを直接渡すことはできない。
     // 第2引数にOffscreenCanvasを指定してもダメ。ImageBitMapなら渡すことができる。
     // 参考: https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Structured_clone_algorithm
-    globalThis.postMessage(canvas.transferToImageBitmap());
+    const bitmap = canvas.transferToImageBitmap();
+    // TODO: 常に1つの図形しか描画できていない。
+    // createImageBitMapにはOffscreenCanvasを直接渡せる。
+    //const bitmap = await createImageBitmap(canvas,
+    //    drawnInfo.x, drawnInfo.y, drawnInfo.w, drawnInfo.h);
+    globalThis.postMessage(bitmap);
     requestAnimationFrame(render);
   };
 
