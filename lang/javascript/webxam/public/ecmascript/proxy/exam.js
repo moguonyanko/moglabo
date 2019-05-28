a5bc63a10bb8a9e9b884f414da1634b95ceb32a7
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2019-05-28 18:12:25 +0900

    Added Proxy construct example

--- lang/javascript/webxam/public/ecmascript/proxy/exam.js
+++ lang/javascript/webxam/public/ecmascript/proxy/exam.js
@@ -2,6 +2,44 @@
  * @fileoverview Proxy調査用スクリプト
  */
 
+const traceNewInstance = ({klass, onTrace}) => {
+    const handler = {
+        construct(target, args, newTarget) {
+            if (typeof onTrace === 'function') {
+                onTrace(target, args, newTarget);
+            }
+            // newTargetをnewするとnew演算子によるインスタンス生成時に
+            // スタックオーバーフローになる。
+            return new target(...args);
+        }
+    };
+    return new Proxy(klass, handler);
+};
+
+function Sample() {}
+
+// classで宣言するとSample.applyがエラーになる。
+Sample.prototype.toString = () => 'This is Sample class';
+
+function Other() {}
+
+Other.prototype.toString = () => 'This is Other class';
+
+const createInstance = ({type, onTrace}) => {
+    const P = traceNewInstance({klass: Sample, onTrace});
+    const sampleArgs = ['Hello', 123, true];
+    if (type === 'reflect') {
+        return Reflect.construct(P, sampleArgs, Other);
+    } else if (type === 'object') {
+        // Object.createはProxyのhandler.construct()でトラップされない。
+        const o = Object.create(P.prototype);
+        Other.apply(o, sampleArgs);
+        return o;
+    } else {
+        return new P(...sampleArgs);
+    }
+}
+
 const createVirtualMultipleArray = n => {
     if (isNaN(n)) {
         throw new Error(`${n} is not a number`);
@@ -10,6 +48,7 @@ const createVirtualMultipleArray = n => {
         get(target, index) {
             return index * n;
         },
+        // in演算子で要素を調べた時に呼び出される。配列に対しても同様。
         has(target, number) {
             return number % n === 0;
         }
@@ -36,6 +75,17 @@ const listeners = {
                 output.innerHTML = `${v} is not a multiple of ${n}`;
             }
         });
+    },
+    checkConstruction(root, target) {
+        const type = target.value;
+        const output = root.querySelector('.output');
+        output.innerHTML = '';
+        const onTrace = (target, args, newTarget) => {
+            const log = `Trapped: ${target.name}, ${args.join(',')}, ${newTarget.name}<br />`;
+            output.innerHTML += log;
+        };
+        const instance = createInstance({type, onTrace});
+        output.innerHTML += `${instance.toString()}<br />`;
     }
 };
 
@@ -46,14 +96,15 @@ const emitEvent = (event, root) => {
     }
     event.stopPropagation();
     if (typeof listeners[name] === 'function') {
-        listeners[name](root);
+        listeners[name](root, event.target);
     }
 };
 
 const addListener = () => {
     const exams = document.querySelectorAll('.example');
     Array.from(exams).forEach(root => {
-        root.addEventListener('pointerup', event => {
+        // radioに対してはpointerupが発生しない。
+        root.addEventListener('click', event => {
             emitEvent(event, root);
         });
     });
