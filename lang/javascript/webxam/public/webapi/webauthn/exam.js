64dbd20236bbce9af0c3e833f011ff4791871209
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2019-06-21 18:10:50 +0900

    Added WebAuthn credential getter example

--- lang/javascript/webxam/public/webapi/webauthn/exam.js
+++ lang/javascript/webxam/public/webapi/webauthn/exam.js
@@ -2,100 +2,144 @@
  * @fileoverview WebAuthn調査用スクリプト
  */
 
+const defaultTimeout = 10000;
+
 const getChallenge = async () => {
-    const response = await fetch('/webxam/service/randomnumber');
-    if (!response.ok) {
-        throw new Error(`Cannot create challenge: ${response.status}`);
+  const response = await fetch('/webxam/service/randomnumber');
+  if (!response.ok) {
+    throw new Error(`Cannot create challenge: ${response.status}`);
+  }
+  return await response.json();
+};
+
+/**
+ * テスト用のユーザーIDをランダムな文字列として生成する。
+ */
+const createUserId = async () => {
+  const response = await fetch('/webxam/service/randomstring?linelimit=1');
+  if (!response.ok) {
+    throw new Error(`Cannot create user id: ${response.status}`);
+  }
+  return await response.text();
+};
+
+const createCredentialArgs = async ({ authenticatorAttachment, extensions }) => {
+  const args = {
+    publicKey: {
+      rp: {
+        name: 'My Auth Sample App'
+        // idは適当な値を指定するとエラーになる。
+        //,id: 'sample.com'
+      },
+      user: {
+        id: Uint8Array.from(await createUserId(), c => c.charCodeAt(0)),
+        name: 'sampleuser@example.org',
+        displayName: 'Sample User'
+      },
+      pubKeyCredParams: [{
+        type: 'public-key',
+        alg: -7
+      }],
+      attestation: 'direct',
+      authenticatorSelection: { authenticatorAttachment },
+      timeout: defaultTimeout,
+      challenge: new Uint8Array(await getChallenge()).buffer,
+      extensions
     }
-    return await response.json();
+  };
+  return args;
 };
 
-const createCredentialArgs = async ({ authenticatorAttachment }) => {
-    const args = {
-        publicKey: {
-            // Relying Party (a.k.a. - Service):
-            rp: {
-                name: 'Acme'
-            },
-            user: {
-                id: new Uint8Array(16),
-                name: 'moguonyanko@example.org',
-                displayName: 'Moguo Nyanko'
-            },
-            pubKeyCredParams: [{
-                type: 'public-key',
-                alg: -7
-            }],
-            attestation: 'direct',
-            authenticatorSelection: { authenticatorAttachment },
-            timeout: 20000,
-            challenge: new Uint8Array(await getChallenge()).buffer
-        }
-    };
-
-    return args;
+const getCredentialArgs = async allowCredentials => {
+  const args = {
+    publicKey: {
+      timeout: defaultTimeout,
+      allowCredentials,
+      challenge: new Uint8Array(await getChallenge()).buffer
+    }
+  };
+  return args;
 };
 
-const getCredentialArgs = async () => {
-    const args = {
-        publicKey: {
-            timeout: 20000,
-            // allowCredentials: [newCredential]
-            challenge: new Uint8Array(await getChallenge()).buffer
-        }
-    };
-    return args;
+const getCredential = async rawId => {
+  const allowCredentials = [{
+    id: rawId,
+    transports: ['usb', 'nfc', 'ble'],
+    type: 'public-key'
+  }];
+  const getArgs = await getCredentialArgs(allowCredentials);
+  return navigator.credentials.get(getArgs);
 };
 
-const tryAuth = async ({ authenticatorAttachment }) => {
-    const createArgs = await createCredentialArgs({ authenticatorAttachment });
-    const cred = await navigator.credentials.create(createArgs);
-    console.log('新規アカウント作成', cred);
-    const idList = [{
-        id: cred.rawId,
-        transports: ['usb', 'nfc', 'ble'],
-        type: 'public-key'
-    }];
-    const getArgs = await getCredentialArgs();
-    // 今回はアカウント情報をそのままコピーして使う。
-    getArgs.publicKey.allowCredentials = idList;
-    return navigator.credentials.get(getArgs);
+const createCredential = async ({ authenticatorAttachment, extensions }) => {
+  const createArgs =
+    await createCredentialArgs({ authenticatorAttachment, extensions });
+  return await navigator.credentials.create(createArgs);
 };
 
 // DOM
 
 const getAttachment = el => {
-    return Array.from(el.querySelectorAll(`input[name='attachment']`))
-        .filter(el => el.checked)[0].value;
+  return Array.from(el.querySelectorAll(`input[name='attachment']`))
+    .filter(el => el.checked)[0].value;
 };
+
+// TODO: 
+// rawIdを保持している。idからrawIdを得る方法が分かればidを保持するようにしたい。
+let lastCredentialId;
 
 const listeners = {
-    async executeAuth(el) {
-        const o = el.querySelector('.output');
-        try {
-            const args = {
-                authenticatorAttachment: getAttachment(el)
-            };
-            const result = await tryAuth(args);
-            o.innerHTML += `${result}<br />`;
-        } catch (err) {
-            o.innerHTML += `<span class="error">${err.message}</span><br />`;
+  async executeAuth(el) {
+    const o = el.querySelector('.output');
+    try {
+      const args = {
+        authenticatorAttachment: getAttachment(el),
+        extensions: {
+          loc: true,
+          uvi: true
         }
-    },
-    clearOutput(el) {
-        const o = el.querySelector('.output');
-        o.innerHTML = '';
+      };
+      const cred = await createCredential(args);
+      console.log(cred);
+      lastCredentialId = cred.rawId;
+      o.innerHTML = `${cred.id}<br />`;
+    } catch (err) {
+      o.innerHTML = `<span class="error">${err.message}</span><br />`;
     }
+  },
+  clearOutput(el) {
+    el.querySelector('.output').innerHTML = '';
+  },
+  async getLastClientData(el) {
+    const o = el.querySelector('.output');
+    try {
+      //const targetId = el.querySelector('.credentialid').value;
+      const cred = await getCredential(lastCredentialId);
+      const data = new TextDecoder('UTF-8').decode(cred.response.clientDataJSON);
+      const json = JSON.parse(data);
+      o.innerHTML = `${JSON.stringify(json)}<br />`;
+    } catch (err) {
+      o.innerHTML = `<span class="error">${err.message}</span><br />`;
+    }
+  },
+  async checkEnableAuthByPlatform() {
+    // eslint-disable-next-line no-undef
+    if (await PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable()) {
+      alert('プラットフォームによる認証可能');
+    } else {
+      alert('プラットフォームによる認証不可能');
+    }
+  }
 };
 
-window.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('.example').forEach(el => {
-        el.addEventListener('pointerup', async event => {
-            const et = event.target.dataset.eventTarget;
-            if (!et || typeof listeners[et] !== 'function') {
-                return;
-            }
-            await listeners[et](el);
-        });
+window.addEventListener('DOMContentLoaded', async () => {
+  document.querySelectorAll('.example').forEach(el => {
+    el.addEventListener('pointerup', async event => {
+      const et = event.target.dataset.eventTarget;
+      if (!et || typeof listeners[et] !== 'function') {
+        return;
+      }
+      await listeners[et](el);
     });
+  });
 });
--- lang/javascript/webxam/service/randomString.js
+++ lang/javascript/webxam/service/randomString.js
@@ -10,22 +10,25 @@ const Chance = require('chance');
 const url = require('url');
 
 class RandomString {
-    constructor() {
-        this.chance = new Chance();
-        this.contentType = 'text/plain';
-    }
+  constructor() {
+    this.chance = new Chance();
+    this.contentType = 'text/plain';
+  }
 
-    execute({ request, response }) {
-        const query = url.parse(request.url, true).query;
-        const paramLimit = parseInt(query.linelimit);
-        const lineLimit = !isNaN(paramLimit) ? paramLimit : 10;
-        let currentLine = 0;
-        while (this.chance.bool({ likelihood: 95 }) &&
-            currentLine < lineLimit) {
-            response.write(`${this.chance.string()}\n`);
-            currentLine += 1;
-        }
+  execute({ request, response }) {
+    const query = url.parse(request.url, true).query;
+    const paramLimit = parseInt(query.linelimit);
+    const lineLimit = !isNaN(paramLimit) ? paramLimit : 10;
+    let currentLine = 0;
+    while (this.chance.bool({ likelihood: 95 })) {
+      response.write(`${this.chance.string()}`);
+      currentLine += 1;
+      if (currentLine >= lineLimit) {
+        break;
+      }
+      response.write(`\n`);
     }
+  }
 }
 
 module.exports = RandomString;
