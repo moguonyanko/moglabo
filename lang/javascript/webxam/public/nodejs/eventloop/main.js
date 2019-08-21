b6112ff443e7fe5862ca991a91a90a41b637891d
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2019-08-21 16:35:00 +0900

    Modified event loop block request sample

--- lang/javascript/webxam/public/nodejs/eventloop/main.js
+++ lang/javascript/webxam/public/nodejs/eventloop/main.js
@@ -2,19 +2,40 @@
  * @fileoverview Event Loop調査用スクリプト(Client)
  */
 
+const defaultTimeout = 5000; // ms
+
+/**
+ * 指定した時間(ms)でタイムアウトするようにfetchするための関数である。
+ * FetchAPIにタイムアウトは存在しない。
+ * 参考:
+ * https://stackoverflow.com/questions/46946380/fetch-api-request-timeout
+ */
+const fetchWithTimeout = ({ url, timeout = defaultTimeout }) => {
+  return new Promise((resolve, reject) => {
+    const aborter = new AbortController();
+    const id = setTimeout(() => {
+      aborter.abort();
+      clearTimeout(id);
+      reject(new Error('Timeout!'));
+    }, timeout);
+    fetch(url, aborter).then(resolve, reject);
+  });
+};
+
 const funcs = {
-  async redos () {
+  async redos({ withNewLine = false }) {
     const samplePath = new Array(100).fill('/');
+    samplePath.push('/../node');
     // 末尾の改行が原因でNodeのEventLoopがブロックされてしまう。
-    samplePath.push('/../node\n');
-    // 以下のようなパスであればブロックされないがそもそもクライアントから
-    // パスを受け取るようなコードは避けるべきである。
-    //const samplePath = '/usr/local/bin/node';
+    // (そもそもクライアントからパスを受け取るようなコードは避けるべきである。)
+    if (withNewLine) {
+      samplePath.push('\n');
+    }
     const url = [
       '/webxam/apps/practicenode/eventloop/redos?',
       `path=${encodeURIComponent(samplePath.join(''))}`
     ].join('');
-    const response = await fetch(url);
+    const response = await fetchWithTimeout({ url });
     if (!response.ok) {
       throw new Error(`Redos example error: ${response.status}`);
     }
@@ -24,18 +45,31 @@ const funcs = {
 
 // DOM
 
+const getArgs = {
+  redos () {
+    return {
+      withNewLine: document.getElementById('withnewline').checked
+    };
+  }
+};
+
 const addListener = () => {
   const main = document.querySelector('main');
   main.addEventListener('pointerup', async event => {
-    const f = funcs[event.target.dataset.eventTarget];
+    const key = event.target.dataset.eventTarget;
+    const f = funcs[key];
     if (typeof f !== 'function') {
       return;
     }
-    const result = await f();
     const output = event.target.parentNode.querySelector('.output');
-    output.textContent = JSON.stringify(result);
+    try {
+      const result = await f(getArgs[key](event.currentTarget));
+      output.textContent = JSON.stringify(result);
+    } catch (err) {
+      output.textContent = err.message;
+    }
   });
-};  
+};
 
 window.addEventListener('DOMContentLoaded', () => {
   addListener();
