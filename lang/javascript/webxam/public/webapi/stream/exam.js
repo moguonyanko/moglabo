6f89c87eff20441448cd9f94c381306861fffd22
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2019-09-06 18:23:56 +0900

    Added WritableStream example

--- lang/javascript/webxam/public/webapi/stream/exam.js
+++ lang/javascript/webxam/public/webapi/stream/exam.js
@@ -19,6 +19,8 @@ const readViaStream = async ({ resource, contentType }) => {
             controller.close();
             break;
           }
+          // ここで少しずつvalueに何らかの処理を行うなどしなければ
+          // Streamの利点がないのでは？
           controller.enqueue(value);
         }
       })();
@@ -31,12 +33,58 @@ const readViaStream = async ({ resource, contentType }) => {
   return await newResponse.blob();
 };
 
+// TODO: 少しずつ読み込むようにしたかったが上手くいっていない。
+// eslint-disable-next-line no-unused-vars
+const writeStreamFromFile = ({ writeStream, file }) => {
+  const writer = writeStream.getWriter();
+  const fileReader = new FileReader();
+  fileReader.onprogress = () => writer.write(fileReader.result);
+  const step = 4;
+  for (let start = 0, end = step; start < file.size;
+    start += step, end = start + step) {
+    fileReader.readAsBinaryString(file.slice(start, end));
+  }
+  writer.close();
+};
+
+const writeViaStream = ({ file, width, height }) => {
+  const canvas = new OffscreenCanvas(width, height),
+    ctx = canvas.getContext('2d');
+
+  return new Promise((resolve, reject) => {
+    const writeStream = new WritableStream({
+      write(chunk) {
+        return new Promise(async (resolve, reject) => {
+          try {
+            const bitmap = await createImageBitmap(chunk);
+            ctx.drawImage(bitmap, 0, 0, width, height);
+            resolve(chunk);
+          } catch (err) {
+            reject(err);
+          }
+        });
+      },
+      close() {
+        resolve(canvas.convertToBlob());
+      },
+      abort(err) {
+        reject(err);
+      }
+    }, new CountQueuingStrategy({ highWaterMark: 1 }));
+
+    const writer = writeStream.getWriter();
+    // 一度にファイル全部をwriteしてしまうのではStreamの意味が無い。
+    writer.write(file);
+    writer.close();
+  });
+};
+
 // DOM
 
 class ReadableStreamExample extends HTMLElement {
   constructor() {
     super();
-    const t = document.getElementById('readable-stream-examle');
+    const t = document.getElementById('readable-stream-example');
     const content = t.cloneNode(true).content;
     const shadow = this.attachShadow({ mode: 'open' });
     shadow.appendChild(content);
@@ -70,6 +118,34 @@ class ReadableStreamExample extends HTMLElement {
   }
 }
 
+class WritableStreamExample extends HTMLElement {
+  constructor() {
+    super();
+    const t = document.getElementById('writable-stream-example');
+    const shadow = this.attachShadow({ mode: 'open' });
+    shadow.appendChild(t.cloneNode(true).content);
+  }
+
+  connectedCallback() {
+    const root = this.shadowRoot;
+    root.addEventListener('change', async event => {
+      const file = event.target.files[0];
+      if (!file) {
+        return;
+      }
+      const canvas = root.querySelector('canvas'),
+        ctx = canvas.getContext('2d');
+      const { width, height } = canvas;
+
+      const blob = await writeViaStream({ file, width, height });
+      const bitmap = await createImageBitmap(blob);
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(bitmap, 0, 0, canvas.width, canvas.height);
+    });
+  }
+}
+
 window.addEventListener('DOMContentLoaded', () => {
   customElements.define('readable-stream', ReadableStreamExample);
+  customElements.define('writable-stream', WritableStreamExample);
 });
