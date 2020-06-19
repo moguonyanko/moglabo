370ce96afb661f7e28fb350482260207a78e93e1
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2020-06-19 21:54:55 +0900

    Added getter via http

--- lang/javascript/gomapp/src/app/hero.service.ts
+++ lang/javascript/gomapp/src/app/hero.service.ts
@@ -4,20 +4,45 @@ import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROS } from './mock_heros';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private herosUrl = 'api/heros';
+
+  constructor(private messageService: MessageService,
+    private http: HttpClient) { }
+
+  private log(content: string): void {
+    this.messageService.add(`${content}, ${new Date().toString()}`);
+  }
+
+  private handleError<T>(opeation: string = 'operation', defaultResult?: T) {
+    const handler = (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`エラー発生:${opeation},${error.message}`);
+      return of(defaultResult as T);
+    };
+    return handler;
+  }
 
   getHero(id: number): Observable<Hero> {
-    this.messageService.add(`HeroService Fetched: ID=${id}, ${new Date().toString()}`);
-    return of(HEROS.find(hero => hero.id === id));
+    const url = `${this.herosUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`HeroService Fetched: ID=${id}`)),
+      catchError(this.handleError<Hero>(`Error at getHero: id=${id}`))
+    );
   }
 
   getHeros(): Observable<Hero[]> {
-    return of(HEROS);
+    return this.http.get<Hero[]>(this.herosUrl)
+      .pipe(
+        tap(heros => this.log('Heros fetched')),
+        catchError(this.handleError<Hero[]>('getHeros', []))
+      );
   }
 }
---
+++ lang/javascript/gomapp/src/app/in-memory-data.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Hero } from './hero';
+
+/**
+ * 参考:
+ * https://angular.jp/tutorial/toh-pt6
+ */
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InMemoryDataService implements InMemoryDbService {
+
+  static DEFAULT_ID: number = 111;
+
+  createDb() {
+    const heros = [
+      { id: InMemoryDataService.DEFAULT_ID, name: 'Hoge' },
+      { id: 112, name: 'Fuga' },
+      { id: 113, name: 'Foo' },
+      { id: 114, name: 'Bar' },
+      { id: 115, name: 'Baz' },
+      { id: 116, name: 'Kitezo' },
+      { id: 117, name: 'Monchi' },
+      { id: 118, name: 'Taro' },
+      { id: 119, name: 'Jiro' },
+      { id: 120, name: 'Usao' }
+    ];
+    // プロパティ名がHeroService.herosUrlのapi以下と一致していないとエラーになる。
+    return { heros };
+  }
+
+  createId(heros: Hero[]): number {
+    let id = InMemoryDataService.DEFAULT_ID;
+    
+    if (heros.length > 0) {
+      id = Math.max(...heros.map(hero => hero.id)) + 1;
+    }
+
+    return id;
+  }
+}
