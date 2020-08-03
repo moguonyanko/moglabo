cdf73a47635b9d880d6f29520742cbb0cd9c1bd3
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2020-08-03 19:25:53 +0900

    Added geoContains example

---
+++ lang/javascript/webxam/public/d3/contains/main.js
@@ -0,0 +1,66 @@
+/**
+ * @fileoverview D3による包含チェックの確認用スクリプト
+ */
+
+const projection = d3.geoEquirectangular();
+
+const getGenerator = context => {
+  return d3.geoPath()
+    .projection(projection)
+    .context(context);
+};
+
+// DOM
+
+let geojson;
+
+const attentionFeature = () => {
+  const layer = document.querySelector('canvas.attention');
+  const context = layer.getContext('2d');
+  const { width, height } = context.canvas;
+  context.clearRect(0, 0, width, height);
+  context.fillStyle = "#FF0000";
+  const generator = getGenerator(context);
+
+  const devP = d3.mouse(layer);
+  const wrdP = projection.invert(devP);
+  const { features } = geojson;
+  // 重なっている地物を強調する可能性を考慮すると、一回包含を検出しただけで
+  // getContainsのループを打ち切るわけにはいかない。
+  features.forEach(feature => {
+    if (d3.geoContains(feature, wrdP)) {
+      context.beginPath();
+      generator(feature);
+      context.fill();
+    }
+  });
+};
+
+const drawMap = () => {
+  const layer = document.querySelector('canvas.map');
+  const context = layer.getContext('2d');
+  const generator = getGenerator(context);
+
+  projection.fitExtent([[0, 0], [800, 400]], geojson);
+
+  context.fillStyle = '#339999';
+  context.beginPath();
+  generator(geojson);
+  context.fill();
+};
+
+const init = async () => {
+  const response = await fetch('sample.json');
+  if (!response.ok) {
+    throw new Error(`Failded load json: ${response.status}`);
+  }
+  geojson = await response.json();
+  drawMap();
+
+  // d3.mouseでイベント発生時のポインタのデバイス座標を得るためにonメソッドを介して
+  // イベントリスナーを設定する必要がある。
+  d3.select('canvas.attention')
+    .on('pointermove', attentionFeature);
+};
+
+init().then();
