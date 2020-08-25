10b6d5071092719329558aaea2db64054026fc47
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2020-08-25 20:01:37 +0900

    Added texture sample

--- lang/java/exercise/src/test/exercise/graphics/TestImages.java
+++ lang/java/exercise/src/test/exercise/graphics/TestImages.java
@@ -2,10 +2,13 @@ package test.exercise.graphics;
 
 import java.awt.*;
 import java.awt.geom.CubicCurve2D;
+import java.awt.geom.GeneralPath;
 import java.awt.geom.QuadCurve2D;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.awt.image.MultiResolutionImage;
 import java.io.IOException;
+import java.nio.Buffer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -14,6 +17,7 @@ import javax.imageio.ImageIO;
 
 import org.junit.Test;
 import org.junit.Ignore;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.is;
 
@@ -25,34 +29,34 @@ import exercise.graphics.Images;
  */
 public class TestImages {
 
-	private String getFormatName(Path srcPath){
-		return srcPath.getFileName().toString().split("\\.")[1];
-	}
+    private String getFormatName(Path srcPath) {
+        return srcPath.getFileName().toString().split("\\.")[1];
+    }
 
-	@Test
-	public void 画像のフォーマット名を得る() throws IOException {
-		Path srcPath = Paths.get("./sample/star.png");
-		
-		String expected = "png";
-		String actual = Images.getFormatName(srcPath);
-		
-		assertThat(actual.toLowerCase(), is(expected));
-	}
+    @Test
+    public void 画像のフォーマット名を得る() throws IOException {
+        Path srcPath = Paths.get("./sample/star.png");
 
-	@Test
+        String expected = "png";
+        String actual = Images.getFormatName(srcPath);
+
+        assertThat(actual.toLowerCase(), is(expected));
+    }
+
+    @Test
     @Ignore("Images.changeDensityが未実装につきスキップします。")
-	public void 画像の解像度を変更する() throws IOException {
-		Path srcPath = Paths.get("./sample/square.jpg");
-		BufferedImage srcImg = ImageIO.read(srcPath.toFile());
+    public void 画像の解像度を変更する() throws IOException {
+        Path srcPath = Paths.get("./sample/square.jpg");
+        BufferedImage srcImg = ImageIO.read(srcPath.toFile());
 
-		int density = 2;
-		BufferedImage dstImg = Images.changeDensity(srcImg, getFormatName(srcPath), density);
-		Path dstPath = Paths.get("./sample/density_result.jpg");
-		ImageIO.write(dstImg, getFormatName(srcPath), dstPath.toFile());
-	}
+        int density = 2;
+        BufferedImage dstImg = Images.changeDensity(srcImg, getFormatName(srcPath), density);
+        Path dstPath = Paths.get("./sample/density_result.jpg");
+        ImageIO.write(dstImg, getFormatName(srcPath), dstPath.toFile());
+    }
 
-	@Test
-	public void 画像のサイズを変更する() {
+    @Test
+    public void 画像のサイズを変更する() {
         try {
             var srcPath = Paths.get("./sample/stars.png");
             var srcImg = ImageIO.read(srcPath.toFile());
@@ -76,14 +80,14 @@ public class TestImages {
             ImageIO.write(dstImg, format, dstPath.toFile());
 
             //Files.delete(dstPath);
-        } catch(IOException e) {
+        } catch (IOException e) {
             fail(e.getMessage());
         }
     }
 
-	@Test
+    @Test
     public void canGetMultiResolutionImage() {
-	    List<Path> paths = List.of(
+        List<Path> paths = List.of(
             Paths.get("./sample/star2_1.png"),
             Paths.get("./sample/star2_2.png"),
             Paths.get("./sample/star2_3.png")
@@ -99,28 +103,28 @@ public class TestImages {
 
     @Test
     public void canReduceImageSize() throws IOException {
-	    var srcPath = Paths.get("./sample/stars.png");
-	    var srcSize = Files.size(srcPath);
-	    var src = ImageIO.read(srcPath.toFile());
+        var srcPath = Paths.get("./sample/stars.png");
+        var srcSize = Files.size(srcPath);
+        var src = ImageIO.read(srcPath.toFile());
 
-	    //Images.dumpHints(src);
+        //Images.dumpHints(src);
         //System.out.println("----- REDUCING -----");
 
-	    var dst = Images.reduceImage(src);
+        var dst = Images.reduceImage(src);
         //Images.dumpHints(dst);
         var dstPath = Paths.get("./sample/stars_reduced.png");
         var dstFile = dstPath.toFile();
         ImageIO.write(dst, "png", dstFile);
-	    var dstSize = Files.size(dstPath);
-	    //Files.delete(dstPath);
+        var dstSize = Files.size(dstPath);
+        //Files.delete(dstPath);
 
         System.out.println("SRC:" + srcSize + " byte");
         System.out.println("DST:" + dstSize + " byte");
 
-        var srcHints = ((Graphics2D)src.getGraphics()).getRenderingHints();
-        var dstHints = ((Graphics2D)dst.getGraphics()).getRenderingHints();
+        var srcHints = ((Graphics2D) src.getGraphics()).getRenderingHints();
+        var dstHints = ((Graphics2D) dst.getGraphics()).getRenderingHints();
         assertEquals(srcHints, dstHints);
-	    assertTrue(srcSize > dstSize);
+        assertTrue(srcSize > dstSize);
     }
 
     @Test
@@ -149,7 +153,7 @@ public class TestImages {
         var w = 200;
         var h = 200;
         var dstImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
-        var g = (Graphics2D)dstImg.getGraphics();
+        var g = (Graphics2D) dstImg.getGraphics();
 
         var stroke = new BasicStroke(2.0f);
         g.setStroke(stroke);
@@ -166,7 +170,57 @@ public class TestImages {
         g.setColor(Color.BLUE);
         g.draw(c2);
 
-        var dstPath = Paths.get("./sample/curve.png");
+        var dstPath = Paths.get("./sample/curve1.png");
         ImageIO.write(dstImg, "png", dstPath.toFile());
+    }
+
+    @Test
+    public void 線種を変更できる() throws IOException {
+        var w = 200;
+        var h = 200;
+        var dstImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+        var g2 = (Graphics2D) dstImg.getGraphics();
+
+        var stroke = new BasicStroke(1.0f,
+            BasicStroke.CAP_BUTT,
+            BasicStroke.JOIN_MITER,
+            10.0f, new float[]{10.0f}, 0.0f);
+        g2.setStroke(stroke);
+        g2.setColor(Color.BLACK);
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+            RenderingHints.VALUE_ANTIALIAS_ON);
+
+        var path = new GeneralPath();
+        path.moveTo(0, 0);
+        path.lineTo(100, 100);
+        path.lineTo(150, 50);
+        path.lineTo(200, 200);
+        g2.draw(path);
+
+        var dstPath = Paths.get("./sample/stroke1.png");
+        ImageIO.write(dstImg, "png", dstPath.toFile());
+    }
+
+    @Test
+    public void テクスチャーで塗りつぶせる() throws IOException {
+        var textureImage = new BufferedImage(6, 6, BufferedImage.TYPE_INT_ARGB);
+        var tg2 = (Graphics2D)textureImage.getGraphics();
+
+        tg2.setColor(Color.RED);
+        tg2.fillRect(0, 0, 2, 2);
+
+        var anchor = new Rectangle2D.Double(0, 0, textureImage.getWidth(),
+            textureImage.getHeight());
+        var paint = new TexturePaint(textureImage, anchor);
+
+        var image = new BufferedImage(200, 200, BufferedImage.TYPE_INT_ARGB);
+        var g2 = (Graphics2D)image.getGraphics();
+        g2.setColor(Color.PINK);
+        g2.drawRect(50, 50, 100, 100);
+        g2.setPaint(paint);
+        g2.fillRect(50, 50, 100, 100);
+
+        var path = Paths.get("./sample/texture1.png");
+        ImageIO.write(image, "png", path.toFile());
     }
 }
