bebcd2fe43657a7dcc00f8a66d55bc07bb56a667
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2020-09-01 15:09:22 +0900

    Modified guess number game example

--- lang/rust/rustady/kazuate/src/main.rs
+++ lang/rust/rustady/kazuate/src/main.rs
@@ -1,8 +1,7 @@
-//extern crate rand;
+//extern crate rand; // コメントアウトしても一切エラーにならない。
 
 use rand::Rng;
 use std::cmp::Ordering;
-
 use std::io;
 
 // 参考:
@@ -14,41 +13,40 @@ fn main() {
     let max = 51;
     let secret_num = rand::thread_rng().gen_range(min, max);
 
-    println!(
-        "秘密の数字({}から{}まで): {}",
-        min, max, secret_num
-    );
+    // println!(
+    //     "秘密の数字({}から{}まで): {}",
+    //     min, max, secret_num
+    // );
 
     loop {
-    // 可変にしたければmutを指定する。デフォルトは不変。
-    let mut guess = String::new();
+        // 可変にしたければmutを指定する。デフォルトは不変。
+        let mut guess = String::new();
 
-    // 参照もデフォルトで不変なので&mutを付けて可変にする。
-    io::stdin()
-        .read_line(&mut guess)
-        // expectメソッドを呼び出してResultオブジェクトの処理を行っていないと
-        // コンパイラから警告される。軽いチェック例外のようなものか。
-        .expect("行の読み込み失敗！");
+        // 参照もデフォルトで不変なので&mutを付けて可変にする。
+        io::stdin()
+            .read_line(&mut guess)
+            // expectメソッドを呼び出してResultオブジェクトの処理を行っていないと
+            // コンパイラから警告される。軽いチェック例外のようなものか。
+            .expect("行の読み込み失敗！");
 
-    // mutを付けると可変である必要がないとして警告される。
-    // 変数のシャドーイングは許容される。今回のように全く同じ対象を指す時以外は
-    // 用いるべきではない。
-    // trimは空白だけでなく入力文字列の改行も除去する。
-    let guess: u64 = guess.trim().parse()
-    // ここのexpectはないとコンパイルエラーとなる。
-    // 左辺の型がu64と明示されている一方parseが返す型はResultなので
-    // expectを通さないと目的のu64型の値を得ることができないためである。
-        .expect("数値を入力して下さい");
+        // mutを付けると可変である必要がないとして警告される。
+        // 変数のシャドーイングは許容される。今回のように全く同じ対象を指す時以外は
+        // 用いるべきではない。
+        // trimは空白だけでなく入力文字列の改行も除去する。
+        let guess: u64 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("あなたの予想: {}", guess);
+        println!("あなたの予想: {}", guess);
 
-    match guess.cmp(&secret_num) {
-        Ordering::Less => println!("小さすぎ"),
-        Ordering::Greater => println!("大きすぎ"),
-        Ordering::Equal => {
-            println!("当たり！");
-            break;
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("小さすぎ"),
+            Ordering::Greater => println!("大きすぎ"),
+            Ordering::Equal => {
+                println!("当たり！");
+                break;
+            }
         }
-    }
     }
 }
