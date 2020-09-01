1f6825cedf2e32561f0f0dee6e5fc40406c72274
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2020-09-01 15:04:29 +0900

    Added number guess game example

---
+++ lang/rust/rustady/kazuate/src/main.rs
@@ -0,0 +1,54 @@
+//extern crate rand;
+
+use rand::Rng;
+use std::cmp::Ordering;
+
+use std::io;
+
+// 参考:
+// https://doc.rust-jp.rs/book/second-edition/ch02-00-guessing-game-tutorial.html
+fn main() {
+    println!("数当てゲーム");
+
+    let min = 1;
+    let max = 51;
+    let secret_num = rand::thread_rng().gen_range(min, max);
+
+    println!(
+        "秘密の数字({}から{}まで): {}",
+        min, max, secret_num
+    );
+
+    loop {
+    // 可変にしたければmutを指定する。デフォルトは不変。
+    let mut guess = String::new();
+
+    // 参照もデフォルトで不変なので&mutを付けて可変にする。
+    io::stdin()
+        .read_line(&mut guess)
+        // expectメソッドを呼び出してResultオブジェクトの処理を行っていないと
+        // コンパイラから警告される。軽いチェック例外のようなものか。
+        .expect("行の読み込み失敗！");
+
+    // mutを付けると可変である必要がないとして警告される。
+    // 変数のシャドーイングは許容される。今回のように全く同じ対象を指す時以外は
+    // 用いるべきではない。
+    // trimは空白だけでなく入力文字列の改行も除去する。
+    let guess: u64 = guess.trim().parse()
+    // ここのexpectはないとコンパイルエラーとなる。
+    // 左辺の型がu64と明示されている一方parseが返す型はResultなので
+    // expectを通さないと目的のu64型の値を得ることができないためである。
+        .expect("数値を入力して下さい");
+
+    println!("あなたの予想: {}", guess);
+
+    match guess.cmp(&secret_num) {
+        Ordering::Less => println!("小さすぎ"),
+        Ordering::Greater => println!("大きすぎ"),
+        Ordering::Equal => {
+            println!("当たり！");
+            break;
+        }
+    }
+    }
+}
