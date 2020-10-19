27d5e8f8848824cd219b7f9a54ab80cc3fcacbe7
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2020-10-19 15:56:34 +0900

    Added cookie store change event example

--- lang/javascript/webxam/public/webapi/cookiestore/main.js
+++ lang/javascript/webxam/public/webapi/cookiestore/main.js
@@ -4,7 +4,7 @@
 
 class MyCookieStore {
   constructor() {
-    this.store = cookieStore;
+    this.store = window.cookieStore;
   }
 
   async setCookie({ name, value }) {
@@ -28,17 +28,43 @@ const runTest = async () => {
 
 // DOM
 
+const dumpCookie = ({ cookie, query }) => {
+  const output = document.querySelector(query);
+  output.innerHTML = JSON.stringify(cookie);
+};
+
+const cm = new MyCookieStore();
+let preCookie;
+
 const funcs = {
   getCookieInfo: async () => {
     const value = document.querySelector('.cookie-number').value;
-    const cm = new MyCookieStore();
-    await cm.setCookie({
-      name: 'number',
-      value
+    const name = 'number', query = '.output.cookie-info';
+    if (!preCookie) {
+      dumpCookie({
+        cookie: await cm.getCookie('number'),
+        query
+      });
+      // Cookieの値が変化していなくてもchangeイベントは発生する。
+      // setが行われたかどうかだけで判定されているようだ。
+      cm.store.addEventListener('change', event => {
+        for (const cookie of event.changed) {
+          if (cookie.name === name) {
+            console.log(`${preCookie?.value} -> ${cookie.value}`);
+            dumpCookie({ cookie, query });
+          }
+        }
+        for (const cookie of event.deleted) {
+          if (cookie.name === name) {
+            // cookie.valueはdeleteされた時点でプロパティごと削除されている。
+            console.log(`${cookie.name} deleted`);
+            dumpCookie({ cookie, query });
+          }
+        }
     });
-    const cookie = await cm.getCookie('number');
-    const output = document.querySelector('.output.cookie-info');
-    output.innerHTML = JSON.stringify(cookie);
+    }
+    preCookie = await cm.getCookie('number');
+    await cm.setCookie({ name, value });
   }
 };
 
@@ -46,7 +72,10 @@ const addListener = () => {
   const main = document.querySelector('main');
   main.addEventListener('click', async event => {
     const { eventTarget } = event.target.dataset;
-    await funcs[eventTarget](event.target);
+    if (typeof funcs[eventTarget] == 'function') {
+      event.stopPropagation();
+      await funcs[eventTarget](event.target);
+    }
   });
 };
 
@@ -55,4 +84,4 @@ const main = async () => {
   addListener();
 };
 
-main().then();
+Promise.resolve(main());
