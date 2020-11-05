beac6d825971b6be0b7672623b50f79ee85ff3ed
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2020-11-05 23:35:08 +0900

    Change use waitAsync

--- lang/javascript/webxam/public/ecmascript/arraybuffer/reader.js
+++ lang/javascript/webxam/public/ecmascript/arraybuffer/reader.js
@@ -1,6 +1,9 @@
-self.addEventListener('message', event => {
+self.addEventListener('message', async event => {
   const { data } = event;
-  const status = Atomics.wait(data, 0, 0);
+  // Atomics.waitAsyncはブロックすることが許容されないブラウザの
+  // メインスレッドで使用されるのが効果的である。
+  const status = await (Atomics.waitAsync(data, 0, 0)).value;
+  //const status = Atomics.wait(data, 0, 0);
   console.info(`再開${status}:${new Date().toString()}`);
   const result = Atomics.load(data, 0);
   self.postMessage(result);
