b3251de7e76daec065840bce166fd53b0cfaf891
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2020-11-27 18:05:31 +0900

    Added generics example

---
+++ lang/rust/rustady/generics/src/main.rs
@@ -0,0 +1,54 @@
+// 参考
+// https://doc.rust-jp.rs/book-ja/ch10-00-generics.html
+// https://doc.rust-jp.rs/book-ja/ch10-01-syntax.html
+
+fn smallest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut current = list[0];
+
+    for &item in list.iter() {
+        if item < current {
+            current = item;
+        }
+    }
+
+    current
+}
+
+#[derive(Debug)]
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl <T> Point<T> {
+    fn get_x(&self) -> &T {
+        &self.x
+    }
+    fn get_y(&self) -> &T {
+        &self.y
+    }
+}
+
+impl Point<f64> {
+    fn distance_from_origin(&self) -> f64 {
+        (self.get_x().powi(2) + self.get_y().powi(2)).sqrt()
+    }
+}
+
+fn main() {
+    let list = vec![10, 8, 30, 2, 43];
+    let result = smallest(&list);
+    println!("Smallest number = {}", result);
+
+    let list = ['A', 'a', 'X', 'u', 'p'];
+    let result = smallest(&list);
+    println!("Smallest charactor = {}", result);
+
+    // 型がどちらもTでないとコンパイルエラーになる。
+    // let p1 = Point { x: 1.5, y: 1 };
+    // Tがf64ではないのでPoint<f64>に実装したメソッドを呼び出せない。
+    // let p1 = Point { x: 1, y: 1 };
+    let p1 = Point { x: 1.2, y: 1.5 };
+    println!("{:?}", p1);
+    println!("原点からの距離 = {}", p1.distance_from_origin());
+}
