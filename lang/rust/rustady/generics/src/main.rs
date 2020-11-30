f4000f8cf1e476ef13039c66b6dd34322c868b8d
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2020-11-30 12:11:43 +0900

    Added implement trait examle

--- lang/rust/rustady/generics/src/main.rs
+++ lang/rust/rustady/generics/src/main.rs
@@ -45,6 +45,10 @@ trait Member {
     } 
 }
 
+trait Driver {
+    fn get_drive_time(&self) -> i32;
+}
+
 #[derive(Debug)]
 struct Student {
     name: String,
@@ -68,11 +72,71 @@ impl Member for Student {
 struct Employee {
     id: String,
     sarary: i64,
+    drive_time: i32,
 }
 
 impl Member for Employee {
     fn description(&self) -> String {
         format!("{},現在の給与は{}", self.id, self.sarary)
+    }
+}
+
+impl Driver for Employee {
+    fn get_drive_time(&self) -> i32 {
+        self.drive_time
+    }
+}
+
+// 引数は&impl Memberとも書ける。Memberがtraitであるかどうかも使う側には
+// 意識させたくないので&implは使いたくない。
+fn desc(target: &Member) {
+    println!("DESCRIPTION:{}", target.description());
+}
+
+fn desc2<T: Member>(target: &T) {
+    println!("DESCRIPTION2:{}", target.description());
+}
+
+fn desc3<T: Member + Driver>(target: &T) {
+    println!("[{}]の運転時間は{}分", target.name(), target.get_drive_time());
+}
+
+// 型変数が増えて煩雑になる場合はwhereを使って整理できる。
+// しかしwhereを使ってもさほど読みやすくなっているとは思えない。
+// より多数かつ複雑な組み合わせの型変数を定義すれば有用性が分かるのかもしれない。
+fn desc4<T, U>(m1: &T, m2: &U) where T: Member + Driver, U: Member {
+    println!("[{}]が[{}]を{}分で送迎した。", m1.name(), m2.name(), 
+        m1.get_drive_time());
+}
+
+// traitを指定する際にいちいちimplを書かせる仕様は間違っている。
+// 対象の抽象性を使う側に意識させるべきではない。
+// そして複数の具象的な型を返す関数は書けない。
+// すなわち以下のように多態性を利用したコードはコンパイルエラーとなる。
+// fn create_member(t: String) -> impl Member {
+//     if t == "1" {
+//         Employee {
+//             id: String::from("A000"), 
+//             sarary: 20000,
+//             drive_time: 0
+//         }
+//     } else {
+//         Student {
+//             name: String::from("NO NAME"), 
+//             no: -1, 
+//             score: 0 
+//         }
+//     }
+// }
+
+trait Append {
+    fn appended_name(&self) -> String;
+}
+
+// Memberを実装しているオブジェクトに限定してAppendを実装する。
+impl<T: Member> Append for T {
+    fn appended_name(&self) -> String {
+        self.name() + "!!!!!"
     }
 }
 
@@ -98,8 +162,17 @@ fn main() {
     };
     println!("{}", student.name());
     println!("{}", student.description());
+    desc(&student);
     let employee = Employee { 
-        id: String::from("A001"), sarary: 100000 
+        id: String::from("A001"), 
+        sarary: 100000,
+        drive_time: 60
     };
     println!("{}:{}", employee.name(), employee.description());
+    desc2(&employee);
+    desc3(&employee);
+    // Memberしか実装していないStudentは渡すことができない。
+    //desc3(&student);
+    desc4(&employee, &student);
+    println!("{}", student.appended_name());
 }
