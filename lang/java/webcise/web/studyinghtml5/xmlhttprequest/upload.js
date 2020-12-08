aaf40f5b62fc5ed7a8cbd114c22c37418d8e82fe
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2020-12-08 12:58:44 +0900

    Modified progress event handler

--- lang/java/webcise/web/studyinghtml5/xmlhttprequest/upload.js
+++ lang/java/webcise/web/studyinghtml5/xmlhttprequest/upload.js
@@ -1,86 +1,113 @@
 (function (win, doc, m) {
-    "use strict";
+  "use strict";
 
-    var area = m.ref("UploadResultArea");
+  const area = m.ref("UploadResultArea"),
+      progress = m.ref("UploadProgress"),
+      targetInput = m.ref("UploadTargetFile");
 
-    function getXHR() {
-        var xhr = new XMLHttpRequest();
-        return xhr;
+  function getXHR() {
+    var xhr = new XMLHttpRequest();
+    return xhr;
+  }
+
+  function getTargetFile() {
+    return targetInput.files[0];
+  }
+
+  function eventLog(evt) {
+    m.log(evt);
+  }
+
+  function reportError(evt) {
+    alert(evt);
+  }
+
+  const getMaxValue = () => parseInt(progress.getAttribute("max"));
+
+  function setProgress(value) {
+    const max = getMaxValue();
+    if (max <= value) {
+      progress.setAttribute("value", max);
+      progress.innerHTML += max + "% upload!";
+    } else {
+      progress.setAttribute("value", value);
+      progress.innerHTML += value + "% upload now...";
     }
+  }
 
-    function getTargetFile() {
-        var targetInput = m.ref("UploadTargetFile");
-        return targetInput.files[0];
+  const finishProgress = () => {
+    const max = progress.getAttribute("max");
+    progress.setAttribute('value', getMaxValue());
+  };
+
+  const resetProgress = () => progress.setAttribute('value', 0);
+
+  function reportProgress(evt) {
+    if (evt.lengthComputable) {
+      var total = evt.total,
+          loaded = evt.loaded;
+
+      var value = (loaded / total) * 100;
+      setProgress(value);
     }
+  }
 
-    function eventLog(evt) {
-        m.log(evt);
-    }
+  function upload() {
+    return new Promise((resolve, reject) => {
+      var xhr = getXHR();
+      xhr.open("POST", "https://myhost/webcise/Upload");
+      /**
+       * Content-Typeは自動的に「multipart/form-data」になりboundaryも
+       * 自動的に生成される。自分で設定すると正しい「multipart/form-data」の
+       * リクエストボディになっていないとしてサーバ側でエラーになる。
+       */
+      //var boundary = "; boundary=---------------------------sampleboundary2015";
+      //xhr.setRequestHeader("Content-Type", "multipart/form-data" + boundary);
 
-    function reportError(evt) {
-        alert(evt);
-    }
-
-    function setProgress(value) {
-        var progress = m.ref("UploadProgress"),
-                max = parseInt(progress.getAttribute("max"));
-        if (max <= value) {
-            progress.setAttribute("value", max);
-            progress.innerHTML += max + "% upload!";
-        } else {
-            progress.setAttribute("value", value);
-            progress.innerHTML += value + "% upload now...";
+      /* サーバ側でエラーが起きてもloadstartとloadendは発生する。 */
+      xhr.upload.onloadstart = eventLog;
+      xhr.upload.onloadend = eventLog;
+      xhr.upload.onprogress = reportProgress;
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            const res = JSON.parse(xhr.responseText);
+            resolve(res);
+          } else if (xhr.status >= 400) {
+            reject(xhr.status);
+          }
         }
-    }
+      };
+      xhr.upload.onload = eventLog;
+      xhr.upload.ontimeout = eventLog;
+      xhr.upload.onabort = reject;
+      xhr.upload.onerror = reject;
 
-    function reportProgress(evt) {
-        if (evt.lengthComputable) {
-            var total = evt.total,
-                    loaded = evt.loaded;
+      var formData = new FormData();
+      var file = getTargetFile();
+      formData.append("samplefile", file);
+      xhr.send(formData);
+    });
+  }
 
-            var value = (loaded / total) * 100;
-            setProgress(value);
-        }
-    }
+  const reset = () => {
+    resetProgress();
+    m.println(area, "", true);
+  };
 
-    function upload() {
-        var xhr = getXHR();
-        xhr.open("POST", "https://myhost/webcise/Upload");
-        /**
-         * Content-Typeは自動的に「multipart/form-data」になりboundaryも
-         * 自動的に生成される。自分で設定すると正しい「multipart/form-data」の
-         * リクエストボディになっていないとしてサーバ側でエラーになる。
-         */
-        //var boundary = "; boundary=---------------------------sampleboundary2015";
-        //xhr.setRequestHeader("Content-Type", "multipart/form-data" + boundary);
-
-        /* サーバ側でエラーが起きてもloadstartとloadendは発生する。 */
-        xhr.upload.onloadstart = eventLog;
-        xhr.upload.onloadend = eventLog;
-        xhr.upload.onprogress = reportProgress;
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                var res = JSON.parse(xhr.responseText);
-                var message = res.message;
-                m.println(area, message, true);
-            }
-        };
-        xhr.upload.onload = eventLog;
-        xhr.upload.ontimeout = eventLog;
-        xhr.upload.onabort = reportError;
-        xhr.upload.onerror = reportError;
-
-        var formData = new FormData();
-        var file = getTargetFile();
-        formData.append("samplefile", file);
-        xhr.send(formData);
-    }
-
-    (function () {
-        m.addListener(m.ref("UploadRunner"), "click", upload, false);
-        m.addListener(m.ref("ClearProgress"), "click", function () {
-            setProgress(0);
-            m.println(area, "", true);
-        }, false);
-    }());
+  (function () {
+    m.addListener(m.ref("UploadRunner"), "click", async () => {
+      try {
+        const res = await upload();
+        // この時点でonprogressの処理が完了していないことがある。
+        finishProgress();
+        m.println(area, res.message, true);
+      } catch (err) {
+        console.log(err);
+        reportError(err);
+      }
+    }, false);
+    m.addListener(m.ref("ClearProgress"), "click", reset, false);
+    targetInput.addEventListener('change', reset);
+  }());
 }(window, document, my));
