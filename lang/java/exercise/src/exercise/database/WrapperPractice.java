47bc7bc6da0df8b744bee8b049c8f659cb5f2d37
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2020-12-22 20:05:01 +0900

    Modified unwrap example

--- lang/java/exercise/src/exercise/database/WrapperPractice.java
+++ lang/java/exercise/src/exercise/database/WrapperPractice.java
@@ -3,38 +3,39 @@ package exercise.database;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class WrapperPractice {
 
-	/**
-	 * @param args the command line arguments
-	 */
 	public static void main(String[] args) {
-		String url = "jdbc:mysql://localhost:3306/geolib";
-		String user = "geofw", pass = "geofw";
+		var url = "jdbc:mysql://localhost:3306/test";
+		var user = "sampleuser";
+		var pass = "samplepass";
 
-		/* usao is allowed 'SELECT' only */
-		String newUser = "usao", newPassword = "usao";
+		var newUser = "usao";
+		var newPassword = "usapass";
 
 		try (var con = DriverManager.getConnection(url, user, pass)) {
 
-			var mysqlClass = com.mysql.jdbc.Connection.class;
+			var mysqlClass = com.mysql.cj.jdbc.ConnectionImpl.class;
 			if(con.isWrapperFor(mysqlClass)){
 				var myCon = con.unwrap(mysqlClass);
 
+				// changeUserはunwrapされたMySQLのクラスを使う必要がある。
 				myCon.changeUser(newUser, newPassword);
 				System.out.println("User changed " + newUser + " from " + user);
 
 				Statement statement = myCon.createStatement();
-				/* Next query is denied */
-				statement.execute("INSERT INTO batchtest VALUES (0, 'sample0');");
+				statement.execute("INSERT INTO test.authors VALUES ('X001', 'Joe');");
 				System.out.println("INSERT finished.");
+				var rs = statement.executeQuery("SELECT * FROM test.authors;");
+				while (rs.next()) {
+					String name = rs.getString("name");
+					System.out.println(name);
+				}
 			}
 
 		} catch (SQLException ex) {
-			Logger.getLogger(WrapperPractice.class.getName()).log(Level.SEVERE, null, ex);
+			System.err.println(ex.getMessage());
 		}
 	}
 }
