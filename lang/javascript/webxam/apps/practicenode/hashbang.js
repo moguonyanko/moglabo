88b8b7e9f808c9497e48850d346f458f8cf363dc
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2020-12-28 14:36:56 +0900

    Added hashbang grammar sample

---
+++ lang/javascript/webxam/apps/practicenode/hashbang.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+/**
+ * @fileoverview Hashbang Grammar調査用モジュール
+ * 参考:
+ * https://github.com/tc39/proposal-hashbang/
+ * 
+ * nodeコマンドの指定はファイル先頭でなければならない。
+ */
+
+console.log('Hello Hasgbang Grammar');
