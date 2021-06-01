89b71a5ad63e1e5791d26c1021431fd5bf1641de
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2021-06-01 18:09:52 +0900

    Added more client hints example

--- lang/javascript/webxam/public/webapi/useragentclienthints/main.js
+++ lang/javascript/webxam/public/webapi/useragentclienthints/main.js
@@ -7,17 +7,31 @@ const getUAData = () => navigator.userAgentData;
 const getEntroyValue = async name => {
   const uaData = getUAData();
   const ua = await uaData.getHighEntropyValues([name]);
+  console.log(ua);
   return ua[name];
+};
+
+const getBrands = () => {
+  const uaData = getUAData();
+  return uaData.brands.map(item => item.brand);
 };
 
 // DOM
 
-const listeners = {
+const onClick = {
   getEntropy: async () => {
     const name = document.querySelector('.entropy').value;
     const result = await getEntroyValue(name);
-    const output = document.querySelector(`div[data-event-dst]`);
+    const output = document.querySelector(`div[data-event-dst='getEntropy']`);
     output.textContent = result;
+  }
+};
+
+const onInit = {
+  getBrands: () => {
+    const brands = getBrands();
+    const output = document.querySelector(`div[data-event-dst='getBrands']`);
+    output.textContent = brands;
   }
 };
 
@@ -28,15 +42,16 @@ const addListenrs = () => {
     if (!eventSrc) {
       return;
     }
-    if (typeof listeners[eventSrc] === 'function') {
+    if (typeof onClick[eventSrc] === 'function') {
       event.stopPropagation();
-      await listeners[eventSrc]();
+      await onClick[eventSrc]();
     }    
   });
 };
 
 const init = () => {
   addListenrs();
+  Object.values(onInit).forEach(f => f());
 };
 
 init();
