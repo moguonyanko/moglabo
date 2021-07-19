82cf7eb8788b677f93e42f9e5bdf48bc452d7919
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2021-07-20 00:22:33 +0900

    Modified popup example

--- lang/javascript/webxam/public/webapi/popup/main.js
+++ lang/javascript/webxam/public/webapi/popup/main.js
@@ -2,9 +2,22 @@
  * @fileoverview ポップアップの振る舞いを確認するためのスクリプト
  */
 
+// 関数を分けただけではポップアップ扱いにならない。
+const openWindow = url => {
+  window.open(url);
+};
+
 const popupFuncs = {
   windowOpen: ({ url }) => {
-    window.open(url);
+    const img = new Image(400, 400);
+    const container = document.querySelector('.imagecontainer');
+    container.innerHTML = '';
+    // コールバック経由はポップアップとみなされブロック対象となる。
+    img.onload = () => {
+      container.appendChild(img);
+      openWindow(url);
+    };
+    img.src = 'sampleshape.png';
   },
   linkClick: ({ url }) => {
     const a = document.createElement('a');
@@ -15,18 +28,23 @@ const popupFuncs = {
 };
 
 const popupArgs = {
-  // url: 'https://myhost/webxam/webapi/popup/sample.html'
-  // url: 'https://www.google.com/search?q=javascript'
-  // url: 'https://myhost/webxam/apps/practicenode/forward?search=javascript'
+  //url: 'https://myhost/webxam/webapi/popup/sample.html'
+  //url: 'https://www.google.com/search?q=javascript'
+  //url: 'https://myhost/webxam/apps/practicenode/forward?search=javascript'
   url: '/webxam/apps/practicenode/meaning?keyword=javascript'
 };
 
 const addListener = () => {
+  // jQueryを使うかどうかはポップアップかどうかの判定に直接は関係しない。
   const main = document.querySelector('main');
   main.addEventListener('click', event => {
     const { eventTarget } = event.target.dataset;
     if (typeof popupFuncs[eventTarget] === 'function') {
       event.stopPropagation();
+      //setTimeout経由だとユーザーの操作に連動していないとみなされポッポアップ扱いとなる。
+      // setTimeout(() => {
+      //   popupFuncs[eventTarget](popupArgs);
+      // }, 1000);
       popupFuncs[eventTarget](popupArgs);
     }
   });
