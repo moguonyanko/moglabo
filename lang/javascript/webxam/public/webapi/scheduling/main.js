a9573673932d06d2381a1cfb62b5371f9d0fddf8
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2021-09-01 03:52:50 +0900

    Added TaskController example

--- lang/javascript/webxam/public/webapi/scheduling/main.js
+++ lang/javascript/webxam/public/webapi/scheduling/main.js
@@ -6,11 +6,37 @@ const sampleTask = () => {
   return 'Hello Scheduling API'
 };
 
+const sampleSubTask2 = () => {
+  // backgroundが返される。
+  return scheduler.currentTaskSignal.priority;
+};
+
+const sampleTask2 = async () => {
+  const result = await scheduler.postTask(sampleSubTask2, {
+    signal: scheduler.currentTaskSignal,
+    priority: 'background'
+  });
+  return result;
+};
+
 const listener = {
   postTask: async () => {
     const output = document.querySelector(`.output[data-event-output='postTask']`);
-    const result = await scheduler.postTask(sampleTask);   
-    output.textContent = result; 
+    const result = await scheduler.postTask(sampleTask);
+    output.textContent = result;
+  },
+  taskController: async () => {
+    const taskController = new TaskController({
+      priority: 'user-blocking'
+    });
+    setTimeout(async () => {
+      const result = await scheduler.postTask(sampleTask2, { taskController });
+      const output = document.querySelector(`.output[data-event-output='taskController']`);
+      output.textContent = result;
+    }, 1000);
+    // sampleTask2よりも先にabortされるように試みている。
+    // このときabortした場合に返される値に変化はあるか？
+    taskController.abort();
   }
 };
 
