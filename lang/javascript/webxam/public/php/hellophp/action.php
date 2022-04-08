b35f18c3034dc0d3c87d124303f3c1aaf3a200d0
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2022-04-08 16:45:15 +0900

    Added php form example

---
+++ lang/javascript/webxam/public/php/hellophp/action.php
@@ -0,0 +1,2 @@
+ようこそ、<?php echo htmlspecialchars($_POST['name']); ?>！
+あなたの年齢は、<?php echo (int)$_POST['age']; ?> 歳です。
--- lang/javascript/webxam/public/php/hellophp/index.php
+++ lang/javascript/webxam/public/php/hellophp/index.php
@@ -7,6 +7,7 @@
     <title>はじめてのPHP</title>
     <link rel="stylesheet" href="../../common.css" />
     <link rel="stylesheet" href="main.css" />
+    <script type="module" src="main.js" defer></script>
 </head>
 
 <body>
@@ -17,13 +18,36 @@
     <?php
     echo "<strong>こんにちは！はじめてのPHPスクリプトです！</strong>";
     ?>
-    <div class="phpinfo">
+    <section class="info">
+        <h2>PHPINFO</h2>
         <?php phpinfo(); ?>
-    </div>
+    </section>
+    <section class="info">
+        <h2>UserAgent</h2>
+        <?php
+        if (strpos($_SERVER['HTTP_USER_AGENT'], 'Firefox') !== false) {
+        ?>
+            <p>Firefox表示中</p>
+        <?php
+        } else {
+        ?>
+            <p>Firefox以外のブラウザで表示中</p>
+        <?php
+        }
+        ?>
+    </section>
+    <section>
+        <h2>フォーム送信</h2>
+        <form action="./action.php" method="POST">
+            <label>name<input type="text" name="name" value="Taro" /></label>
+            <label>age<input type="number" name="age" value="40" min="0" max="130" /></label>
+            <button>送信</button>
+        </form>
+    </section>
     <footer>
         <h3>参考</h3>
         <ul>
-            <li><a href="https://www.php.net/manual/ja/intro-whatis.php">PHP とはなんでしょう?</a></li>
+            <li><a href="https://www.php.net/manual/ja/getting-started.php">PHPマニュアル</a></li>
         </ul>
     </footer>
 </body>
