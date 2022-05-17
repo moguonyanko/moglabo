f1de069e7dcf6971289efaec01df2999d323b358
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2022-05-18 02:29:14 +0900

    Added php info file

---
+++ lang/javascript/webxam/public/php/phpinfo/index.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>PHP Infomation</title>
+    <link rel="stylesheet" href="../../common.css" />
+  </head>
+
+  <body>
+    <header>
+      <a href="../../">home</a>
+      <h1>PHP Infomation</h1>
+    </header>
+
+    <main>
+        <?php
+        phpinfo();
+        ?>
+    </main>
+  </body>
+
+</html>
