cf46a39b575679ab6a7356debeca1a4bb7006943
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2022-07-31 02:26:51 +0900

    Added php image function sample

---
+++ lang/javascript/webxam/public/php/image/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>画像処理および作成</title>
+  <link rel="stylesheet" href="../../common.css" />
+</head>
+
+<body>
+  <header>
+    <a href="../../">home</a>
+    <h1>画像処理および作成</h1>
+  </header>
+  <main>
+    <section>
+      <h2>サポートしている画像フォーマット一覧</h2>
+      <?php 
+      echo '<ul>';
+      foreach (gd_info() as $key => $value) {
+        echo "<li>$key : ", is_bool($value) ? ($value ? 'サポート' : '未サポート') : $value, "</li>";
+      }
+      echo '</ul>';
+      ?>
+    </section>
+  </main>
+  <footer>
+    <h3>参考</h3>
+    <ul>
+      <li><a href="https://www.php.net/manual/ja/refs.utilspec.image.php">PHPマニュアル 画像処理および作成</a></li>
+      <li><a href="https://www.php.net/manual/ja/ref.image.php">GD および Image 関数</a></li>
+    </ul>
+  </footer>
+</body>
+
+</html>
