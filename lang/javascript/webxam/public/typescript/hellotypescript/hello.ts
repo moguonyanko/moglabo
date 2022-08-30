8b1b9791a3e078585d046dd5c5400f5299a78b21
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2022-08-30 17:02:33 +0900

    Added typescript debug config

---
+++ lang/javascript/webxam/public/typescript/hellotypescript/hello.ts
@@ -0,0 +1,8 @@
+/**
+ * @fileoverview 動作確認用TypeScript
+ */
+
+const message: string = 'コンパイルされたTypeScript';
+
+const container: any = document.getElementById('hello-container');
+container.appendChild(document.createTextNode(message));
