610993fd13ec67189db99e16dd8e1988634d668f
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2022-09-03 02:59:52 +0900

    Modified tsconfig

--- lang/javascript/webxam/public/typescript/hellotypescript/hello.ts
+++ lang/javascript/webxam/public/typescript/hellotypescript/hello.ts
@@ -2,7 +2,7 @@
  * @fileoverview 動作確認用TypeScript
  */
 
-const message: string = 'コンパイルされたTypeScript';
+const message: string = 'TypeScript動作確認';
 
 const container: any = document.getElementById('hello-container');
 container.appendChild(document.createTextNode(message));
