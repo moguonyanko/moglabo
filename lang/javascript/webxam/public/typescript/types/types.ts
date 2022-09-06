bc664e66a2398f7f20ca38436ee9d32746411183
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2022-09-07 02:56:16 +0900

    Added type alias sample

--- lang/javascript/webxam/public/typescript/types/types.ts
+++ lang/javascript/webxam/public/typescript/types/types.ts
@@ -7,11 +7,24 @@ const getLength = (value: any[] | string): number => {
   return value.length;  
 };
 
+type Member = {
+  name: string;
+  age: number;
+}
+
+const getMemberInfo = (member: Member): string => {
+  return `My name is ${member.name}, I am ${member.age} years old.`;
+};
+
 const initFuncs = {
   uniontypes: (): void => {
     const output = document.querySelector('.example.uniontypes .output');
     output.textContent += `「Hello」の長さは${getLength('Hello')},`;
     output.textContent += `[1,2,3,4,5]のサイズは${getLength([1,2,3,4,5])}`;
+  },
+  typealias: (): void => {
+    const output = document.querySelector('.example.typealias .output');
+    output.textContent += getMemberInfo({ name: 'Masao', age: 29 });
   }
 };
 
