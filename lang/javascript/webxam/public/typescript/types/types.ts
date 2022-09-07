35c196240ac0bc294500293c45387a04d4e01e33
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2022-09-08 03:33:10 +0900

    Added interface example

--- lang/javascript/webxam/public/typescript/types/types.ts
+++ lang/javascript/webxam/public/typescript/types/types.ts
@@ -16,15 +16,28 @@ const getMemberInfo = (member: Member): string => {
   return `My name is ${member.name}, I am ${member.age} years old.`;
 };
 
+interface Car {
+  speed: number;
+}
+
+const getCarSpeed = (car: Car, boost: 2 | 3 | 4): number => {
+  return car.speed * boost;
+};
+
 const initFuncs = {
   uniontypes: (): void => {
-    const output = document.querySelector('.example.uniontypes .output');
-    output.textContent += `「Hello」の長さは${getLength('Hello')},`;
+    const message: string = 'Hello' as 'Hello';
+    const output: HTMLElement = document.querySelector('.example.uniontypes .output');
+    output.textContent += `「${message}」の長さは${getLength(message)},`;
     output.textContent += `[1,2,3,4,5]のサイズは${getLength([1,2,3,4,5])}`;
   },
-  typealias: (): void => {
-    const output = document.querySelector('.example.typealias .output');
+  typealiases: (): void => {
+    const output: HTMLElement = document.querySelector('.example.typealiases .output');
     output.textContent += getMemberInfo({ name: 'Masao', age: 29 });
+  },
+  interfaces: (): void => {
+    const output: HTMLElement = document.querySelector('.example.interfaces .output');
+    output.textContent += getCarSpeed({ speed: 100 }, 3);
   }
 };
 
