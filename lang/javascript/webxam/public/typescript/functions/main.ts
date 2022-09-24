23eddf45398e695ddd4b43bc86548c880a71c668
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2022-09-25 02:55:14 +0900

    Added generic function example

--- lang/javascript/webxam/public/typescript/functions/main.ts
+++ lang/javascript/webxam/public/typescript/functions/main.ts
@@ -6,11 +6,24 @@ type Greeting = {
   (greet: string): string 
 };
 
+const countAll = <Type extends { length: number }>(target: Type[]): number => {
+  return target.map((t: Type) => t.length).reduce((a: number, b: number) => a + b, 0);
+};
+
 const funcs = {
   callsignatures: (): void => {
     const output: HTMLElement = document.querySelector('.example.callsignatures .output') as HTMLElement;
     const greeting: Greeting = (greet: string) => `${greet}!!!`;
     output.textContent += greeting('こんにちは');
+  },
+  genericfunctions: (): void => {
+    const output: HTMLElement = document.querySelector('.example.genericfunctions .output') as HTMLElement;
+    const sample1 = [[1, 2, 3, 4, 5], 'Hello', ['A', 'B', 'C']];
+    sample1.forEach(s => {
+      output.innerHTML += `${s.toString()}<br />`;
+    });
+    const result = countAll(sample1);
+    output.innerHTML += result;
   }
 };
 
