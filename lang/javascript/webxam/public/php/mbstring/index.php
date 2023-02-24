8127af9d554297d9734e38d150c81e1579230480
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2023-02-24 22:41:15 +0900

    Added mbstring_convert_variables example

--- lang/javascript/webxam/public/php/mbstring/index.php
+++ lang/javascript/webxam/public/php/mbstring/index.php
@@ -29,6 +29,26 @@ declare(strict_types=1);
       ?>
       </div>
     </section>
+    <section>
+      <h2>複数の文字列のエンコーディング変更</h2>
+      <p>BASE64をmbstringで使うのは非推奨とされている。</p>
+      <?php 
+      $w1 = 'こんにちは';
+      $w2 = '𩸽を𠮟る𠮷野家';
+      echo '<p>'.$w1.','.$w2.'</p>';
+      $internal_enc = mb_internal_encoding();
+      echo '<p>内部エンコーディング='.$internal_enc.'</p>';
+      $output_enc = 'SJIS-win';
+      echo '<p>出力エンコーディング='.$output_enc.'</p>';
+      $input_enc = mb_convert_variables($output_enc, $internal_enc, $w1, $w2);
+      echo '<p>'.$input_enc.'から'.$output_enc.'に変更しました。</p>';
+      echo '<p>'.$w1.','.$w2.'</p>';
+      $input_enc = mb_convert_variables($internal_enc, $output_enc, $w1, $w2);
+      echo '<p>'.$input_enc.'から'.$internal_enc.'に戻しました。</p>';
+      echo '<p>'.$w1.','.$w2.'</p>';
+      echo '<p>サロゲートペア文字を含む文字列をサロゲートペア文字を扱えないエンコーディングに変換した場合は元に戻らない。</p>'
+      ?>
+    </section>
   </main>
 
   <footer>
