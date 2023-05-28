edbe58d42112ca11defe1c8d0eceb8b944a27ba2
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2023-05-29 00:49:10 +0900

    地ならしのテンプレート追加

---
+++ lang/javascript/kyopro/past201912/j/index.js
@@ -0,0 +1,17 @@
+/**
+ * @fileoverview 
+ * 問題
+ * https://atcoder.jp/contests/past201912-open/tasks/past201912_j
+ * 解説
+ * https://blog.hamayanhamayan.com/entry/2019/12/31/235948
+ */
+
+import KyoPro from '../../kyopro.js';
+
+class Runner {
+  run(args) {
+    
+  }
+}
+
+new KyoPro(new Runner()).start();
