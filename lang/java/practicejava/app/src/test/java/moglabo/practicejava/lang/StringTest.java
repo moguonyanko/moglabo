c039ba44cee4f8aeb3f91d67ea803f3137edc3eb
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2023-06-20 14:44:48 +0900

    Added string indent example

--- lang/java/practicejava/app/src/test/java/moglabo/practicejava/lang/StringTest.java
+++ lang/java/practicejava/app/src/test/java/moglabo/practicejava/lang/StringTest.java
@@ -19,4 +19,24 @@ public class StringTest {
         assertTrue(actual.equals("XY"));
     }
     
+    /**
+     * 参考:
+     * https://blogs.oracle.com/javamagazine/post/java-text-block-string-line-separator-hashcode
+     */
+    @Test
+    void 文章をindentで整形できる() {
+        var s = """
+                こんにちは
+                こんばんわ
+                𩸽を𠮟る𠮷野家と髙﨑
+                """;
+        
+        s = s.indent(4);
+        System.out.println(s);
+        
+        var s2 = "Hello" + System.lineSeparator() + "World" + System.lineSeparator();
+        s2 = s2.indent(4);
+        System.out.println(s2);
+    }
+    
 }
