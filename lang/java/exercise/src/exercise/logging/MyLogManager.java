3d9a06c99796edae4757ff93e51c21d7c38d9252
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2023-06-26 00:40:44 +0900

    Remove import

--- lang/java/exercise/src/exercise/logging/MyLogManager.java
+++ lang/java/exercise/src/exercise/logging/MyLogManager.java
@@ -1,7 +1,5 @@
 package exercise.logging;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
--- lang/java/exercise/src/test/exercise/lang/TestString.java
+++ lang/java/exercise/src/test/exercise/lang/TestString.java
@@ -4,30 +4,31 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestString {
-    
+
     /**
      * 参考:
      * https://blogs.oracle.com/javamagazine/post/java-quiz-local-instance-variables
      */
     private static class Command {
+
         private final String url;
-        
-        private Command (String url) {
+
+        private Command(String url) {
             this.url = url;
         }
-        
+
         private String execute() {
             // 事故を避けるならローカル変数でフィールドを隠蔽するのは避けるべき。
             var tmp = url.substring(0, url.lastIndexOf("/"));
             return tmp.substring(0, tmp.lastIndexOf("/"));
         }
     }
-    
+
     @Test
     public void substringで文字列を切り出せる() {
         var url = "https://myhost/webpage/index.html";
         var result = new Command(url).execute();
         assertTrue("https://myhost".equals(result));
     }
-    
+
 }
