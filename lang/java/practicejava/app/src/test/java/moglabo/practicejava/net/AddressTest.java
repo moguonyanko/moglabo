8e3a00a4fff348004a53f0a86e2abae5f86087a4
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2023-07-19 00:47:12 +0900

    Added reuse address method example

--- lang/java/practicejava/app/src/test/java/moglabo/practicejava/net/AddressTest.java
+++ lang/java/practicejava/app/src/test/java/moglabo/practicejava/net/AddressTest.java
@@ -1,15 +1,44 @@
 package moglabo.practicejava.net;
 
+import java.io.IOException;
 import java.net.InetAddress;
+import java.net.ServerSocket;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AddressTest {
-    
+
     @Test
     void ループバックアドレスを判別できる() throws Exception {
-        var adr = InetAddress.getLocalHost();
+        // localhostはループバックアドレスとは限らない。
+        var adr = InetAddress.getLoopbackAddress();
         assertTrue(adr.isLoopbackAddress());
     }
-    
+
+    @Test
+    void ローカルアドレスを再利用できる() throws IOException {
+        var port = 38888;
+        try (var server1 = new ServerSocket(port); 
+             // portが衝突するとBindExceptionが発生するので1足す。
+             var server2 = new ServerSocket(port + 1)) {
+            // 戻り値がtrueかどうかはOSに依存する。macOSではtrueになる。
+            // ServerSocketコンストラクタに渡したポート番号が他のServerSocketで使われて
+            // いるとserver.getReuseAddress()はfalseを返すことがある。
+            var reusable1 = server1.getReuseAddress();
+            assertTrue(reusable1);
+            var reusable2 = server2.getReuseAddress();
+            assertTrue(reusable2);
+        }
+    }
+
+    @Test
+    void 受信バッファサイズを確認できる() throws IOException {
+        try (var server = new ServerSocket(38888)) {
+            var size = server.getReceiveBufferSize();
+            System.out.println(size);
+            assertNotNull(size);
+        }
+    }
+
 }
