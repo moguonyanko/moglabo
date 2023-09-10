a969b525023a123ae276523384d59613e483f036
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2023-09-11 02:24:04 +0900

    Added JDK21 new feature example

--- lang/java/practicejava/app/src/test/java/moglabo/practicejava/lang/SyntaxTest.java
+++ lang/java/practicejava/app/src/test/java/moglabo/practicejava/lang/SyntaxTest.java
@@ -55,4 +55,27 @@ public class SyntaxTest {
         }
     }
     
+//    private record Student(String name, int age, int score) {
+//        
+//    }
+    
+    // JDK21まではプレビュー機能
+//    private boolean isMatchRecordStudent(Object o) {
+//        return o instanceof Student(String name, int age, int score);
+//    }
+ 
+    // switchのパターンマッチはJDK21までプレビュー機能
+//    @Test
+//    void whenでマッチできる() {
+//        var i = 1;
+//        
+//        var result = switch (i) {
+//            case 0 -> "ZERO";
+//            case Integer n when n > 0 -> "PLUS";
+//            case Integer n when n < 0 -> "MINUS";
+//            default -> "Undefined";
+//        };
+//        
+//        assertEquals("PLUS", result);
+//    }
 }
