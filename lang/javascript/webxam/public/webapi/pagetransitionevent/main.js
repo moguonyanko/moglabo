e4500a1a125af535de1476093f31b3e0b1d503a6
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2023-09-19 20:20:25 +0900

    Added bfcache enable function

--- lang/javascript/webxam/public/webapi/pagetransitionevent/main.js
+++ lang/javascript/webxam/public/webapi/pagetransitionevent/main.js
@@ -11,8 +11,12 @@ const funcs = {
       localStorage.setItem('hidecount', current + 1)
     }
   },
-  pageshow: () => {
+  pageshow: event => {
     const output = document.querySelector('.output')
+    if (event.persisted) {
+      output.textContent = 'bfcache使用不可能'
+      return
+    }
     const value = localStorage.getItem('hidecount')
     if (!isNaN(parseInt(value))) {
       // +=にすると追記されてしまう。bfcacheは各要素への入力内容もキャッシュから取得して再現するため。
@@ -21,9 +25,29 @@ const funcs = {
   }
 };
 
-Object.keys(funcs).forEach(key => {
-  window.addEventListener(key, funcs[key])
-})
+const noop = () => {};
 
 // unloadイベントのイベントリスナーを登録するとbfcacheが無効化される。
-//window.addEventListener('unload', () => {})
+const enableBfCache = enable => {
+  if (enable) {
+    window.removeEventListener('unload', noop)
+  } else {
+    window.addEventListener('unload', noop)
+  }
+};
+
+const addListener = () => {
+  Object.keys(funcs).forEach(key => {
+    window.addEventListener(key, funcs[key])
+  })
+  
+  document.querySelector('main').addEventListener('click', event => {
+    const { eventTarget } = event.target.dataset
+    if (eventTarget === 'enablebfcache') {
+      event.stopPropagation()
+      enableBfCache(event.target.checked)
+    }
+  });
+}
+
+addListener()
