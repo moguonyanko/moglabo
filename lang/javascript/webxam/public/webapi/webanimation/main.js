d4e0f04ff619d6f6875b026d9f2a561a8ef56e69
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2023-11-22 01:03:35 +0900

    Added ScrollTimeline example

--- lang/javascript/webxam/public/webapi/webanimation/main.js
+++ lang/javascript/webxam/public/webapi/webanimation/main.js
@@ -27,8 +27,32 @@ const initViewTimeline = () => {
   output.innerHTML += `end offset: ${timeline.endOffset}<br />`
 }
 
+const initScrollTimeline = () => {
+  const box = document.querySelector('.box')
+
+  // TODO: コンテナ内でboxを回転させるようにしたい。
+  const timeline = new ScrollTimeline({
+    source: document.documentElement,
+    // source: document.querySelector('.scrollTimeline .content'),
+    axis: 'block' 
+  })
+
+  box.animate(  {
+    rotate: ["0deg", "720deg"],
+    left: ["0%", "100%"],
+  },
+  {
+    timeline,
+  })
+
+  const output = document.querySelector('.scrollTimeline .output')
+  output.innerHTML += `Timeline source element: ${timeline.source.nodeName}<br />`
+  output.innerHTML += `Timeline scroll axis: ${timeline.axis}<br />`
+}
+
 const init = () => {
   initViewTimeline()
+  initScrollTimeline()
 }
 
 init()
