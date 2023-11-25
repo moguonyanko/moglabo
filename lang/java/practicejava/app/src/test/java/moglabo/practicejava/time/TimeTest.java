7030428e721054cc96b3db747c72c9908a7c75cc
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2023-11-26 01:39:16 +0900

    Added SimpleDateFormat example

--- lang/java/practicejava/app/src/test/java/moglabo/practicejava/time/TimeTest.java
+++ lang/java/practicejava/app/src/test/java/moglabo/practicejava/time/TimeTest.java
@@ -1,12 +1,30 @@
 package moglabo.practicejava.time;
 
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoField;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TimeTest {
+    
+    private static final String SAMPLE_PATTERN = "yyyyMMddHHmmss";
+    
+    private static final Timestamp SAMPLE_TIMESTAMP = 
+            Timestamp.valueOf("2023-11-24 15:15:55.12345");
+    private static final String SAMPLE_TIMESTAMP_STRING = "20231124151555";
+    
+    @Test
+    void 文字列からSimpleDateFormatを生成できる() {
+        var formatter = new SimpleDateFormat(SAMPLE_PATTERN);
+        var result = formatter.format(SAMPLE_TIMESTAMP);
+        System.out.println(result);
+        assertEquals(SAMPLE_TIMESTAMP_STRING, result);
+    }
     
     /**
      * 参考:
@@ -18,12 +36,12 @@ public class TimeTest {
      */
     @Test
     void 文字列からDateTimeFormatterを生成できる() {
-        var pattern = "yyyyMMddHHmmss";
-        var formatter = DateTimeFormatter.ofPattern(pattern);
+        var formatter = DateTimeFormatter.ofPattern(SAMPLE_PATTERN);
         // formatにTimestampは直接渡せない。
-        var ts = Timestamp.valueOf("2023-11-24 15:15:55.12345");
-        var result = formatter.format(ts.toInstant());
+        var yearOfEra = SAMPLE_TIMESTAMP.toInstant().get(ChronoField.YEAR_OF_ERA);
+        var result = formatter.format(yearOfEra);
         System.out.println(result);
+        assertEquals(SAMPLE_TIMESTAMP_STRING, result);
     }
     
 }
