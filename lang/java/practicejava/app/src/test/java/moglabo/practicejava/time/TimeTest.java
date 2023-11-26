516429ef720a49f144214b7c0cf567539cc51c4b
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2023-11-26 13:22:30 +0900

    Modified DateTimeFormatter example

--- lang/java/practicejava/app/src/test/java/moglabo/practicejava/time/TimeTest.java
+++ lang/java/practicejava/app/src/test/java/moglabo/practicejava/time/TimeTest.java
@@ -2,25 +2,28 @@ package moglabo.practicejava.time;
 
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoField;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TimeTest {
     
-    private static final String SAMPLE_PATTERN = "yyyyMMddHHmmss";
+    private static final String SAMPLE_DATETIME_PATTERN = "yyyyMMddHHmmss";
     
+    /**
+     * データベースの日時系のカラムから得た値を想定してTimestampを使っている。
+     */
     private static final Timestamp SAMPLE_TIMESTAMP = 
             Timestamp.valueOf("2023-11-24 15:15:55.12345");
+    /**
+     * SAMPLE_TIMESTAMPから期待される文字列表現
+     */
     private static final String SAMPLE_TIMESTAMP_STRING = "20231124151555";
     
     @Test
     void 文字列からSimpleDateFormatを生成できる() {
-        var formatter = new SimpleDateFormat(SAMPLE_PATTERN);
+        var formatter = new SimpleDateFormat(SAMPLE_DATETIME_PATTERN);
         var result = formatter.format(SAMPLE_TIMESTAMP);
         System.out.println(result);
         assertEquals(SAMPLE_TIMESTAMP_STRING, result);
@@ -31,15 +34,16 @@ public class TimeTest {
      * https://www.baeldung.com/java-datetimeformatter
      * https://www.baeldung.com/java-time-instant-to-java-sql-timestamp
      * 
-     * TODO:
-     * 実行時例外となる。指定しているパターンがDateTimeFormatterでは無効なのだろうか？
      */
     @Test
     void 文字列からDateTimeFormatterを生成できる() {
-        var formatter = DateTimeFormatter.ofPattern(SAMPLE_PATTERN);
-        // formatにTimestampは直接渡せない。
-        var yearOfEra = SAMPLE_TIMESTAMP.toInstant().get(ChronoField.YEAR_OF_ERA);
-        var result = formatter.format(yearOfEra);
+        var formatter = DateTimeFormatter.ofPattern(SAMPLE_DATETIME_PATTERN);
+        /**
+         * formatにTimestampは直接渡せない。またTimestamp.toInstant()の戻り値を渡すと
+         * 「UnsupportedTemporalTypeException: Unsupported field: YearOfEra」となる。
+         */
+        var localDateTime = SAMPLE_TIMESTAMP.toLocalDateTime();
+        var result = formatter.format(localDateTime);
         System.out.println(result);
         assertEquals(SAMPLE_TIMESTAMP_STRING, result);
     }
