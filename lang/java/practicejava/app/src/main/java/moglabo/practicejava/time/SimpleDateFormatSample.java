23b81ce0b1e977d430a5981cbb7c85f464ff8f5e
Kouichi Yamada <moguo_50000_nyanko@yahoo.co.jp>
2023-11-29 01:44:47 +0900

    Added SimpleDateFormat with multi-threading example

---
+++ lang/java/practicejava/app/src/main/java/moglabo/practicejava/time/SimpleDateFormatSample.java
@@ -0,0 +1,73 @@
+package moglabo.practicejava.time;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+
+public class SimpleDateFormatSample {
+
+    private static final String SAMPLE_DATETIME_PATTERN = "yyyyMMddHHmmss";
+
+    private static final SimpleDateFormat SIMPLE_DATE_FORMATTER
+            = new SimpleDateFormat(SAMPLE_DATETIME_PATTERN);
+
+    private static class FormatTask implements Callable<String> {
+
+        private final Timestamp timestamp;
+
+        public FormatTask(Timestamp timestamp) {
+            this.timestamp = timestamp;
+        }
+
+        /**
+         * @todo
+         * SimpleDateFormat.format()時にSimpleDateFormatがスレッドセーフでないことに
+         * 起因する例外が発生するはずだが発生しない。
+         */
+        @Override
+        public String call() {
+            var result = SIMPLE_DATE_FORMATTER.format(timestamp);
+            return result;
+        }
+
+    }
+
+    /**
+     * 参考
+     * https://mkyong.com/java/how-to-get-current-timestamps-in-java/
+     */
+    private static Timestamp getSampleTimestamp(int plusDay) {
+        var instant = LocalDateTime.now().plusDays(plusDay).toInstant(ZoneOffset.UTC);
+        var timestamp = Timestamp.from(instant);
+        return timestamp;
+    }
+
+    private static List<String> executeFormatting(int threadSize) throws InterruptedException {
+        var executor = Executors.newVirtualThreadPerTaskExecutor();
+        List<FormatTask> tasks = new ArrayList<>();
+        for (var i = 0; i < threadSize; i++) {
+            tasks.add(new FormatTask(getSampleTimestamp(i)));
+        }
+        var futures = executor.invokeAll(tasks);
+        return futures.stream().map(f -> {
+            try {
+                return f.get();
+            } catch (InterruptedException | ExecutionException ex) {
+                throw new IllegalStateException(ex);
+            }
+        }).collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        var result = executeFormatting(10000);
+        System.out.println(result.size());
+    }
+
+}
