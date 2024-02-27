83079a45624ecfd14b4cf1da447250404b91e00d
moguonyanko <moguo_50000_nyanko@yahoo.co.jp>
2024-02-27 17:49:44 +0900

    Add leaflet watermark example

--- lang/javascript/webxam/public/leaflet/gridlayer/main.js
+++ lang/javascript/webxam/public/leaflet/gridlayer/main.js
@@ -4,24 +4,24 @@
 /* eslint-disable no-undef */
 
 const init = () => {
-	class CanvasCircles extends L.GridLayer {
-		createTile(coords) {
-			const tile = document.createElement('canvas')
+  class CanvasCircles extends L.GridLayer {
+    createTile(coords) {
+      const tile = document.createElement('canvas')
 
-			const tileSize = this.getTileSize()
-			tile.setAttribute('width', tileSize.x)
-			tile.setAttribute('height', tileSize.y)
+      const tileSize = this.getTileSize()
+      tile.setAttribute('width', tileSize.x)
+      tile.setAttribute('height', tileSize.y)
 
-			const ctx = tile.getContext('2d')
+      const ctx = tile.getContext('2d')
 
       ctx.fillStyle = 'red'
-			ctx.arc(tileSize.x / 2, tileSize.x / 2, 4 + coords.z * 4, 0, 2 * Math.PI, false)
-			ctx.fill()
+      ctx.arc(tileSize.x / 2, tileSize.x / 2, 4 + coords.z * 4, 0, 2 * Math.PI, false)
+      ctx.fill()
 
       // createTileがdoneを引数に取っていなければタイルを返すだけでタイルは表示される。
-			return tile
-		}
-	}
+      return tile
+    }
+  }
 
   class MyGridLayer extends L.GridLayer {
     createTile(coords, done) {
@@ -39,11 +39,29 @@ const init = () => {
     }
   }
 
+  class HelloWatermark extends L.Control {
+    constructor(options) {
+      super(options)
+    }
+
+    onAdd(map) {
+      const img = L.DomUtil.create('img')
+      img.src = '../../image/hello.png'
+      img.style.height = '3em'
+      img.style.width = '10em'
+      return img
+    }
+
+    onRemove(map) {
+      console.log('Removed watermark')
+    }
+  }
+
   const osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
   })
-  
+
   const myGridLayer = new MyGridLayer(),
     myCanvasLayer = new CanvasCircles()
 
@@ -64,6 +82,9 @@ const init = () => {
   // map.addLayer(myGridLayer)
   // map.addLayer(myCanvasLayer)
   map.setView({ lat: 35.656159, lng: 139.7552989 }, 8)
+
+  const watermarkCtrl = new HelloWatermark({ position: 'bottomleft' }).addTo(map)
+  console.log(watermarkCtrl)
 }
 
 init()
