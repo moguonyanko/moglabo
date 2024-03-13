39b712e984a7cc1b6fd52f17651e8d548e2be16c
moguonyanko <moguo_50000_nyanko@yahoo.co.jp>
2024-03-13 10:50:22 +0900

    凸包サンプルのリファクタリング

--- lang/javascript/webxam/public/leaflet/convexhull/main.js
+++ lang/javascript/webxam/public/leaflet/convexhull/main.js
@@ -1,53 +1,44 @@
 /**
  * @fileoverview 凸包計算用スクリプト
+ * Windowオブジェクトに公開されてしまっている関数や変数が多いがサンプルなので放置する。
+ * もし気になるならscript要素のtype属性にmoduleを指定すればいい。
  */
 
-const main = async () => {
-  const osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  })
+let convexfullLayer = null
 
-  const map = L.map('map', {
-    renderer: L.canvas(),
-    layers: [osm]
-  })
-
-  const loadGeoJson = async path => await (await fetch(path)).json()
-  const multipoint = await loadGeoJson('multipoint.json')
-
-  let convexfullLayer = null
-
-  const clearConvexhull = () => {
-    if (convexfullLayer) {
-      convexfullLayer.remove()
-    }
+const clearConvexhull = () => {
+  if (convexfullLayer) {
+    convexfullLayer.remove()
   }
+}
 
-  const funcs = {
-    drawConvexhull: async () => {
-      clearConvexhull()
-      const response = await fetch('/brest/gis/convexhull/', {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(multipoint)
-      })
-      const { result } = await response.json()
-      convexfullLayer = L.geoJSON([result]).addTo(map)
-    },
-    clearConvexhull
-  }
+const listeners = {
+  drawConvexhull: async (map, multipoint) => {
+    clearConvexhull()
+    const response = await fetch('/brest/gis/convexhull/', {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(multipoint)
+    })
+    const { result } = await response.json()
+    convexfullLayer = L.geoJSON([result]).addTo(map)
+  },
+  clearConvexhull
+}
 
+const addListener = (map, multipoint) => {
   document.querySelector('main').addEventListener('click', async event => {
-    const { eventTarget } = event.target.dataset
-    if (typeof funcs[eventTarget] === 'function') {
-      await funcs[eventTarget]()
+    const { eventName } = event.target.dataset
+    if (typeof listeners[eventName] === 'function') {
+      await listeners[eventName](map, multipoint)
     }
   })
+}
 
+const drawMarker = (map, multipoint) => {
   // CircleMarkerでGeoJSONのPointをCanvasに描画するときのスタイル。
   // 通常のMarkerだとimg要素で追加されるため数が多い場合にパフォーマンスが低下する。
   const circleMarkerStyle = {
@@ -65,16 +56,36 @@ const main = async () => {
       return L.circleMarker(latlng, circleMarkerStyle);
     }
   }).addTo(map)
+}
+
+const initMap = () => {
+  const osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  })
+
+  const map = L.map('map', {
+    renderer: L.canvas(),
+    layers: [osm]
+  })
 
   map.setView({ lat: 35.65910479239084, lng: 139.7561359405518 }, 15)
 
-  const viewClickLatLng = evt => {
-    const { lat, lng } = evt.latlng
+  const viewClickLatLng = event => {
+    const { lat, lng } = event.latlng
     const info = `lat: ${lat}, lng: ${lng}`
     console.log(info)
   }
+  
+  return map.on('click', viewClickLatLng)
+}
 
-  map.on('click', viewClickLatLng)
+const main = async () => {
+  const map = initMap()
+  const loadGeoJson = async path => await (await fetch(path)).json()
+  const multipoint = await loadGeoJson('multipoint.json')
+  addListener(map, multipoint)
+  drawMarker(map, multipoint)
 }
 
 main().then()
