ddc8327683ca42de2f601e414cb3ec60dcffc610
moguonyanko <moguo_50000_nyanko@yahoo.co.jp>
2024-03-17 14:00:27 +0900

    backdrop疑似要素のサンプル追加

---
+++ lang/javascript/webxam/public/css/backdrop/main.js
@@ -0,0 +1,20 @@
+/**
+ * @fileoverview ::backdrop疑似要素の使い方を調べるためのスクリプト
+ */
+
+const listeners = {
+  showDialog: () => {
+    const dialog = document.getElementById('simple-dialog')
+    dialog.showModal()
+  }
+}
+
+const init = () => {
+  const main = document.querySelector('main')
+  main.addEventListener('click', event => {
+    const { eventName } = event.target.dataset
+    listeners[eventName]?.()
+  })
+}
+
+init()
