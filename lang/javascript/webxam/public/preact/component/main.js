5130a2aa46f7b04833166efa4b932bfe30b187d3
moguonyanko <moguo_50000_nyanko@yahoo.co.jp>
2024-04-29 16:29:41 +0900

    状態変化を伴うコンポーネントのサンプル追加

--- lang/javascript/webxam/public/preact/component/main.js
+++ lang/javascript/webxam/public/preact/component/main.js
@@ -3,7 +3,7 @@
  */
 import { h, render, Component } from 'https://esm.sh/preact'
 import htm from 'https://esm.sh/htm'
-import { Header } from '../comcom.js'
+import { Header as MyHeader } from '../comcom.js'
 
 const html = htm.bind(h)
 
@@ -12,21 +12,27 @@ class Greeting extends Component {
     super()
     this.state = {
       value: props.value,
-      name: props.name  
+      name: props.name
     }
   }
 
+  // arrow-functonで定義しないとsetStateを参照できない。
   onInput = event => {
+    // 空文字の場合も保存しないと最後に入力された文字が表示される。
     this.setState({
       value: event.currentTarget.value
-    })    
+    })
   }
 
+  // arrow-functonで定義しないとsetStateを参照できない。
   onSubmit = event => {
     event.preventDefault()
+    if (!this.state.value) { // 空文字が保存されていたら何もしない。
+      return
+    }
     this.setState({
       name: this.state.value // getState()は存在しない。
-    })   
+    })
   }
 
   render() {
@@ -42,10 +48,39 @@ class Greeting extends Component {
   }
 }
 
+class MyClock extends Component {
+  constructor() {
+    super()
+    this.updateTime() // 時刻初期化
+  }
+
+  updateTime() {
+    this.setState({ time: Date.now() })
+  }
+
+  componentDidMount() {
+    this.intervalId = setInterval(() => this.updateTime(), 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId)
+  }
+
+  render() {
+    const timeState = new Date(this.state.time)
+    return html`<div>${timeState.toLocaleString()}</div>`
+  }
+}
+
 const init = () => {
-  render(html`<${Header} title="コンポーネント" homepath="../../" />`,
+  render(html`<${MyHeader} title="コンポーネント" homepath="../../" />`,
     document.body)
-  render(html`<${Greeting} name="NO NAME" value="𩸽太郎" />`, document.querySelector('main'))
+
+  const mainEle = document.querySelector('main')
+  render(html`<${Greeting} name="NO NAME" value="𩸽太郎" /><${MyClock} />`,
+    mainEle)
+  // 別にrenderすると他の要素を上書きしてしまうのでまとめてrenderする必要がある。
+  // render(html`<${Clock} />`, mainEle)
 }
 
 init()
