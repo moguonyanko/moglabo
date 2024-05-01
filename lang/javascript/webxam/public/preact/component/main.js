3af6eada7c9313d9fc742b7f6b42531ea10ef4bd
moguonyanko <moguo_50000_nyanko@yahoo.co.jp>
2024-05-01 17:53:30 +0900

    Preactコンポーネントのサンプル追加

--- lang/javascript/webxam/public/preact/component/main.js
+++ lang/javascript/webxam/public/preact/component/main.js
@@ -1,7 +1,7 @@
 /**
  * @fileoverview Preactのコンポーネント調査用スクリプト
  */
-import { h, render, Component } from 'https://esm.sh/preact'
+import { h, render, Component, Fragment } from 'https://esm.sh/preact'
 import htm from 'https://esm.sh/htm'
 import { Header as MyHeader } from '../comcom.js'
 
@@ -36,15 +36,15 @@ class Greeting extends Component {
   }
 
   render() {
-    return html`
+    // この例ではあまり意味がないがFragmentを使うと要素群に対して単一のルート要素を持たせやすくなる。
+    return html`<${Fragment}>
       <div>
         <h1>こんにちは、${this.state.name}</h1>
         <form onSubmit=${this.onSubmit}>
           <input type="text" value=${this.state.value} onInput=${this.onInput} />
           <button type="submit">更新</button>
         </form>
-      </div>
-    `
+      </div></${Fragment}>`
   }
 }
 
@@ -67,20 +67,70 @@ class MyClock extends Component {
   }
 
   render() {
-    const timeState = new Date(this.state.time)
-    return html`<div>${timeState.toLocaleString()}</div>`
+    if (!this.state.errored) {
+      const timeState = new Date(this.state.time)
+      return html`<div>${timeState.toLocaleString()}</div>`
+    } else {
+      return html`<div>${this.state.message}</div>`
+    }
   }
 }
+
+class BrokenMyClock extends MyClock {
+  constructor() {
+    super()
+    setTimeout(() => { throw new Error('Broken Clock!') }, 3000)
+  }
+
+  // TODO: イベントが発生しない。仮想DOMではinput要素しかイベント発生源になり得ないのか？
+  // onPointerUp() {
+  //   throw new Error('Broken Clock!')
+  // }
+
+  // TODO: 例外が発生しても呼び出されない。
+  componentDidCatch(error) {
+    this.setState({ errored: true, message: error.message })
+  }
+}
+
+function MyList(props) {
+  const content = JSON.parse(props.items).map(item =>
+    html`<${Fragment} key=${item.id}>
+    <dt>${item.name}</dt>
+    <dd>${item.price}</dd>
+  </${Fragment}>`)
+  return html`<div><dl>${content}</dl></div>`
+}
+
+const sampleItems = [
+  {
+    name: 'Apple', price: 200
+  },
+  {
+    name: 'Orange', price: 230
+  },
+  {
+    name: 'Melon', price: 1000
+  },
+]
 
 const init = () => {
   render(html`<${MyHeader} title="コンポーネント" homepath="../../" />`,
     document.body)
 
-  const mainEle = document.querySelector('main')
-  render(html`<${Greeting} name="NO NAME" value="𩸽太郎" /><${MyClock} />`,
-    mainEle)
-  // 別にrenderすると他の要素を上書きしてしまうのでまとめてrenderする必要がある。
-  // render(html`<${Clock} />`, mainEle)
+  // 複数のコンポーネントを同じ要素に対して個別にrenderすると他のコンポーネントを上書きしてしまうので
+  // その場合は全ての要素を一度にrenderする必要がある。
+  const container = document.querySelector('.container')
+  render(html`<${Greeting} name="NO NAME" value="𩸽太郎" />
+    <p>時計</p>
+    <${MyClock} />
+    <p>エラーが発生する時計</p>
+    <${BrokenMyClock} />
+    <p>リスト</p>
+    <${MyList} items="${JSON.stringify(sampleItems)}" />`, container) // コンポーネントにclassを指定しても除去される。
+  // なぜか初期化エラーになる。renderを複数回呼び出すのは好ましくないのかもしれない。
+  // render(html`<${MyClock} />`, document.getElementById('myclock'))
+  // render(html`<${BrokenMyClock} />`, document.getElementById('brokenmyclock'))
 }
 
 init()
