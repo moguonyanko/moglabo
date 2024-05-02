66ac6c0af8ee77adce0f587565917dddd08a0c67
moguonyanko <moguo_50000_nyanko@yahoo.co.jp>
2024-05-02 11:10:53 +0900

    useReducerのサンプル追加

--- lang/javascript/webxam/public/preact/hook/main.js
+++ lang/javascript/webxam/public/preact/hook/main.js
@@ -4,7 +4,7 @@
  * https://preactjs.com/guide/v10/hooks/
  */
 import { render, Component, Fragment } from 'https://esm.sh/preact'
-import { useState, useCallback } from 'https://esm.sh/preact/hooks'
+import { useState, useCallback, useReducer } from 'https://esm.sh/preact/hooks'
 import { html } from '../comcom.js'
 
 const useCounter = () => {
@@ -33,9 +33,37 @@ const MyCounterEn = () => {
   <button onClick=${decrement}>Decrement</button>`
 }
 
+const counterActions = {
+  increment: count => count + 1,
+  decrement: count => count - 1,
+  reset: () => 0
+}
+
+const myReducer = (count, actionName) => {
+  if (actionName in counterActions) {
+    return counterActions[actionName](count)
+  } else {
+    throw new Error(`${actionName} is unsupported`)
+  }
+}
+
+const MyReducerCounter = () => {
+  const initialCount = 0
+  const [count, dispatch] = useReducer(myReducer, initialCount)
+
+  return html`<${Fragment}>
+  <span>${count}</span>
+  <button onClick=${() => dispatch('increment')}>+1</button>
+  <button onClick=${() => dispatch('decrement')}>-1</button>
+  <button onClick=${() => dispatch('reset')}>reset</button>
+  </${Fragment}>`
+}
+
 const init = () => {
-  const counterContainer = document.querySelector('.counter')
+  const counterContainer = document.querySelector('.counter.useState')
   render(html`<${MyCounterJa} /><${MyCounterEn} />`, counterContainer)
+  const reducerContainer = document.querySelector('.counter.useReducer')
+  render(html`<${MyReducerCounter} />`, reducerContainer)
 }
 
 init()
