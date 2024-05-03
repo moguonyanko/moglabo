dfe2d15051410d39f863aed6be6e075ddcd74be3
moguonyanko <moguo_50000_nyanko@yahoo.co.jp>
2024-05-03 11:16:36 +0900

    select要素を含むフォームのサンプル追加

--- lang/javascript/webxam/public/preact/form/main.js
+++ lang/javascript/webxam/public/preact/form/main.js
@@ -27,9 +27,35 @@ const SimpleTodoForm = props => {
     </form>`
 }
 
+const SelectForm = props => {
+  const [value, setValue] = useState(props.defaultValue)
+
+  const onSubmit = event => {
+    alert(`登録:${value}`) // 実際はここでサーバへの送信を行う。
+    event.preventDefault()
+  }
+
+  const onChange = event => {
+    setValue(event.currentTarget.value)
+  }
+
+  return html`
+    <form onSubmit=${onSubmit}>
+      <select value=${value} onChange=${onChange}>
+        <option value="apple">りんご</option>
+        <option value="orange">みかん</option>
+        <option value="banana">バナナ</option>
+      </select>      
+      <button type="submit">登録</button>
+    </form>`
+}
+
 const init = () => {
   const simpleFormContainer = document.querySelector('.container.simpleForm')
   render(html`<${SimpleTodoForm} defaultValue="PreactTest" />`, simpleFormContainer)
+
+  const selectFormContainer = document.querySelector('.container.selectForm')
+  render(html`<${SelectForm} defaultValue="banana" />`, selectFormContainer)
 }
 
 init()
