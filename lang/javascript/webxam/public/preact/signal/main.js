43e4d0b3d476584d26fb247cb91f5cef4a755f84
moguonyanko <moguo_50000_nyanko@yahoo.co.jp>
2024-05-05 15:32:01 +0900

    signalのインポート先を修正

--- lang/javascript/webxam/public/preact/signal/main.js
+++ lang/javascript/webxam/public/preact/signal/main.js
@@ -3,22 +3,53 @@
  * 参考:
  * https://preactjs.com/guide/v10/signals/
  */
-import { render, Fragment } from 'https://esm.sh/preact'
-import { useState, useReducer, useRef, useEffect, useErrorBoundary } from 'https://esm.sh/preact/hooks'
-import { signal } from "@preact/signals"
+import { render, Component } from 'https://esm.sh/preact'
+import { useSignal, signal, effect } from "https://esm.sh/@preact/signals"
+//import { useSignal, signal, effect } from "https://esm.sh/@preact/signals-react"
 import { html } from '../comcom.js'
 
 const signalCount = signal(0)
 
 function SignalCounter() {
-  return html`<${Fragment}>
-  <span>${signalCount}</span>
-  <button onClick=${() => signalCount.value++}>加算</button>
-  </${Fragment}>`
+//  const signalCount = useSignal(0)
+// TODO: effectに渡した関数が呼び出されない。
+  effect(() => console.log(signalCount))
+
+  const onClick = () => {
+    signalCount.value += 1
+    return signalCount.value
+  }
+
+  // TODO: 値を更新しても再レンダリングされない。
+  return html`<div>
+  <span>${signalCount.value}</span>
+  <button onClick=${onClick}>加算</button>
+  </div>`
 }
 
+/**
+ * クラスコンポーネントを使ってもシグナルの値に従って再レンダリングは行われない。
+ */
+// class SignalCounter extends Component {
+//   constructor() {
+//     super()
+//     effect(() => console.log(signalCount))
+//   }
+
+//   onClick() {
+//     return signalCount.value++
+//   }
+
+//   render() {
+//     return html`<div>
+//     <span>${signalCount.value}</span>
+//     <button onClick=${this.onClick}>加算</button>
+//     </div>`  
+//   }
+// }
+
 const init = () => {
-  const counterContainer = document.querySelector('.counter')
+  const counterContainer = document.querySelector('.container.signalCounter')
   render(html`<${SignalCounter} />`, counterContainer)
 }
 
