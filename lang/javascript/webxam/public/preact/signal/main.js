315bf98c9161d7564229c118c5f83f612b69fa5c
moguonyanko <moguo_50000_nyanko@yahoo.co.jp>
2024-05-06 00:39:51 +0900

    signalのサンプル修正中

--- lang/javascript/webxam/public/preact/signal/main.js
+++ lang/javascript/webxam/public/preact/signal/main.js
@@ -4,53 +4,55 @@
  * https://preactjs.com/guide/v10/signals/
  */
 import { render, Component } from 'https://esm.sh/preact'
+import { useState } from 'https://esm.sh/preact/hooks'
 import { useSignal, signal, effect } from "https://esm.sh/@preact/signals"
 //import { useSignal, signal, effect } from "https://esm.sh/@preact/signals-react"
 import { html } from '../comcom.js'
 
 const signalCount = signal(0)
 
-function SignalCounter() {
+function SignalCounterFunction() {
 //  const signalCount = useSignal(0)
 // TODO: effectに渡した関数が呼び出されない。
   effect(() => console.log(signalCount))
 
   const onClick = () => {
-    signalCount.value += 1
-    return signalCount.value
+    return signalCount.value++
   }
 
   // TODO: 値を更新しても再レンダリングされない。
   return html`<div>
   <span>${signalCount.value}</span>
-  <button onClick=${onClick}>加算</button>
+  <button onClick=${() => signalCount.value++}>加算</button>
   </div>`
 }
 
 /**
  * クラスコンポーネントを使ってもシグナルの値に従って再レンダリングは行われない。
  */
-// class SignalCounter extends Component {
-//   constructor() {
-//     super()
-//     effect(() => console.log(signalCount))
-//   }
+class SignalCounterClass extends Component {
+  constructor() {
+    super()
+    effect(() => console.log(signalCount))
+  }
 
-//   onClick() {
-//     return signalCount.value++
-//   }
+  onClick() {
+    signalCount.value++
+    this.render() // renderを呼び直しても再レンダリングされない。されたとしてもsignalの意味はないが。
+    return signalCount.value
+  }
 
-//   render() {
-//     return html`<div>
-//     <span>${signalCount.value}</span>
-//     <button onClick=${this.onClick}>加算</button>
-//     </div>`  
-//   }
-// }
+  render() {
+    return html`<div>
+    <span>${signalCount.value}</span>
+    <button onClick=${this.onClick.bind(this)}>加算</button>
+    </div>`  
+  }
+}
 
 const init = () => {
   const counterContainer = document.querySelector('.container.signalCounter')
-  render(html`<${SignalCounter} />`, counterContainer)
+  render(html`<${SignalCounterFunction} /><${SignalCounterClass} />`, counterContainer)
 }
 
 init()
