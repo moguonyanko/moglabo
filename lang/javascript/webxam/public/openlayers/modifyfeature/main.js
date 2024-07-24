ab9d9393f47e0e1e4a5ddee817151b1cc0bc1ff4
moguonyanko <moguo_50000_nyanko@yahoo.co.jp>
2024-07-25 00:14:48 +0900

    自己交差した点を戻す処理の実装

--- lang/javascript/webxam/public/openlayers/modifyfeature/main.js
+++ lang/javascript/webxam/public/openlayers/modifyfeature/main.js
@@ -7,7 +7,7 @@
 
 import { initMap } from '../openlayerscommon.js'
 
-const { Select, Modify, Draw } = ol.interaction
+const { Select, Modify, Draw, Snap } = ol.interaction
 const { defaults } = ol.interaction.defaults
 const VectorLayer = ol.layer.Vector
 const VectorSource = ol.source.Vector
@@ -16,7 +16,7 @@ const { GeoJSON } = ol.format
 const geoJson = new GeoJSON()
 
 const source = new VectorSource({
-  url: 'sample_donutpolygon.json',
+  url: 'sample_polygons.json',
   format: new GeoJSON(),
   wrapX: false
 })
@@ -30,7 +30,6 @@ const select = new Select({
   wrapX: false
 })
 /**
- * TODO: ポリゴンが交差していたらエラーになるようにしたい。
  * 参考:
  * https://openlayers.org/en/latest/apidoc/module-ol_geom_Polygon-Polygon.html#intersectsCoordinate
  */
@@ -38,16 +37,17 @@ const modify = new Modify({
   features: select.getFeatures()
 })
 
+//図形をフリーハンドで描ける。
 // const draw = new Draw({
 //   source,
 //   type: 'Polygon',
 // })
 
-let map, preFeature
-
-modify.on('modifystart', event => {
-  preFeature = event.features.item(0).clone()
-})
+// modify.removePoint()を呼び出すだけでundoできるので古い地物を保存しておく必要はない。
+// let oldFeature
+// modify.on('modifystart', event => {
+//   oldFeature = event.features.item(0).clone()
+// })
 
 modify.on('modifyend', async event => {
   const newFeature = event.features.pop()
@@ -55,7 +55,6 @@ modify.on('modifyend', async event => {
   const body = geoJson.writeFeatures([newFeature])
   const response = await fetch('/brest/gis/crosscheck/', {
     method: 'POST',
-    mode: 'cors',
     headers: {
       'Content-Type': 'application/json'
     },
@@ -67,18 +66,18 @@ modify.on('modifyend', async event => {
   const { result } = await response.json()
   if (result) {
     alert('ポリゴンが自己交差しています！')
-    // TOOD: 編集前のポリゴンに戻す処理
-    source.removeFeature(newFeature)
-    source.addFeature(preFeature)
-    // TODO: 図形を非選択状態に戻したい。
-    //map.removeInteraction(modify)
-    //map.addInteraction(modify)
-    select.setActive(false)
+    // 一点前に戻る。
+    modify.removePoint()
   }
 })
 
-map = initMap({
-  interactions: defaults().extend([select, modify]),
+// 点や線分に入力中の点を「吸着」させることができる。
+const snap = new Snap({
+  source
+})
+
+const map = initMap({
+  interactions: defaults().extend([select, modify, snap]),
   layers: [vector],
   zoom: 17
 })
