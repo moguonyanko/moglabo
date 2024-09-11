73473b9f0cd44ce687e1b16f7dc5dfe80933fbf1
moguonyanko <moguo_50000_nyanko@yahoo.co.jp>
2024-09-12 01:14:59 +0900

    Add zig comptime more example

--- lang/zig/practicezig/test_comptime.zig
+++ lang/zig/practicezig/test_comptime.zig
@@ -1,8 +1,12 @@
 const expect = @import("std").testing.expect;
 
+// 普通に呼び出すと引数nが35程度でとても遅くなる関数
 fn fibonacci(n: u32) u32 {
-    if (n == 0 or n == 1) return n;
-    return fibonacci(n - 1) + fibonacci(n - 2);
+    if (n == 0 or n == 1) {
+        return n;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 }
 
 test "comptimeで実行時間を短縮できる" {
@@ -14,4 +18,27 @@ test "comptimeで実行時間を短縮できる" {
     };
     try expect(y == expected);
     try expect(x == expected);
+}
+
+test "コンパイル時にリテラルは型を強制される" {
+    const a = 11; // コンパイル時に値が収まる型を強制される。
+    // コンパイル時に条件を満たす型を割り当てることができる。
+    const b: if (a < 10) f32 else i32 = 5;
+    try expect(b == 5);
+    try expect(@TypeOf(b) == i32);
+}
+
+fn Rect(
+    comptime T: type,
+    comptime width: comptime_int,
+    comptime height: comptime_int,
+) type {
+    return [width][height]T;
+}
+
+test "型を返すことができる" {
+    const width = 120;
+    const height = 360;
+    const rect = Rect(u16, width, height);
+    try expect(rect == [width][height]u16);
 }
