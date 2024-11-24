2e9238001eb3635fb5621ad487d5f4ba4d1de8f2
moguonyanko <moguo_50000_nyanko@yahoo.co.jp>
2024-11-25 01:44:56 +0900

    Add Detector API example

--- lang/javascript/webxam/public/webapi/translator/main.js
+++ lang/javascript/webxam/public/webapi/translator/main.js
@@ -5,6 +5,20 @@
  */
 /* eslint-disable no-undef */
 
+const translate = async ({sourceText, sourceLanguage, targetLanguage}) => {
+  const translator = await translation.createTranslator({
+    sourceLanguage,
+    targetLanguage
+  })
+  translator.ondownloadprogress = progressEvent => {
+    // TODO: ProgressEventが発生していない？
+    console.log(progressEvent.loaded, progressEvent.total)
+  }
+
+  const result = await translator.translate(sourceText)
+  return result
+}
+
 const funcs = {
   canTranslate: async () => {
     const sourceLanguage = document.querySelector('.canTranslate .sourceLanguage').value
@@ -21,20 +35,33 @@ const funcs = {
   translate: async () => {
     const sourceLanguage = document.querySelector('.translate .sourceLanguage').value
     const targetLanguage = document.querySelector('.translate .targetLanguage').value
-
-    const translator = await translation.createTranslator({
-      sourceLanguage,
-      targetLanguage
-    })
-    translator.ondownloadprogress = progressEvent => {
-      // TODO: ProgressEventが発生していない？
-      console.log(progressEvent.loaded, progressEvent.total)
-    }
-
     const sourceText = document.querySelector('.translate .sourceText').value
-    const result = await translator.translate(sourceText)
+    const result = await translate({sourceText, sourceLanguage, targetLanguage})
     const output = document.querySelector('.translate .output')
     output.textContent = result
+  },
+  translateWithDetection: async () => {
+    const canDetectResult = await translation.canDetect()
+    if (canDetectResult === 'no') {
+      return
+    }
+    if (canDetectResult === 'readily') {
+      const detector = await translation.createDetector()
+      const sourceText = document.querySelector('.detector .sourceText').value
+      const detectorResults = await detector.detect(sourceText) 
+      let sourceLanguage
+      let maxConficdence = 0
+      for (let detectorResult of detectorResults) {
+        console.log(detectorResult)
+        if (maxConficdence < detectorResult.confidence) {
+          sourceLanguage = detectorResult.detectedLanguage
+          maxConficdence = detectorResult.confidence
+        }
+      }
+      const result = await translate({sourceText, sourceLanguage, targetLanguage: 'ja'})
+      const output = document.querySelector('.detector .output')
+      output.textContent = result
+    }
   }
 }
 
@@ -53,9 +80,13 @@ const enableTranslatorApi = () => {
   return 'translation' in self && 'createTranslator' in self.translation
 }
 
+const enableDetectorApi = () => {
+  return 'translation' in self && 'canDetect' in self.translation
+}
+
 const checkEnableApi = () => {
   const infoEle = document.getElementById('enableApi')
-  const result = enableTranslatorApi()
+  const result = enableTranslatorApi() && enableDetectorApi()
   infoEle.textContent = result
   if (result) {
     infoEle.classList.add('enable')
