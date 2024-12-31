1c378b71c7d73376f8fb74113bef6652ca0ba288
moguonyanko <moguo_50000_nyanko@yahoo.co.jp>
2025-01-01 01:40:54 +0900

    Modified comment

--- lang/javascript/webxam/public/webapi/viewtransitions/spa.js
+++ lang/javascript/webxam/public/webapi/viewtransitions/spa.js
@@ -1,5 +1,5 @@
 /**
- * @fileoverview PageRevealEventを試すためのスクリプト
+ * @fileoverview 基本的な SPA ビュー遷移を試すためのスクリプト
  * 参考:
  * https://developer.mozilla.org/ja/docs/Web/API/View_Transition_API/Using#javascript_%E3%82%92%E6%B4%BB%E7%94%A8%E3%81%97%E3%81%9F%E7%8B%AC%E8%87%AA%E3%81%AE%E6%96%87%E6%9B%B8%E5%86%85_spa_%E9%81%B7%E7%A7%BB
  * https://mdn.github.io/dom-examples/view-transitions/spa/#
