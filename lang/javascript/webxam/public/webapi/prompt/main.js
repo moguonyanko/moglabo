cf310bc3f9d9704472fd81854911c3c7d080e63f
moguonyanko <moguo_50000_nyanko@yahoo.co.jp>
2025-02-20 01:13:44 +0900

    Prompt APIのpromptメソッドのサンプル追加

--- lang/javascript/webxam/public/webapi/prompt/main.js
+++ lang/javascript/webxam/public/webapi/prompt/main.js
@@ -1,4 +1,32 @@
 /**
  * @fileoverview Prompt APIを試すためのスクリプトです。
+ * 参考:
+ * https://web.dev/articles/ai-chatbot-promptapi?hl=ja
  */
 
+// DOM
+
+const systemPrompt = 'You are an excellent assistant. I will answer questions about work and study.'
+
+const listeners = {
+  doPrompt: async () => {
+    const section = document.querySelector('.promptExample')
+    const session = await self.ai.languageModel.create({
+      systemPrompt
+    })
+    const prt = section.querySelector('.prompt').value
+    const result = await session.prompt(prt)
+    const output = section.querySelector('.output')
+    output.textContent = result
+  }
+}
+
+const init = () => {
+  const main = document.querySelector('main')
+  main.addEventListener('click', async event => {
+    const { eventListener } = event.target.dataset
+    await listeners[eventListener]?.()
+  })
+}
+
+init()
