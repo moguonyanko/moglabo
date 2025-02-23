4c6b3f249de7d22000b1f2adb65e9a48ad47680b
moguonyanko <moguo_50000_nyanko@yahoo.co.jp>
2025-02-24 00:53:04 +0900

    Add scrollend event example

--- lang/javascript/webxam/public/webapi/scrollend/main.js
+++ lang/javascript/webxam/public/webapi/scrollend/main.js
@@ -2,3 +2,13 @@
  * @fileoverview scrollendイベントを調査するためのスクリプト
  */
 
+const init = () => {
+  // bodyやmain要素はscrollイベントやscrollendイベントを発生させない。
+  const sampleArea = document.querySelector('.samplearea')
+  sampleArea.addEventListener('scrollend', event => {
+    const output = document.querySelector('.scrollendsample .output')
+    output.innerHTML += `scrollend: ${new Date().getTime()}<br />`
+  })
+}
+
+init()
