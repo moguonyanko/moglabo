22c70157bec4b13be752dbc0e5355256c806ba70
moguonyanko <moguo_50000_nyanko@yahoo.co.jp>
2025-03-08 01:37:37 +0900

    ファイル変更を監視するサンプル追加

--- lang/javascript/webxam/public/webapi/filesystemobserver/main.js
+++ lang/javascript/webxam/public/webapi/filesystemobserver/main.js
@@ -9,12 +9,39 @@ const enableFileSystemObserver = () => {
   return typeof new FileSystemObserver(() => {}).observe === 'function'
 }
 
+const getDisplayFileRecordFunc = output => {
+  return (records, observer) => { 
+    for (const record of records) {
+      output.innerHTML += `${JSON.stringify(record)}<br />`
+    }
+  }
+}
+
+let observer;
+
 const listenres = {
+  connect: async () => {
+    const output = document.querySelector('.callbackFunction .output')
+    observer = new FileSystemObserver(getDisplayFileRecordFunc(output))
+    const fileHandle = await showSaveFilePicker()
+    await observer.observe(fileHandle)
+  },
+  disconnect: () => {
+    observer.disconnect()
+    const output = document.querySelector('.callbackFunction .output')
+    output.textContent = ''
+  }
 }
 
 const init = () => {
   document.getElementById('enableFileSystemObserver').textContent = 
   enableFileSystemObserver()
+
+  const main = document.querySelector('main')
+  main.addEventListener('click', async event => {
+    const { eventListener } = event.target.dataset
+    await listenres[eventListener]?.()
+  })
 }
 
 init()
