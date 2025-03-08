53bd4edc2c1bed39656343d16f77fd286babc913
moguonyanko <moguo_50000_nyanko@yahoo.co.jp>
2025-03-09 00:54:02 +0900

    ディレクトリを監視するサンプル追加

---
+++ lang/javascript/webxam/public/webapi/originprivatefilesystem/main.js
@@ -0,0 +1,58 @@
+/**
+ * @fileoverview Origin Private File SystemのAPIを試すためのスクリプト
+ */
+/* eslint-disable no-undef */
+
+// DOM
+
+const enableFileSystemObserver = () => {
+  return typeof new FileSystemObserver(() => {}).observe === 'function'
+}
+
+const getObserveDumpFunc = output => {
+  return (records, observer) => { 
+    for (const record of records) {
+      output.innerHTML += `${JSON.stringify(record)}<br />`
+    }
+  }
+}
+
+let fileObserver, directoryObserver;
+
+const listenres = {
+  connectFile: async () => {
+    const output = document.querySelector('.observeFile .output')
+    fileObserver = new FileSystemObserver(getObserveDumpFunc(output))
+    const handle = await showSaveFilePicker()
+    await fileObserver.observe(handle)
+  },
+  disconnectFile: () => {
+    fileObserver.disconnect()
+    const output = document.querySelector('.observeFile .output')
+    output.textContent = ''
+  },
+  connectDirectory: async () => {
+    const output = document.querySelector('.observeDirectory .output')
+    directoryObserver = new FileSystemObserver(getObserveDumpFunc(output))
+    const handle = await showDirectoryPicker()
+    await directoryObserver.observe(handle)
+  },
+  disconnectDirectory: async () => {
+    directoryObserver.disconnect()
+    const output = document.querySelector('.observeDirectory .output')
+    output.textContent = ''
+  }
+}
+
+const init = () => {
+  document.getElementById('enableFileSystemObserver').textContent = 
+  enableFileSystemObserver()
+
+  const main = document.querySelector('main')
+  main.addEventListener('click', async event => {
+    const { eventListener } = event.target.dataset
+    await listenres[eventListener]?.()
+  })
+}
+
+init()
