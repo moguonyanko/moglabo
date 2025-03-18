ddf4126a55923d2fb7250b456189a54bf751b910
moguonyanko <moguo_50000_nyanko@yahoo.co.jp>
2025-03-19 01:20:12 +0900

    テキスト生成のためのサンプルページ追加

---
+++ lang/javascript/webxam/public/genai/generatetext/main.js
@@ -0,0 +1,39 @@
+/**
+ * @fileoverview Gemini APIでテキスト生成するサンプル
+ */
+
+const listeners = {
+  sendContents: async () => {
+    const contents = document.querySelector('.simple-generation-text-sample .sample-text')
+      .value
+    if (!contents) {
+      return
+    }
+    const response = await fetch(`/brest/generate/text/`, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        contents
+      })
+    })
+    if (!response.ok) {
+      throw new Error(`ERROR: ${response.statusText}`)
+    }
+    const json = await response.json()
+    const output = document.querySelector('.imple-generation-text-sample .output')
+    output.textContent = json.results
+  }
+}
+
+const init = () => {
+  const main = document.querySelector('main')
+  main.addEventListener('click', async event => {
+    const { eventListener } = event.target.dataset
+    await listeners[eventListener]?.()
+  })
+}
+
+init()
