75b5f9b35dea0fb93e60627850a1dabbbc46aee2
moguonyanko <moguo_50000_nyanko@yahoo.co.jp>
2025-03-23 01:36:59 +0900

    画像からテキスト生成するサンプル追加

--- lang/javascript/webxam/public/genai/generatetext/main.js
+++ lang/javascript/webxam/public/genai/generatetext/main.js
@@ -2,6 +2,8 @@
  * @fileoverview Gemini APIでテキスト生成するサンプル
  */
 
+// DOM
+
 const listeners = {
   sendContents: async () => {
     const contents = document.querySelector('.simple-generation-text-sample .sample-text')
@@ -9,11 +11,11 @@ const listeners = {
     if (!contents) {
       return
     }
-    const response = await fetch(`/brest/genaiapi/generate/text/`, {
+    const response = await fetch('/brest/genaiapi/generate/text/', {
       method: 'POST',
       mode: 'cors',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify({
         contents
@@ -22,9 +24,19 @@ const listeners = {
     if (!response.ok) {
       throw new Error(`ERROR: ${response.statusText}`)
     }
-    const json = await response.json()
     const output = document.querySelector('.simple-generation-text-sample .output')
-    output.textContent = json.results
+    output.textContent = (await response.json()).text
+  },
+  sendImage: async () => {
+    const contents = new FormData()
+    const selectedFile = document.querySelector(".generation-text-by-image .selected-file")
+    contents.append("file", selectedFile.files[0])
+    const response = await fetch('/brest/genaiapi/generate/text-from-image/', {
+      method: 'POST',
+      body: contents
+    })    
+    const output = document.querySelector('.generation-text-by-image .output')
+    output.textContent = (await response.json()).text
   }
 }
 
