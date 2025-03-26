c7229766d15ab75f7f159b504c8878f45fc51122
moguonyanko <moguo_50000_nyanko@yahoo.co.jp>
2025-03-27 00:53:28 +0900

    画像生成サンプル追加

---
+++ lang/javascript/webxam/public/genai/generateimage/main.js
@@ -0,0 +1,66 @@
+/**
+ * @fileoverview 画像生成を試すためのスクリプト
+ */
+
+const getGeneratedImageBlob = async contents => {
+  const response =  await fetch('/brest/genaiapi/generate/image/', {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      contents
+    })
+  })
+  if (!response.ok) {
+    const message = await response.text()
+    throw new Error(message)
+  }
+  const contentType = response.headers['COntent-Type']
+  if (contentType.startsWith('image')) {
+    return await response.blob()    
+  } else {
+    return new Blob(await response.text(), { type: contentType })
+  }
+}
+
+// DOM
+
+const listeners = {
+  generateImage: async () => {
+    const contents = document.querySelector('.prompt').value
+    const output = document.querySelector('.simple-generation-image .output')
+    try {
+      const blob = await getGeneratedImageBlob(contents)
+      const imgUrl = URL.createObjectURL(blob)
+      const img = new Image()
+      img.onload = () => {
+        URL.revokeObjectURL(imgUrl)
+        output.textContent = ''
+        output.appendChild(img)
+      }
+      img.src = imgUrl  
+    } catch (err) {
+      output.textContent = err.message
+      const evt = new CustomEvent('generateimageerror', {
+        detail: err.message
+      })
+      window.dispatchEvent(evt)
+      return
+    }
+  }
+}
+
+const init = () => {
+  document.body.addEventListener('click', async event => {
+    const { eventListener } = event.target.dataset
+    await listeners[eventListener]?.()
+  })
+
+  window.addEventListener('generateimageerror', err => {
+    alert(err.detail)
+  })
+}
+
+init()
