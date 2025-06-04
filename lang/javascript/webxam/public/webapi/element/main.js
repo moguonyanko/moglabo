e086f59da0c476d880969f1c5eb9334908bca119
moguonyako <moguonyanko@gmail.com>
2025-06-04 09:19:43 +0900

    FavLangDialogコンポーネントを追加し、ダイアログの表示とイベント処理を実装

--- lang/javascript/webxam/public/webapi/element/main.js
+++ lang/javascript/webxam/public/webapi/element/main.js
@@ -16,6 +16,127 @@ const toggleMoveTarget = type => {
   }
 }
 
+/**
+ * 特定の構造を含むdialogを提供したいのでbuild-in elementではなく
+ * 通常のHTMLElementを継承している。
+ */
+class FavLangDialog extends HTMLElement {
+  constructor() {
+    super()
+
+    const shadow = this.attachShadow({ mode: 'open' })
+    const template = document.getElementById('fav-lang-dialog')
+    if (template) {
+      const clone = template.content.cloneNode(true)
+      shadow.appendChild(clone)
+    } else {
+      console.warn('fav-lang-dialog template not found')
+    }
+  }
+
+  appendDialogEventMessage(message) {
+    const langInfo = document.createTextNode(message)
+    this.output.appendChild(langInfo)
+    this.output.appendChild(document.createElement('br'))
+  }
+
+  appendCancelEventMessage(message) {
+    this.appendDialogEventMessage(`cancelイベント発生:${message}`)
+  }
+
+  // built-inのCustomElementではないので、showModalは独自に実装する必要がある。
+  // close, requestCloseは外部から呼び出すことはないので実装していない。
+
+  /**
+   * CustomElementのコンストラクタは引数としてoutputを受け取ることはできないので、
+   * showModalメソッドでoutputを受け取る。
+   * @param {HTMLElement} output 
+   */
+  showModal(output) {
+    this.output = output
+    const dialog = this.shadowRoot.querySelector('dialog')
+    if (dialog) {
+      dialog.showModal()
+    } else {
+      console.error('Dialog element not found in shadow DOM')
+    }
+  }
+
+  // close() {
+  //   const dialog = this.shadowRoot.querySelector('dialog')
+  //   if (dialog) {
+  //     dialog.close()
+  //   } else {
+  //     console.error('Dialog element not found in shadow DOM')
+  //   }
+  // }
+
+  // requestClose(reason) {
+  //   const dialog = this.shadowRoot.querySelector('dialog')
+  //   if (dialog) {
+  //     dialog.requestClose(reason)
+  //   } else {
+  //     console.error('Dialog element not found in shadow DOM')
+  //   }
+  // }
+
+  connectedCallback() {
+    const dialog = this.shadowRoot.querySelector('dialog')
+
+    // submit以外が指定されたボタンをクリックした時に、requestCloseが呼び出されると
+    // cancelイベントが発生する。ただし2025/06/03時点のFirefoxNightlyでは、
+    // cancelイベントは発生しない。
+    // cancelするかどうかを決定できるイベントなのでcancelableイベントの方が妥当な名称なのではないか？
+    dialog.addEventListener('cancel', event => {
+      console.log(event)
+      if (!event.cancelable) {
+        return
+      }
+      const baseMessage = 'cancelイベント発生'
+      const selectedLang = dialog.querySelector('#favlang').value
+      if (selectedLang) { // cancelせずダイアログのcloseへ移行する。
+        this.appendCancelEventMessage(selectedLang)
+      } else { // ダイアログのcloseをcancelする。
+        event.preventDefault()
+        this.appendCancelEventMessage('言語を何か選択して下さい')
+        this.dispatchEvent(new CustomEvent('dialogcloseerror', {
+          composed: true,
+          bubbles: true,
+          detail: {
+            message: '言語を何か選択して下さい'
+          }
+        }))
+      }
+    })
+
+    // dialog.getElementByIdは未定義である。
+    const closeButton = dialog.querySelector('#closeButton')
+    closeButton.addEventListener('click', () => {
+      // cancelイベントを発生させた後、closeイベントを発生させてダイアログを閉じることができる。
+      // ただし引数に渡した値がcloseイベントに渡されることはない。
+      dialog.requestClose("request close")
+    })
+
+    dialog.addEventListener('close', event => {
+      console.log(event)
+      this.appendDialogEventMessage('closeイベント完了')
+
+      this.dispatchEvent(new CustomEvent('dialogclose', {
+        composed: true,
+        bubbles: true,
+        detail: {
+          message: 'ダイアログが閉じられました'
+        }
+      }))
+    })
+  }
+
+  disconnectedCallback() {
+    // ダイアログが削除された時の処理
+    console.log('FavLangDialog disconnected')
+  }
+}
+
 const funcs = {
   openDialog: () => {
     const dialog = document.getElementById('inertTestDialog');
@@ -60,59 +181,23 @@ const funcs = {
   doInsertBefore: () => {
     toggleMoveTarget('insertBefore')
   },
-  showDialog: () => {
-    const output = document.querySelector('.request-close .output')
+  showDialog: () => { 
+    const base = document.querySelector('.request-close')
+    const output = base.querySelector('.output')
     output.textContent = ''
-
-    const dialog = document.getElementById('favDialog')
-    dialog.showModal()
-
-    const appendDialogEventMessage = message => {
-      const langInfo = document.createTextNode(message)
-      output.appendChild(langInfo)
-      output.appendChild(document.createElement('br'))
-    }
-
-    const appendCancelEventMessage = message => {
-      appendDialogEventMessage(`cancelイベント発生:${message}`)
-    }
-
-    // submit以外が指定されたボタンをクリックした時に、requestCloseが呼び出されると
-    // cancelイベントが発生する。ただし2025/06/03時点のFirefoxNightlyでは、
-    // cancelイベントは発生しない。
-    // cancelするかどうかを決定できるイベントなのでcancelableイベントの方が妥当な名称なのではないか？
-    dialog.addEventListener('cancel', event => {
-      console.log(event)
-      if (!event.cancelable) {
-        return
-      }
-      const baseMessage = 'cancelイベント発生'
-      const selectedLang = dialog.querySelector('#favlang').value
-      if (selectedLang) { // cancelせずダイアログのcloseへ移行する。
-        appendCancelEventMessage(selectedLang)
-      } else { // ダイアログのcloseをcancelする。
-        event.preventDefault()
-        appendCancelEventMessage('言語を何か選択して下さい')
-        dialog.dispatchEvent(new Event('dialogcloseerror', {
-          detail: {
-            message: '言語を何か選択して下さい'
-          }
-        }))
-      }
+    const dialog = document.createElement('fav-lang-dialog')
+    base.appendChild(dialog)
+    
+    dialog.addEventListener('dialogclose', event => {
+      base.removeChild(dialog)
+      // output.textContent = event.detail.message
     })
 
-    // dialog.getElementByIdは未定義である。
-    const closeButton = dialog.querySelector('#closeButton')
-    closeButton.addEventListener('click', () => {
-      // cancelイベントを発生させた後、closeイベントを発生させてダイアログを閉じることができる。
-      // ただし引数に渡した値がcloseイベントに渡されることはない。
-      dialog.requestClose("request close")
+    dialog.addEventListener('dialogcloseerror', event => {
+      alert(event.detail.message)
     })
 
-    dialog.addEventListener('close', event => {
-      console.log(event)
-      appendDialogEventMessage('closeイベント完了')
-    })
+    dialog.showModal(output)
   }
 };
 
@@ -125,10 +210,6 @@ const init = () => {
         funcs[eventTarget]();
       }
     }
-
-    window.addEventListener('dialogcloseerror', event => {
-      alert(event.detail.message)
-    })
   });
 
   // TODO: beforematchイベントを発生させることができていない。
@@ -136,6 +217,8 @@ const init = () => {
   document.body.addEventListener('beforematch', event => {
     console.log(event);
   });
+
+  customElements.define('fav-lang-dialog', FavLangDialog)
 };
 
 init();
