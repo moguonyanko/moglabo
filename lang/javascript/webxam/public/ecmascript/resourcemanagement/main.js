2211236bd69962da30a93c26b8e4c3876e4360cd
moguonyako <moguonyanko@gmail.com>
2025-06-25 00:30:20 +0900

    リソース管理のためのDisposableStackのサンプルを追加し、関連するHTML要素とJavaScriptのロジックを実装

--- lang/javascript/webxam/public/ecmascript/resourcemanagement/main.js
+++ lang/javascript/webxam/public/ecmascript/resourcemanagement/main.js
@@ -12,26 +12,76 @@ class SampleTextReader {
   }
 }
 
+const getSampleResource = callback => {
+  return {
+    reader: new SampleTextReader(),
+    [Symbol.dispose]() {
+      const text = this.reader.releaseLock()
+      callback(text)
+    }
+  }
+}
+
 const actions = {
   sampleResourceManagementClicked: event => {
     const output = document.querySelector('.using-simple-example .output')
 
-    using sampleResource = {
-      reader: new SampleTextReader(),
-      [Symbol.dispose]() {
-        const text = this.reader.releaseLock()
-        output.innerHTML += `<br />${text}`
-      }
-    }
+    using sampleResource = getSampleResource(text => {
+      output.innerHTML += `<br />${text}`
+    })
+
     const { reader } = sampleResource
 
-    // リソースを使用するブロックのスコープが分離されていなくても自動でリソース解放される。
+    // Javaのtry-with-resourcesのようにリソースを使用するブロックのスコープが明示的に分離されていなくても、
+    // usingで指定した変数のガベージコレクションが実行されるタイミングでリソースが解放される。
     // {
     const text = reader.read()
     output.textContent = text
     // }
 
     // 自動でリソースが解放される
+  },
+  disposableStackSampleClicked: () => {
+    const output = document.querySelector('.disposable-stack-example .output')
+
+    {
+      using useStack = new DisposableStack()
+      useStack.use(getSampleResource(text => {
+        output.innerHTML += `use:${text}<br />`
+      }))
+    }
+
+    {
+      using adoptStack = new DisposableStack()
+      adoptStack.adopt(new SampleTextReader(), reader => {
+        const releasedResult = reader.releaseLock()
+        output.innerHTML += `adopt:${releasedResult}<br />`
+      })
+    }
+
+    {
+      using deferStack = new DisposableStack()
+      deferStack.defer(() => {
+        output.innerHTML += `defer<br />`
+      })
+    }
+
+    {
+      using beforeMoveStack = new DisposableStack()
+      beforeMoveStack.adopt(new SampleTextReader(), reader => {
+        const releasedResult = reader.releaseLock()
+        output.innerHTML += `moved:${releasedResult}<br />`
+      })
+      using afterMoveStack = beforeMoveStack.move()
+    }
+
+    {
+      const disposeStack = new DisposableStack()
+      disposeStack.use(getSampleResource(text => {
+        output.innerHTML += `dispose:${text}<br />`
+      }))
+      disposeStack.dispose()
+    }
   }
 }
 
