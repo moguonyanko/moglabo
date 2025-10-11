4876a11993cf1d9f43292cfbc6f933f4d169099b
moguonyako <moguonyanko@gmail.com>
2025-10-11 15:08:38 +0900

    CustomElementsをformに追加するサンプル作成

--- lang/javascript/webxam/public/webapi/customelements/exam.js
+++ lang/javascript/webxam/public/webapi/customelements/exam.js
@@ -51,9 +51,17 @@ class CustomMessage extends HTMLElement {
   }
 }
 
+const getInnerHTML = ({ tag, includeShadowRoots }) => {
+  if (typeof tag.getInnerHTML === 'function') {
+    return tag.getInnerHTML({ includeShadowRoots })
+  } else {
+    return ''
+  }
+}
+
 const getTemplateInnerHTMLList = ({ query, includeShadowRoots = true }) => {
   const templates = Array.from(document.querySelectorAll(query));
-  return templates.map(t => t.getInnerHTML({ includeShadowRoots }));
+  return templates.map(t => getInnerHTML({ tag: t, includeShadowRoots }));
 };
 
 class SampleDeclarativeElement extends HTMLElement {
@@ -83,15 +91,107 @@ class CustomProfile extends HTMLElement {
   }
 }
 
+/**
+ * built-inのCustomElementsではないのでHTMLSelectElementは継承できない。
+ * ユーザーによって設定された値をこの要素を組み込んだformのパラメータとして認識させるためには、
+ * setFormValueを使って値をElementInternalsに設定する必要がある。
+ */
+class CustomFavoritesLanguage extends HTMLElement {
+  static formAssociated = true
+
+  constructor() {
+    super()
+    const template = document.getElementById('custom-favorites-language')
+    const shadow = this.attachShadow({ mode: 'open' })
+    shadow.appendChild(template.cloneNode(true).content)
+
+    // プロパティにElementInternalsを設定する。
+    this.internals_ = this.attachInternals()
+    this.#updateValue(this.#selectElement().value)
+  }  
+
+  #updateValue(newValue) {
+    this.internals_.setFormValue(newValue)
+  }
+
+  #selectElement() {
+    return this.shadowRoot.querySelector('select')
+  }
+
+  get name() {
+    return this.getAttribute('name')
+  }
+
+  set name(selectedName) {
+    this.setAttribute('name', selectedName)
+  }  
+
+  connectedCallback() {
+    this.#selectElement().addEventListener('change', event => {
+      this.#updateValue(this.#selectElement().value)
+    })
+  }
+
+  // [Symbol.iterator]を実装してもCustomElementsで選択された値を
+  // form.entries()でイテレートされている時の値として返すことはできない。
+  // *[Symbol.iterator]() {
+  //   const select = this.shadowRoot.querySelector('select')
+  //   const [name, value] = [
+  //     this.getAttribute('name'),
+  //     select.value
+  //   ]
+  //   yield* [name, value]
+  // }
+}
+
+const clickListeners = {
+  onSampleDummySubmit: event => {
+    event.preventDefault()
+    const form = document.getElementById('sample-form')
+    const output = document.querySelector('.attach-internals-sample .output')
+
+    const sampleCustomSelect = document.getElementById('sample-form-select')
+    // カスタム要素がformに追加できていることの確認
+    // CustomElementsのクラスにformAssociated=trueがないと
+    // formを参照した際にNotSupportedErrorとなる。
+    const formElement = sampleCustomSelect.internals_.form
+    output.textContent = formElement.id
+
+    const formData = new FormData(formElement)
+    const formIterator = formData.entries()
+    const entries = Object.fromEntries(formIterator)
+    console.log(entries)
+
+    // formIteratorはもうイテレートできないので再度entriesを呼ぶ必要がある。
+    for (const [name, value] of formData.entries()) {
+      output.innerHTML += `<br />${name}:${value}`
+    }    
+  }
+}
+
+const addListener = () => {
+  document.body.addEventListener('click', event => {
+    const { eventListener } = event.target.dataset
+    const listener = clickListeners[eventListener]
+    if (typeof listener === 'function') {
+      event.stopPropagation()
+      listener(event)
+    }
+  })
+}
+
 const init = () => {
   customElements.define('my-autocomplete-form', MyAutoCompleteForm);
   customElements.define('custom-message', CustomMessage);
   customElements.define('custom-profile', CustomProfile);
+  customElements.define('custom-favorites-language', CustomFavoritesLanguage)
 
   const query = 'sample-declarative-element';
   console.log(getTemplateInnerHTMLList({ query }));
   console.log(getTemplateInnerHTMLList({ query, includeShadowRoots: false }));
   customElements.define(query, SampleDeclarativeElement);
+
+  addListener()
 };
 
 init();
