935580f6866ef1d4bf3adf30cbce110094e95d52
moguonyako <moguonyanko@gmail.com>
2025-11-07 20:33:16 +0900

    アップロードされた画像を表示する欄を追加

--- lang/javascript/webxam/public/genai/robotics/main.js
+++ lang/javascript/webxam/public/genai/robotics/main.js
@@ -2,7 +2,25 @@
  * @fileoverview ロボット工学系APIを確認するためのサンプルスクリプト
  */
 
-const listeners = {
+const changeListeners = {
+  selectedImage: () => {
+    const fileInput = document.getElementById('selected-files')
+    const display = document.getElementById('selected-image')
+    display.textContent = ''
+    Array.from(fileInput.files).forEach(file => {
+      const img = document.createElement('img')
+      // img.style.maxWidth = '100px'
+      // img.style.marginRight = '10px'
+      img.src = URL.createObjectURL(file)
+      img.onload = () => {
+        display.appendChild(img)
+        URL.revokeObjectURL(img.src)
+      }
+    })
+  }
+}
+
+const clickListeners = {
   detectObjects: async () => {
     const file = document.getElementById('selected-files')
     const targets = document.getElementById('targets').value.split(',')
@@ -28,18 +46,25 @@ const listeners = {
   }
 }
 
+const listeners = {
+  click: clickListeners,
+  change: changeListeners
+}
+
 const init = () => {
   const main = document.querySelector('main')
-  main.addEventListener('click', async event => {
-    const { eventListener } = event.target.dataset
-    if (typeof listeners[eventListener] === 'function') {
-      try {
-        event.target.setAttribute('disabled', 'true')
-        await listeners[eventListener]()
-      } finally {
-        event.target.removeAttribute('disabled')
+  Object.keys(listeners).forEach(eventType => {
+    main.addEventListener(eventType, async event => {
+      const { eventListener } = event.target.dataset
+      if (typeof listeners[eventType][eventListener] === 'function') {
+        try {
+          event.target.setAttribute('disabled', 'true')
+          await listeners[eventType][eventListener]()
+        } finally {
+          event.target.removeAttribute('disabled')
+        }
       }
-    }
+    })
   })
 }
 
