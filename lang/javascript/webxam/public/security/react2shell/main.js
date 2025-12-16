21937d4d76aee47ca8ea84f2b480b330cc72cd5e
moguonyako <moguonyanko@gmail.com>
2025-12-16 09:35:00 +0900

    React2Shellのセキュリティサンプルページを新規作成し、リモートコード実行機能を実装

---
+++ lang/javascript/webxam/public/security/react2shell/main.js
@@ -0,0 +1,57 @@
+/**
+ * @fileoverview React2Shell Security Example
+ */
+
+const rceTargetServers = {
+  SimpleRseApp: 'http://localhost:8081/',
+}
+
+const SERVICE_BASE_URL = 'https://localhost/brest/security/react2shell/'
+
+const clicks = {
+  onExecuteRce: async () => {
+    const rceTargetApp = Array.from(document.querySelectorAll('input[name="rce-target-app"]'))
+      .find(r => r.checked).value 
+    const rceTargetUrl = rceTargetServers[rceTargetApp]
+
+    const rceCommand = document.getElementById('rce-command').value
+
+    if (!rceCommand || !rceTargetUrl) {
+      return
+    }
+
+    const url = `${SERVICE_BASE_URL}/command/`
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        url: rceTargetUrl,
+        command: rceCommand
+      })
+    })
+
+    const { result } = await response.json()
+    console.log(result)
+    const output = document.querySelector('.react2shell-sample .output')
+    output.textContent = JSON.stringify(result, null, 2)
+  }
+}
+
+const init = () => {
+  document.body.addEventListener('click', async event => {
+    const { eventListener } = event.target.dataset
+    if (eventListener && typeof clicks[eventListener] === 'function') {
+      event.stopPropagation()
+      try {
+        event.target.setAttribute('disabled', 'disabled')
+        await clicks[eventListener]()
+      } finally {
+        event.target.removeAttribute('disabled')
+      }
+    }
+  })
+}
+
+init()
