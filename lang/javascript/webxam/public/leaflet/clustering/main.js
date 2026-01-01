877477e347c6529147207a216ac049279bb4dfa0
moguonyako <moguonyanko@gmail.com>
2026-01-01 14:35:24 +0900

    クラスタリング機能を追加し、関連するHTML、JavaScript、JSONファイルを作成した

---
+++ lang/javascript/webxam/public/leaflet/clustering/main.js
@@ -0,0 +1,89 @@
+/**
+ * @fileoverview クラスタリングを試すためのスクリプト
+ */
+/* eslint-disable no-undef */
+
+import { initMap, loadJson } from '../leaflet_util.js'
+
+let map, clusteringTargetPoints, clusteringPolygonLayer
+
+const reset = () => {
+  if (clusteringPolygonLayer) {
+    clusteringPolygonLayer.remove()
+  }
+}
+
+const listeners = {
+  onReset: () => {
+    reset()
+  },
+  onExecuteClustering: async () => {
+    reset()
+    const k = document.getElementById('cluster-count').value
+
+    const response = await fetch('/brest/gis/cluster/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ 
+        type: "FeatureCollection",
+        features: clusteringTargetPoints, 
+        k })
+    })
+
+    const geojsonData = await response.json()
+
+    // GeoJSONレイヤーを作成して地図に追加
+    clusteringPolygonLayer = L.geoJSON(geojsonData, {
+      style: function (feature) {
+        return { color: "blue", weight: 2, fillOpacity: 0.1 };
+      },
+      onEachFeature: function (feature, layer) {
+        // ポップアップに情報を表示
+        layer.bindPopup(
+          `担当者: ${feature.properties.worker_id}<br>` +
+          `拠点数: ${feature.properties.point_count}`
+        );
+      }
+    }).addTo(map)
+  },
+  reset
+}
+
+const addListener = (map, points) => {
+  document.querySelector('main').addEventListener('click', async event => {
+    const { eventName } = event.target.dataset
+    if (typeof listeners[eventName] === 'function') {
+      try {
+        event.target.setAttribute('disabled', 'disabled')
+        await listeners[eventName](map, points)
+      } finally {
+        event.target.removeAttribute('disabled')
+      }
+    }
+  })
+}
+
+const main = async () => {
+  map = initMap({
+    lat: 35.652969988398745, lng: 139.7564792633057
+  })
+  clusteringTargetPoints = await loadJson('points.json')
+  const markerStyle = {
+    radius: 16,
+    fillColor: 'blue',
+    color: 'black',
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+  }
+
+  L.geoJSON([clusteringTargetPoints], {
+    pointToLayer: (feature, latlng) => {
+      return L.circleMarker(latlng, markerStyle)
+    }
+  }).addTo(map)
+
+  addListener()
+}
+
+main().then()
