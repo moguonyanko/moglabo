fdb7d41359340fd9d1bad95de9e373d4da0dbeff
moguonyako <moguonyanko@gmail.com>
2026-02-22 12:33:53 +0900

    トランジションのサンプル追加

--- lang/javascript/webxam/public/webapi/navigation/main.js
+++ lang/javascript/webxam/public/webapi/navigation/main.js
@@ -4,37 +4,70 @@
  * https://web.dev/blog/baseline-navigation-api?hl=en&authuser=3
  */
 
-async function renderContent(path) {
-  console.log(`Rendering UI for: ${path}...`)
-
-  const rendaringArea = document.getElementById('rendering-area')
-  rendaringArea.innerHTML = ''
-  const response = await fetch(path)
+const fetchPageContent = async url => {
+  const response = await fetch(url.pathname)
+  if (!response.ok) {
+    const detail = `fetch error:${response.status}:${await response.text()}`
+    window.dispatchEvent(new CustomEvent('fetcherror', { detail }))
+    return
+  }
   const html = await response.text()
-  rendaringArea.innerHTML = html
+  return html
 }
 
-navigation.addEventListener('navigate', event => {
-  console.log(event)
-  const url = new URL(event.destination.url)
+const renderContent = async content => {
+  const rendaringArea = document.getElementById('rendering-area')
+  rendaringArea.innerHTML = content
+}
 
-  event.intercept({
-    async handler() {
-      await renderContent(url.pathname)
-    }
-  })
-})
+const navigationListeners = {
+  simpleNavigate: event => {
+    const url = new URL(event.destination.url)
+
+    // URLが更新される。
+    // 更新後の状態でリロードすると更新先URLのコンテンツだけが表示された状態になってしまう。
+    event.intercept({
+      async handler() {
+        const content = await fetchPageContent(url)
+        await renderContent(content)
+      }
+    })
+  },
+  navigateWithTransition: event => {
+    const url = new URL(event.destination.url)
+
+    event.intercept({
+      async handler() {
+        const content = await fetchPageContent(url)
+        // トランジションの表現を伴ってページ更新される。
+        document.startViewTransition(async () => {
+          await renderContent(content)
+        })
+      }
+    })
+  }
+}
+
+const getNavigationName = event => {
+  // 戻るでページが表示された時event.sourceElementは未定義になっている。
+  return event.sourceElement?.dataset?.navigationName
+}
 
 const addListeners = () => {
-  const main = document.querySelector('main')
-  main.addEventListener('click', event => {
-    const { navigatable } = event.target.dataset
-    if (Boolean(navigatable)) {
-      // NavigationAPIを使うとしてもpreventDefaultはしないと複数回イベントが発生する。
-      event.preventDefault()
-      // event.stopPropagation()
-      navigation.navigate(event.target.href)
+  navigation.addEventListener('navigate', event => {
+    console.log(event)
+    if (event.canIntercept) {
+      const navigationName = getNavigationName(event)
+      if (navigationName) {
+        // navigattionListeners[navigator]が関数かどうかのチェックをするより
+        // 呼び出して問題がある場合は即エラーにしてしまった方が良い。
+        navigationListeners[navigationName](event)
+      }
     }
+  })
+
+  window.addEventListener('fetcherror', err => {
+    alert(err.message)
   })
 }
 
