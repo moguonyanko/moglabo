73f85245683bf6513d27f3de317ba69f4b47c7ae
moguonyako <moguonyanko@gmail.com>
2026-06-08 12:09:22 +0900

    ロックのabotのサンプル追加

--- lang/javascript/webxam/public/webapi/weblocks/main.js
+++ lang/javascript/webxam/public/webapi/weblocks/main.js
@@ -4,14 +4,34 @@
 
 // DOM
 
+const display = (baseClass, message) => {
+  const output = document.querySelector(`.${baseClass} .output`)
+  output.elements['result'].value += message + ' '
+}
+
 const listeners = {
-  "get-sample-lock": async () => {
-    const output = document.querySelector('.example.get-sample-lock .output')
+  onGetSampleLock: async () => {
     await navigator.locks.request('myResource',
-      { 'mode': 'shared' },
-      async lock => {
-        output.elements['result'].value = 'Get Lock'
-      })
+      { mode: 'shared' },
+      async lock => display('get-sample-lock', 'Get Lock'))
+  },
+  onGetSampleLockWithCancel: async () => {
+    const controller = new AbortController()
+    // ロックが取得されるとabortできないので相当短い時間を指定しないとabortを試せない。
+    const msForCancel = 1
+    setTimeout(() => controller.abort(), msForCancel)
+
+    try {
+      const callback = async lock =>
+        display('get-sample-lock-with-cancel', 'Get Lock With Cancel')
+      const options = {
+        // 未指定なのでmodeはデフォルト値のexclusiveになる。
+        signal: controller.signal
+      }
+      await navigator.locks.request('myResource', options, callback)
+    } catch (err) {
+      display('get-sample-lock-with-cancel', err.message)
+    }
   }
 }
 
